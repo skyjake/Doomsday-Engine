38923f7a6bfa2df2f9e83a3a83d57cf1e971540d
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-02-26 17:59:43 +0000

    Initial checkin

---
+++ distrib/makedist.bat
@@ -0,0 +1,136 @@
+@ECHO OFF
+REM -- Makes the Doomsday distribution in the Distrib subdir.
+
+SET Dist=Distrib\
+SET DistBin=%Dist%\Bin\
+
+REM -- Source directories (where files will be copied from).
+SET SRC=..\doomsday\
+SET FMODLIB=D:\sdk\fmod\api\
+SET SidgarDir=C:\Projects\SiDGaR\Release\
+SET KickDir=C:\Projects\KickStart\
+SET BSPDir=c:\utils\glbsp\
+
+IF "%1" == "rel" GOTO releasedirs
+IF "%1" == "beta" GOTO betadirs
+
+ECHO ===================== MAKING  D E B U G  DISTRIBUTION =====================
+SET BinDir=%SRC%Bin\Debug\
+SET SupDir=%SRC%Bin\Debug\
+GOTO startcopy
+
+:betadirs
+ECHO ====================== MAKING  B E T A  DISTRIBUTION ======================
+SET BinDir=%SRC%Bin\Debug\
+SET SupDir=%SRC%Bin\Release\
+GOTO startcopy
+
+:releasedirs
+ECHO =================== MAKING  R E L E A S E  DISTRIBUTION ===================
+SET BinDir=%SRC%Bin\Release\
+SET SupDir=%SRC%Bin\Release\
+
+:startcopy
+
+REM +---------------------------------------------------------------+
+REM + Binaries							    +
+REM +---------------------------------------------------------------+
+echo Copying binaries...
+
+xcopy %BinDir%Doomsday.exe	%DistBin%
+xcopy %BinDir%jDoom.dll 	%DistBin%
+xcopy %BinDir%jHeretic.dll 	%DistBin%
+xcopy %BinDir%jHexen.dll 	%DistBin%
+xcopy %SupDir%drOpenGL.dll 	%DistBin%
+xcopy %SupDir%drD3D.dll 	%DistBin%
+xcopy %SupDir%dsA3D.dll		%DistBin%
+xcopy %SupDir%dsCompat.dll	%DistBin%
+xcopy %SupDir%dpDehRead.dll     %DistBin%
+xcopy %SupDir%jtNet2.dll 	%DistBin%
+xcopy %SRC%DLLs\LZSS.dll 	%DistBin%
+xcopy %SRC%DLLs\EAX.dll 	%DistBin%
+xcopy %SRC%DLLs\zlib.dll	%DistBin%
+xcopy %FMODLIB%fmod.dll		%DistBin%
+xcopy %SidgarDir%*.exe		%Dist%
+
+REM +---------------------------------------------------------------+
+REM + Data							    +
+REM +---------------------------------------------------------------+
+echo Copying Data...
+
+xcopy %SRC%Data\Doomsday.wad		%Dist%Data\
+xcopy %SRC%Data\CPHelp.txt		%Dist%Data\
+xcopy %SRC%Data\Detail\Detail.wad	%Dist%Data\
+xcopy %SRC%Data\jDoom\jDoom.wad		%Dist%Data\jDoom\
+xcopy %SRC%Data\jDoom\udemo2.cdm	%Dist%Run\jDoom\Demo\
+xcopy %SRC%Data\jHeretic\jHeretic.wad	%Dist%Data\jHeretic\
+xcopy %SRC%Data\jHexen\jHexen.wad	%Dist%Data\jHexen\
+xcopy %SRC%Data\Fonts\*.dfn		%Dist%Data\Fonts\
+xcopy %SRC%Data\Fonts\Readme.txt	%Dist%Data\Fonts\
+xcopy %SRC%Data\KeyMaps\*.dkm		%Dist%Data\KeyMaps\
+
+REM +---------------------------------------------------------------+
+REM + Definitions						    +
+REM +---------------------------------------------------------------+
+echo Copying Definitions...
+
+xcopy /s %SRC%Defs\jDoom\*.ded		%Dist%Defs\jDoom\
+xcopy /s %SRC%Defs\jHeretic\*.ded	%Dist%Defs\jHeretic\
+xcopy /s %SRC%Defs\jHexen\*.ded		%Dist%Defs\jHexen\
+
+REM +---------------------------------------------------------------+
+REM + Scripts    						    +
+REM +---------------------------------------------------------------+
+echo Copying Scripts...
+
+xcopy %SRC%Runtime\jDoom\Startup.cfg	%Dist%Run\jDoom\
+xcopy %SRC%Runtime\jHeretic\Startup.cfg %Dist%Run\jHeretic\
+xcopy %SRC%Runtime\jHexen\Startup.cfg	%Dist%Run\jHexen\
+
+REM +---------------------------------------------------------------+
+REM + Model Files						    +
+REM +---------------------------------------------------------------+
+echo Copying Models (not jDoom's)...
+
+REM -- deltree /y %Dist%\Md2\jDoom
+rd /s /q %Dist%Md2\jHeretic
+rd /s /q %Dist%Md2\jHexen
+
+REM -- xcopy /e /s Md2\jDoom\*.*       %Dist%\Md2\jDoom\
+xcopy /y /e /s %SRC%Md2\jHeretic\*.*    %Dist%Md2\jHeretic\
+xcopy /y /e /s %SRC%Md2\jHexen\*.*      %Dist%Md2\jHexen\
+
+REM +---------------------------------------------------------------+
+REM + Documentation						    +
+REM +---------------------------------------------------------------+
+echo Copying Documentation...
+
+xcopy %SRC%Doc\DHistory.txt           	%Dist%Doc\
+xcopy %SRC%Doc\Ame\TXT\Readme.txt	%Dist%Doc\
+xcopy %SRC%Doc\Ame\TXT\Beginner.txt	%Dist%Doc\
+xcopy %SRC%Doc\Ame\TXT\CmdLine.txt	%Dist%Doc\
+xcopy %SRC%Doc\DEDDoc.txt		%Dist%Doc\
+xcopy %SRC%Doc\DSS.txt			%Dist%Doc\
+xcopy %SRC%Doc\Network.txt		%Dist%Doc\
+xcopy %SRC%Doc\Example.bat            	%Dist%Doc\
+xcopy %SRC%Doc\InFine.txt		%Dist%Doc\
+xcopy %SRC%Doc\jDoom\Doomlic.txt      	%Dist%Doc\jDoom\
+xcopy %SRC%Doc\jDoom\History.txt	%Dist%Doc\jDoom\
+xcopy %SRC%Doc\jDoom\jDoom.txt		%Dist%Doc\jDoom\
+xcopy %SRC%Doc\jHeretic\jHeretic.txt	%Dist%Doc\jHeretic\
+xcopy %SRC%Doc\jHeretic\History.txt	%Dist%Doc\jHeretic\
+xcopy %SRC%Doc\Ravenlic.txt		%Dist%Doc\jHeretic\
+xcopy %SRC%Doc\jHexen\jHexen.txt	%Dist%Doc\jHexen\
+xcopy %SRC%Doc\jHexen\History.txt	%Dist%Doc\jHexen\
+xcopy %SRC%Doc\Ravenlic.txt		%Dist%Doc\jHexen\
+
+REM +---------------------------------------------------------------+
+REM + Other Files  						    +
+REM +---------------------------------------------------------------+
+echo Copying Other Files...
+
+xcopy %KickDir%Kicks.exe 	%Dist%
+xcopy %KickDir%Profiles\*.kss   %Dist%
+xcopy %BSPDir%glbsp.exe   	%Dist%
+xcopy %BSPDir%glbsp.txt   	%Dist%
+
---
+++ distrib/makesnap.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+REM --- Build the binary snapshot.
+REM --- %1 optional; "nodll" will not add rendering and sound DLLs
+
+ECHO ========================= MAKING BINARY SNAPSHOT ==========================
+
+SET RAR=-m5
+
+REM --- Pack it with RAR.
+
+SET OUT=Out\
+md %OUT%
+
+SET SRC=..\doomsday\
+SET FILE=%OUT%ddsnapshot
+del %FILE%.rar
+
+REM --- The files
+
+rar -ep -r a %FILE% %SRC%Bin\Release\Doomsday.exe %SRC%Bin\Release\jDoom.dll %SRC%Bin\Release\jHeretic.dll %SRC%Bin\Release\jHexen.dll
+
+if "%1"=="nodll" goto skipdll
+rar -ep -r a %FILE% %SRC%Bin\Release\drOpenGL.dll %SRC%Bin\Release\drD3D.dll %SRC%Bin\Release\dsA3D.dll %SRC%Bin\Release\dsCompat.dll
+:skipdll
+
+echo Uploading to The Mirror...
+ftpscrpt -f z:\scripts\mirror-snapshot.scp
+
+echo Uploading to Fourwinds...
+ftpscrpt -f z:\scripts\snapshot.scp
+
+echo Done.
+
