f54f34c3cc98b132546e30b4f07cf4f95bd91ec9
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-03-14 15:40:08 +0000

    Cleanup; P_MapExists added

--- doomsday/Include/jDoom/p_setup.h
+++ doomsday/Include/jDoom/p_setup.h
@@ -1,54 +1,13 @@
-// Emacs style mode select   -*- C++ -*- 
-//-----------------------------------------------------------------------------
-//
-// $Id$
-//
-// Copyright (C) 1993-1996 by id Software, Inc.
-//
-// This source is available for distribution and/or modification
-// only under the terms of the DOOM Source Code License as
-// published by id Software. All rights reserved.
-//
-// The source is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
-// for more details.
-//
-// DESCRIPTION:
-//   Setup a game, startup stuff.
-//
-//-----------------------------------------------------------------------------
-
-
+/* $Id$
+ * Map setup routines.
+ */
 #ifndef __P_SETUP__
 #define __P_SETUP__
 
-
-#ifdef __GNUG__
-#pragma interface
-#endif
-
-
-// NOT called by W_Ticker. Fixme.
-void
-P_SetupLevel
-( int		episode,
-  int		map,
-  int		playermask,
-  skill_t	skill);
-
 // Called by startup code.
-void P_Init (void);
+void	P_Init (void);
+
+boolean	P_MapExists(int episode, int map);
+void	P_SetupLevel(int episode, int map, int playermask, skill_t skill);
 
 #endif
-//-----------------------------------------------------------------------------
-//
-// $Log$
-// Revision 1.1  2003/02/26 19:18:37  skyjake
-// Initial checkin
-//
-// Revision 1.1  2002/09/29 01:04:13  Jaakko
-// Added all headers
-//
-//
-//-----------------------------------------------------------------------------
