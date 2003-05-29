f4df36103527fafcb688a7169054e1a15968e190
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-05-29 16:23:08 +0000

    Added auto-upload

--- distrib/makeplug.bat
+++ distrib/makeplug.bat
@@ -19,3 +19,9 @@ REM --- Files
 
 rar -r a %FILE% %SRC%Include\doomsday.h %SRC%Include\dd_share.h %SRC%Include\dd_plugin.h %SRC%Include\dd_types.h %SRC%Include\p_think.h %SRC%Include\dd_dfdat.h %SRC%Include\dglib.h %SRC%Include\Common\g_dgl.h %SRC%Src\Common\g_dglinit.c
 rar -r -ep a %FILE% %SRC%Bin\Release\Doomsday.lib
+
+echo Uploading to The Mirror...
+ftpscrpt -f z:\scripts\mirror-plugsdk.scp
+
+echo Uploading to Fourwinds...
+ftpscrpt -f z:\scripts\plugsdk.scp
