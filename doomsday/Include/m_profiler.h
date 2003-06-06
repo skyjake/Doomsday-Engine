8befd1bad2eff0637d641a9c9be01150664cd637
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-06 14:13:04 +0000

    Added a missing underscore

--- doomsday/Include/m_profiler.h
+++ doomsday/Include/m_profiler.h
@@ -24,7 +24,7 @@ typedef struct profiler_s {
 # define END_PROF_TIMERS()	,NUM_PROFS }; static profiler_t profiler_[NUM_PROFS];
 # define BEGIN_PROF(x)		(profiler_[x].startCount++, profiler_[x].startTime = Sys_GetRealTime())
 # define END_PROF(x)		(profiler_[x].totalTime += Sys_GetRealTime() - profiler_[x].startTime)
-# define PRINT_PROF(x)		Con_Message(#x ": %i ms (%i starts)\n", profiler[x].totalTime, profiler[x].startCount) 
+# define PRINT_PROF(x)		Con_Message(#x ": %i ms (%i starts)\n", profiler_[x].totalTime, profiler_[x].startCount) 
 #else // Profiling is disabled.
 # define END_PROF_TIMERS()	,NUM_PROFS };
 # define BEGIN_PROF(x)		
