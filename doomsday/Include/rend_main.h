637be511742f342c4b921294185d315fafb195cd
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-10 09:25:20 +0000

    Added Rend_PointDist3D()

--- doomsday/Include/rend_main.h
+++ doomsday/Include/rend_main.h
@@ -1,6 +1,7 @@
 #ifndef __DOOMSDAY_REND_MAIN_H__
 #define __DOOMSDAY_REND_MAIN_H__
 
+#include "rend_list.h"
 #include "r_things.h"
 
 extern float	vx, vy, vz, vang, vpitch, fieldOfView, yfov;
@@ -16,7 +17,9 @@ void	Rend_RenderMap(void);
 void	Rend_ModelViewMatrix(boolean use_angles);
 
 float	Rend_PointDist2D(float c[2]);
+float	Rend_PointDist3D(float c[3]);
 float	Rend_SignedPointDist2D(float c[2]);
 int		Rend_SegFacingDir(float v1[2], float v2[2]);
+int		Rend_MidTexturePos(rendpoly_t *quad, float tcyoff, boolean lower_unpeg);
 
 #endif
