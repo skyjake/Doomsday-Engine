9e729d948656593fc9a88ad054b744c7f9114876
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-23 08:23:46 +0000

    Added new cl/sv headers

--- doomsday/Include/de_network.h
+++ doomsday/Include/de_network.h
@@ -9,7 +9,12 @@
 #include "net_demo.h"
 #include "sv_def.h"
 #include "sv_pool.h"
+#include "sv_frame.h"
 #include "sv_sound.h"
 #include "cl_def.h"
+#include "cl_mobj.h"
+#include "cl_frame.h"
+#include "cl_sound.h"
+#include "cl_world.h"
 
 #endif 
