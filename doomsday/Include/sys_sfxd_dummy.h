118ddfeb433ac4782ee58847950cd258406f658b
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-23 08:05:12 +0000

    Initial checkin, dummy driver for dedicated server

---
+++ doomsday/Include/sys_sfxd_dummy.h
@@ -0,0 +1,12 @@
+//===========================================================================
+// SYS_SFXD_DUMMY.H
+//===========================================================================
+#ifndef __DOOMSDAY_SYSTEM_SFX_DUMMY_H__
+#define __DOOMSDAY_SYSTEM_SFX_DUMMY_H__
+
+#include "sys_sfxd.h"
+
+// The dummy driver is used in dedicated server mode.
+extern sfxdriver_t sfxd_dummy;
+
+#endif 
