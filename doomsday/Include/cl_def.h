af5f8fc2f724f954c2f55555e75a49792f944afe
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-24 21:18:58 +0000

    Cleanup, added new client mobj flags

--- doomsday/Include/cl_def.h
+++ doomsday/Include/cl_def.h
@@ -8,9 +8,19 @@ extern id_t clientID;
 extern int server_time;
 extern int predict_tics;
 
+// Flags for clmobjs.
+#define CLMF_HIDDEN			0x01	// Not officially created yet
+#define CLMF_UNPREDICTABLE	0x02	// Temporarily hidden (until next delta)
+#define CLMF_SOUND			0x04	// Sound is queued for playing on unhide.
+#define CLMF_NULLED			0x08	// Once nulled, it can't be updated.
+
 typedef struct clmobj_s
 {
 	struct clmobj_s *next, *prev;
+	byte flags;	
+	uint time;						// Time of last update.
+	int sound;						// Queued sound ID.
+	float volume;					// Volume for queued sound.
 	mobj_t mo;
 } clmobj_t;
 
@@ -31,7 +41,6 @@ typedef struct playerstate_s
 extern boolean handshake_received;
 extern int game_ready;
 extern boolean net_loggedin;
-extern clmobj_t cmRoot;
 extern boolean clientPaused;
 
 void Cl_InitID(void);
@@ -40,29 +49,6 @@ void Cl_GetPackets(void);
 void Cl_Ticker(void);
 int Cl_GameReady();
 void Cl_SendHello(void);
-
-// 
-// cl_frame.c
-//
-void Cl_FrameReceived(void);
-
-// 
-// cl_mobj.c
-//
-void Cl_InitClientMobjs();
-void Cl_CleanClientMobjs();
-void Cl_DestroyClientMobjs();
-void Cl_SwapBuffers();
-void Cl_HandleFrame();
-void Cl_PredictMovement(boolean forward, boolean age_buffer);
-void Cl_UnpackMobj(mobj_t *mobj);
-void Cl_UnpackMobj2(int unpack_flags, mobj_t *mobj);
-void Cl_UnsetThingPosition(mobj_t *thing);
-void Cl_SetThingPosition(mobj_t *thing);
-int Cl_ReadMobjDelta(void);
-clmobj_t *Cl_FindMobj(thid_t id);
-void Cl_CheckMobj(mobj_t *mo);
-void Cl_UpdateRealPlayerMobj(mobj_t *mo, mobj_t *clmo, int flags);
 
 //
 // cl_player.c
@@ -76,27 +62,9 @@ void Cl_LocalCommand(void);
 void Cl_MovePlayer(ddplayer_t *pl);
 void Cl_MoveLocalPlayer(int dx, int dy, int dz, boolean onground);
 int Cl_ReadPlayerDelta(void);
+void Cl_ReadPlayerDelta2(void);
 void Cl_UpdatePlayerPos(ddplayer_t *pl);
 void Cl_MovePsprites(void);
 void Cl_CoordsReceived(void);
-
-//
-// cl_world.c
-//
-void Cl_InitTranslations(void);
-int Cl_ReadSectorDelta(void);
-int Cl_ReadLumpDelta(void);
-int Cl_ReadSideDelta(void);
-int Cl_ReadPolyDelta(void);
-void Cl_InitMovers();
-void Cl_RemoveMovers();
-void Cl_HandleSectorUpdate();
-void Cl_HandleWallUpdate();
-void Cl_HandlePlaneSound();
-
-//
-// cl_sound.c
-//
-void Cl_Sound(void);
 
 #endif
