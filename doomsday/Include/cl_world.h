b5d4492941c98c66529aa6a9b676034e7040f984
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-24 21:20:18 +0000

    Moved to a separate file

---
+++ doomsday/Include/cl_world.h
@@ -0,0 +1,19 @@
+//===========================================================================
+// CL_WORLD.H
+//===========================================================================
+#ifndef __DOOMSDAY_CLIENT_WORLD_H__
+#define __DOOMSDAY_CLIENT_WORLD_H__
+
+void Cl_InitTranslations(void);
+void Cl_InitMovers();
+void Cl_RemoveMovers();
+
+int Cl_ReadSectorDelta(void);
+int Cl_ReadLumpDelta(void);
+int Cl_ReadSideDelta(void);
+int Cl_ReadPolyDelta(void);
+void Cl_ReadSectorDelta2(void);
+void Cl_ReadSideDelta2(void);
+void Cl_ReadPolyDelta2(void);
+
+#endif 
