10cb04177924d6564da4fe8ceee5e42cfa6e95ba
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-24 19:53:04 +0000

    Initial checkin, the Logical Sound Manager

---
+++ doomsday/Include/s_logic.h
@@ -0,0 +1,13 @@
+//===========================================================================
+// S_LOGIC.H
+//===========================================================================
+#ifndef __DOOMSDAY_SOUND_LOGICAL_H__
+#define __DOOMSDAY_SOUND_LOGICAL_H__
+
+void		Sfx_InitLogical(void);
+void		Sfx_PurgeLogical(void);
+void		Sfx_StartLogical(int id, mobj_t *origin, boolean isRepeating);
+void		Sfx_StopLogical(int id, mobj_t *origin);
+boolean		Sfx_IsPlaying(int id, mobj_t *origin);
+
+#endif 
