d36d9a45f1c72776a397e44b516c9b1f3679332e
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-24 21:19:54 +0000

    Moved clientside sound routines to a separate file, psv_frame2

---
+++ doomsday/Include/cl_sound.h
@@ -0,0 +1,10 @@
+//===========================================================================
+// CL_SOUND.H
+//===========================================================================
+#ifndef __DOOMSDAY_CLIENT_SOUND_H__
+#define __DOOMSDAY_CLIENT_SOUND_H__
+
+void Cl_ReadSoundDelta2(deltatype_t type);
+void Cl_Sound(void);
+
+#endif 
