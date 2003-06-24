a27aa7df926744965e1de905a9ea2375425ea65a
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-24 21:19:26 +0000

    Moved client mobj routines to a separate file

---
+++ doomsday/Include/cl_mobj.h
@@ -0,0 +1,23 @@
+//===========================================================================
+// CL_MOBJ.H
+//===========================================================================
+#ifndef __DOOMSDAY_CLIENT_MOBJ_H__
+#define __DOOMSDAY_CLIENT_MOBJ_H__
+
+void Cl_InitClientMobjs();
+void Cl_Reset(void);
+void Cl_DestroyClientMobjs();
+clmobj_t *Cl_CreateMobj(thid_t id);
+void Cl_DestroyMobj(clmobj_t *cmo);
+boolean Cl_MobjIterator(boolean (*callback)(clmobj_t*, void*), void *parm);
+void Cl_PredictMovement(void);
+void Cl_UnsetThingPosition(clmobj_t *cmo);
+void Cl_SetThingPosition(clmobj_t *cmo);
+int Cl_ReadMobjDelta(void);
+void Cl_ReadMobjDelta2(boolean allowCreate);
+void Cl_ReadNullMobjDelta2(void);
+clmobj_t *Cl_FindMobj(thid_t id);
+void Cl_CheckMobj(mobj_t *mo);
+void Cl_UpdateRealPlayerMobj(mobj_t *mo, mobj_t *clmo, int flags);
+
+#endif 
