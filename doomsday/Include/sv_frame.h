137870c3bcd4ee1a515d8d64331fdafa41d7cdbb
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-24 21:22:04 +0000

    Moved to a separate file, psv_frame2

---
+++ doomsday/Include/sv_frame.h
@@ -0,0 +1,10 @@
+//===========================================================================
+// SV_FRAME.H
+//===========================================================================
+#ifndef __DOOMSDAY_SERVER_FRAME_H__
+#define __DOOMSDAY_SERVER_FRAME_H__
+
+void	Sv_TransmitFrame(void);
+int		Sv_GetMaxFrameSize(int playerNumber);
+
+#endif 
