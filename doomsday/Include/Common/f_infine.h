0d533fa82b5362aec9f81922896115e9262e13c4
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-06-27 20:02:06 +0000

    Added conditions array

--- doomsday/Include/Common/f_infine.h
+++ doomsday/Include/Common/f_infine.h
@@ -1,10 +1,19 @@
 #ifndef __INFINE_H__
 #define __INFINE_H__
 
+// Condition truth values (that clients can't deduce on their own).
+enum 
+{
+	FICOND_SECRET,
+	FICOND_LEAVEHUB,
+	NUM_FICONDS
+};
+
 extern boolean brief_disabled;
 
 void	FI_StartScript(char *finalescript, boolean after);
 void	FI_End(void);
+void	FI_SetCondition(int index, boolean value);
 int		FI_Briefing(int episode, int map);
 int		FI_Debriefing(int episode, int map);
 int		FI_SkipRequest(void);
