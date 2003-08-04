d697ae272cb1f69b523ff5489b94ac9b2f05eed9
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-08-04 22:04:40 +0000

    Rend_PointDist2D is now a macro

--- doomsday/Include/rend_main.h
+++ doomsday/Include/rend_main.h
@@ -1,6 +1,7 @@
 #ifndef __DOOMSDAY_REND_MAIN_H__
 #define __DOOMSDAY_REND_MAIN_H__
 
+#include <math.h>
 #include "rend_list.h"
 #include "r_things.h"
 
@@ -13,7 +14,8 @@ extern float	vx, vy, vz, vang, vpitch, fieldOfView, yfov;
 extern boolean	smoothTexAnim;
 extern float	viewsidex, viewsidey;	
 extern int		missileBlend, litSprites;
-extern boolean	whitefog;
+extern boolean	useFog;
+extern byte		fogColor[4];
 extern int		r_ambient;
 
 void	Rend_Init(void);
@@ -22,9 +24,12 @@ void	Rend_Reset(void);
 void	Rend_RenderMap(void);
 void	Rend_ModelViewMatrix(boolean use_angles);
 
-float	Rend_PointDist2D(float c[2]);
+#define Rend_PointDist2D(c) (fabs((vz-c[VY])*viewsidex - (vx-c[VX])*viewsidey))
+
+//float	Rend_PointDist2D(float c[2]);
 float	Rend_PointDist3D(float c[3]);
 float	Rend_SignedPointDist2D(float c[2]);
+int		Rend_SectorLight(sector_t *sec);
 int		Rend_SegFacingDir(float v1[2], float v2[2]);
 int		Rend_MidTexturePos(float *top, float *bottom, float *texoffy, 
 						   float tcyoff, boolean lower_unpeg);
