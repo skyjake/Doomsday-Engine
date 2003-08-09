e18f87525078129cf88b2c5996c5ed0674f611a9
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-08-09 23:56:46 +0000

    Removed Rend_Render3DVisSprite()

--- doomsday/Include/rend_sprite.h
+++ doomsday/Include/rend_sprite.h
@@ -9,7 +9,6 @@ extern float maxSpriteAngle;
 void Rend_DrawMasked (void);
 void Rend_DrawPlayerSprites(void);
 void Rend_Draw3DPlayerSprites(void);
-void Rend_Render3DVisSprite(vissprite_t *vismdl);
 void Rend_SpriteTexCoord(int pnum, int x, int y);
 void Rend_RenderSprite(vissprite_t *spr);
 
