b441ee745b84e84fda18c7722905e8796072d168
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-08-13 23:35:55 +0000

    Moved clipnode_t to rend_clip.c, no need for global scope

--- doomsday/Include/rend_clip.h
+++ doomsday/Include/rend_clip.h
@@ -6,22 +6,11 @@
 
 #include "m_bams.h"
 
-typedef struct clipnode_s
-{
-	int					used;			// 1 if the node is in use.
-	struct clipnode_s	*prev, *next;	// Previous and next nodes.
-	binangle_t			start, end;		// The start and end angles 
-}										// (start < end).
-clipnode_t;
-
-extern clipnode_t *clipnodes;	// The list of clipnodes.
-extern clipnode_t *cliphead;	// The head node.
-
-void		C_Init();
-void		C_ClearRanges();
-void		C_Ranger(); // Debugging aid.
+void		C_Init(void);
+boolean		C_IsFull(void);
+void		C_ClearRanges(void);
+void		C_Ranger(void); // Debugging aid.
 int			C_SafeAddRange(binangle_t startAngle, binangle_t endAngle);
-clipnode_t *C_AngleClippedBy(binangle_t bang);
 boolean		C_IsPointVisible(float x, float y, float height);
 
 // Add a segment relative to the current viewpoint.
