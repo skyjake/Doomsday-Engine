b794558537b907896ad87f157c818d2be4dbf194
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-08-15 22:07:52 +0000

    Moved blending modes here

--- doomsday/Include/rend_main.h
+++ doomsday/Include/rend_main.h
@@ -10,6 +10,14 @@
 #define SEG_TOP		0x2
 #define SEG_BOTTOM	0x4
 
+typedef enum blendmode_e {
+	BM_NORMAL,
+	BM_ADD,
+	BM_DARK,
+	BM_SUBTRACT,
+	BM_REVERSE_SUBTRACT
+} blendmode_t;
+
 extern float	vx, vy, vz, vang, vpitch, fieldOfView, yfov;
 extern boolean	smoothTexAnim;
 extern float	viewsidex, viewsidey;	
@@ -23,10 +31,10 @@ void	Rend_Reset(void);
 
 void	Rend_RenderMap(void);
 void	Rend_ModelViewMatrix(boolean use_angles);
+void	Rend_BlendMode(blendmode_t mode);
 
 #define Rend_PointDist2D(c) (fabs((vz-c[VY])*viewsidex - (vx-c[VX])*viewsidey))
 
-//float	Rend_PointDist2D(float c[2]);
 float	Rend_PointDist3D(float c[3]);
 float	Rend_SignedPointDist2D(float c[2]);
 int		Rend_SectorLight(sector_t *sec);
