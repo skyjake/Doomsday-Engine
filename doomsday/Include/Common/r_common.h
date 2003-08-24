87b0a11b15db1ab9f1729e6aceed0272aac21da1
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-08-24 00:11:29 +0000

    Initial checkin, common game refresh routines

---
+++ doomsday/Include/Common/r_common.h
@@ -0,0 +1,9 @@
+//===========================================================================
+// R_COMMON.H
+//===========================================================================
+#ifndef __DOOMSDAY_COMMON_REFRESH_H__
+#define __DOOMSDAY_COMMON_REFRESH_H__
+
+void R_PrecachePSprites(void);
+
+#endif 
---
+++ doomsday/Src/Common/r_common.c
@@ -0,0 +1,87 @@
+
+//**************************************************************************
+//**
+//** R_COMMON.C
+//**
+//**************************************************************************
+
+// HEADER FILES ------------------------------------------------------------
+
+#ifdef __JDOOM__
+#include "doomdef.h"
+#include "d_items.h"
+#endif
+
+#ifdef __JHERETIC__
+#include "doomdef.h"
+#endif
+
+#ifdef __JHEXEN__
+#include "h2def.h"
+#endif
+
+// MACROS ------------------------------------------------------------------
+
+// TYPES -------------------------------------------------------------------
+
+// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
+
+// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+
+// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
+
+// EXTERNAL DATA DECLARATIONS ----------------------------------------------
+
+// PUBLIC DATA DEFINITIONS -------------------------------------------------
+
+// PRIVATE DATA DEFINITIONS ------------------------------------------------
+
+// CODE --------------------------------------------------------------------
+
+//===========================================================================
+// R_PrecachePSprites
+//===========================================================================
+void R_PrecachePSprites(void)
+{
+	int i;
+#ifdef __JHEXEN__
+	int k;
+#endif
+
+	for(i = 0; i < NUMWEAPONS; i++)
+	{
+#ifdef __JDOOM__
+		R_PrecacheSkinsForState(weaponinfo[i].upstate);
+		R_PrecacheSkinsForState(weaponinfo[i].downstate);
+		R_PrecacheSkinsForState(weaponinfo[i].readystate);
+		R_PrecacheSkinsForState(weaponinfo[i].atkstate);
+		R_PrecacheSkinsForState(weaponinfo[i].flashstate);
+#endif
+
+#ifdef __JHERETIC__
+		R_PrecacheSkinsForState(wpnlev1info[i].upstate);
+		R_PrecacheSkinsForState(wpnlev1info[i].downstate);
+		R_PrecacheSkinsForState(wpnlev1info[i].readystate);
+		R_PrecacheSkinsForState(wpnlev1info[i].atkstate);
+		R_PrecacheSkinsForState(wpnlev1info[i].holdatkstate);
+		R_PrecacheSkinsForState(wpnlev1info[i].flashstate);
+
+		R_PrecacheSkinsForState(wpnlev2info[i].upstate);
+		R_PrecacheSkinsForState(wpnlev2info[i].downstate);
+		R_PrecacheSkinsForState(wpnlev2info[i].readystate);
+		R_PrecacheSkinsForState(wpnlev2info[i].atkstate);
+		R_PrecacheSkinsForState(wpnlev2info[i].holdatkstate);
+		R_PrecacheSkinsForState(wpnlev2info[i].flashstate);
+#endif
+
+#ifdef __JHEXEN__
+		k = players[consoleplayer].class;
+		R_PrecacheSkinsForState(WeaponInfo[i][k].upstate);
+		R_PrecacheSkinsForState(WeaponInfo[i][k].downstate);
+		R_PrecacheSkinsForState(WeaponInfo[i][k].readystate);
+		R_PrecacheSkinsForState(WeaponInfo[i][k].atkstate);
+		R_PrecacheSkinsForState(WeaponInfo[i][k].holdatkstate);
+		R_PrecacheSkinsForState(WeaponInfo[i][k].flashstate);
+#endif		
+	}
+}
