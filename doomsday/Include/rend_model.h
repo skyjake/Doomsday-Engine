62bc207e9cab275541261429171a78c729f38725
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-08-28 01:49:44 +0000

    Added cvar rend-model-shiny-multitex

--- doomsday/Include/rend_model.h
+++ doomsday/Include/rend_model.h
@@ -9,6 +9,7 @@
 extern int		modelLight;
 extern int		frameInter;
 extern int		mirrorHudModels;
+extern int		modelShinyMultitex;
 extern float	rend_model_lod;
 
 void Rend_RenderModel(vissprite_t *spr);
