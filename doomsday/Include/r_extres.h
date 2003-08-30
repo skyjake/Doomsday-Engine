4193d19f8c6e84ee896a078a139741df68c872cc
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-08-30 15:30:04 +0000

    Added Graphics resource class, non-game resource handling

--- doomsday/Include/r_extres.h
+++ doomsday/Include/r_extres.h
@@ -13,11 +13,13 @@ typedef enum resourceclass_e {
 	RC_LIGHTMAP,
 	RC_MUSIC,		// Names == lumpnames.
 	RC_SFX,			// Names == lumpnames.
+	RC_GRAPHICS,	// Doomsday graphics.
 	NUM_RESOURCE_CLASSES
 } resourceclass_t;
 
 void	R_InitExternalResources(void);
 void	R_SetDataPath(const char *path);
+void	R_InitDataPaths(const char *path, boolean justGamePaths);
 const char *R_GetDataPath(void);
 void	R_PrependDataPath(const char *origPath, char *newPath);
 boolean	R_FindResource(resourceclass_t resClass, const char *name, 
