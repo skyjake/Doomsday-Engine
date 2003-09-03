ab2c08e7b25bbca4d24c1e6ca41eacc37d0eb6ee
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-09-03 16:01:31 +0000

    Added a proper GPL banner

--- doomsday/Include/con_action.h
+++ doomsday/Include/con_action.h
@@ -1,11 +1,30 @@
-//===========================================================================
-// CON_ACTION.H
-//===========================================================================
+/* DE1: $Id$
+ * Copyright (C) 2003 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * con_action.h: Action Commands (Player Controls)
+ */
+
 #ifndef __DOOMSDAY_CONSOLE_ACTION_H__
 #define __DOOMSDAY_CONSOLE_ACTION_H__
 
 void Con_DefineActions(action_t *acts);
 void Con_ClearActions(void);
+
 // The command begins with a '+' or a '-'.
 // Returns true if the action was changed successfully.
 int Con_ActionCommand(char *cmd, boolean has_prefix);
