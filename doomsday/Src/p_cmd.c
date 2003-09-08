c97ed6b7f4c1fb01eee6c7e05e52d9e246d80cfd
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-09-08 22:12:29 +0000

    Initial checkin, ticcmd processing

---
+++ doomsday/Src/p_cmd.c
@@ -0,0 +1,72 @@
+/* DE1: $Id$
+ * Copyright (C) 2003 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * p_cmd.c: Tic Commands
+ *
+ * Tic commands are generated out of controller state. There is one 
+ * command per input tic (35 Hz). The commands are used to control all
+ * players.
+ */
+
+// HEADER FILES ------------------------------------------------------------
+
+#include "de_base.h"
+#include "de_network.h"
+#include "de_play.h"
+
+// MACROS ------------------------------------------------------------------
+
+// TYPES -------------------------------------------------------------------
+
+// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
+
+// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+
+// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
+
+// EXTERNAL DATA DECLARATIONS ----------------------------------------------
+
+// PUBLIC DATA DEFINITIONS -------------------------------------------------
+
+// PRIVATE DATA DEFINITIONS ------------------------------------------------
+
+// CODE --------------------------------------------------------------------
+
+/*
+ * Build one command for the specified player. This routine is used to
+ * generate commands for local players. The commands are added to the
+ * command buffer.
+ *
+ * This function is called from the input thread.
+ */
+void P_BuildCommand(int playerNumber)
+{
+	ddplayer_t *player = players + playerNumber;
+	client_t *client = clients + playerNumber;
+	ticcmd_t cmd;
+
+	// The command will stay 'empty' if no controls are active.
+	memset(&cmd, 0, sizeof(cmd));
+
+	// Examine the state of controllers to see which controls are active.
+	//...
+
+	// The command is now complete. Insert it into the client's command
+	// buffer, where it will be read from by the refresh thread.
+	Net_NewLocalCmd(&cmd, playerNumber);
+}
