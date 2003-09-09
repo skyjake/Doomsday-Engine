3b62241f801cffa7efd72d750941df35b5566aa7
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2003-09-09 21:39:27 +0000

    Initial checkin, players and player classes

---
+++ doomsday/Include/p_player.h
@@ -0,0 +1,40 @@
+/* DE1: $Id$
+ * Copyright (C) 2003 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * p_player.h: Players and Player Classes
+ */
+
+#ifndef __DOOMSDAY_PLAYERS_H__
+#define __DOOMSDAY_PLAYERS_H__
+
+// Class flags.
+#define PCF_CAMERA			0x1	// Invisible camera.
+#define PCF_VERTICAL_FLY	0x2	// Fly in traditional vertical mode.
+
+/*
+ * Player classes define the characteristics 
+ */
+typedef struct playerclass_s {
+	int flags;
+	int allowActions;			// TCAF-bits for all permitted actions.
+	fixed_t forwardMove[2];		// Normal speed and high speed.
+	fixed_t sideMove[2];
+	fixed_t upMove[2];			// Vertical flying speed.
+} playerclass_t;
+
+#endif
---
+++ doomsday/Src/p_player.c
@@ -0,0 +1,41 @@
+/* DE1: $Id$
+ * Copyright (C) 2003 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * p_player.c: Players and Player Classes
+ */
+
+// HEADER FILES ------------------------------------------------------------
+
+// MACROS ------------------------------------------------------------------
+
+// TYPES -------------------------------------------------------------------
+
+// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
+
+// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+
+// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
+
+// EXTERNAL DATA DECLARATIONS ----------------------------------------------
+
+// PUBLIC DATA DEFINITIONS -------------------------------------------------
+
+// PRIVATE DATA DEFINITIONS ------------------------------------------------
+
+// CODE --------------------------------------------------------------------
+
