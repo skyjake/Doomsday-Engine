81e67919e2cd5042fd91cba697e0a0a8692852fc
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2004-01-25 21:38:53 +0000

    Moved sys_findfile.h to main Include dir

--- doomsday/Include/de_platform.h
+++ doomsday/Include/de_platform.h
@@ -30,6 +30,11 @@
 #include "dd_types.h"
 
 /*
+ * Wrappers for the Win32-style findfirst/next routines.
+ */
+#include "sys_findfile.h"
+
+/*
  * The Win32 Platform
  */
 #if defined(WIN32)
@@ -76,11 +81,6 @@ typedef unsigned int DWORD;
 #define DIR_WRONG_SEP_CHAR 	'\\'
 
 #include "Unix/sys_path.h"
-
-/*
- * Replacements for the Win32 findfirst/next routines.
- */
-#include "Unix/sys_findfile.h"
 
 #define _A_SUBDIR 	A_SUBDIR
 
