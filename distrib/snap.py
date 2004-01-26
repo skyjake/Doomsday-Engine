c7d9e268ebfd7ca55be207a4f4142c815a883343
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2004-01-26 14:55:21 +0000

    Updated for the 1.8 series

--- distrib/snap.py
+++ distrib/snap.py
@@ -12,6 +12,7 @@
 # -def			Include definition files
 # -all			Include ALL definitions files
 # -gfx			Include Data\Graphics\
+# -sdl			Include SDL libraries
 # -test			Don't upload
 
 import os, tempfile, sys, shutil, re
@@ -33,9 +34,11 @@ for sub in neededDirs:
 	os.mkdir( os.path.join( archDir, sub ) )
 
 files = [ (binDir + '/Doomsday.exe', 'Bin/Doomsday.exe'),
-          (binDir + '/jDoom.dll', 'Bin/jDoom.dll'),
-          (binDir + '/jHeretic.dll', 'Bin/jHeretic.dll'),
-          (binDir + '/jHexen.dll', 'Bin/jHexen.dll') ]
+          (binDir + '/jDoom.dll', 'Bin/jDoom.dll')
+          #,
+          #(binDir + '/jHeretic.dll', 'Bin/jHeretic.dll'),
+          #(binDir + '/jHexen.dll', 'Bin/jHexen.dll') 
+          ]
 
 # Select the appropriate files.
 if not '-nodll' in sys.argv:
@@ -79,11 +82,17 @@ if '-gfx' in sys.argv:
 	for file in os.listdir( os.path.join( baseDir, 'Data/Graphics' ) ):
 		loc = os.path.join( 'Data/Graphics', file )
 		files.append( (loc, loc) )
+		
+# SDL?
+if '-sdl' in sys.argv:
+	for file in os.listdir( os.path.join( baseDir, 'DLLs' ) ):
+		if re.match( "(?i)^sdl.*\.dll$", file ):
+			files.append( (os.path.join( 'DLLs', file ), 
+						   os.path.join( 'Bin', file )) )
 
 # Copy the files.
 for src, dest in files:
 	try:
-		print os.path.join( baseDir, src )
 		shutil.copyfile( os.path.join( baseDir, src ),
 		                 os.path.join( archDir, dest ) )
 	except IOError, (errno, strerror):
@@ -91,16 +100,31 @@ for src, dest in files:
 		print "  Skipping " + os.path.join( baseDir, src ) + "..."
 
 # Make the archive.
-outFile = os.path.join( outDir, 'ddsnapshot.rar' )
+outFile = os.path.join( outDir, 'deng-snap-1.8.alpha-1.exe' )
 try:
 	os.remove( outFile )
 except:
 	pass
-os.system( "rar -r -ep1 -m5 a %s %s\\*" % (outFile, archDir) )
+os.system( "rar -sfx -r -ep1 -m5 a %s %s\\*" % (outFile, archDir) )
+
+# Generate Self-Extracting RAR comment.
+commentFile = os.path.join( archDir, '_comment.txt' )
+f = open( commentFile, 'w' )
+f.write( "Title=Doomsday Engine Binary Snapshot\n" )
+f.write( "Overwrite=1\n" )
+f.write( "Text\n{\n" )
+for line in open( 'RelNotes.html' ).readlines():
+	f.write( line )
+f.write( "\n}\n" )
+f.close()
+
+# Add comment to the the EXE.
+os.system( "rar c -z%s %s" % (commentFile, outFile) )
 
 print "Removing temporary directory: " + archDir
 shutil.rmtree( archDir )
 
+"""
 if not '-test' in sys.argv:
 	# Upload.
 	print "Uploading to the Mirror..."
@@ -109,5 +133,6 @@ if not '-test' in sys.argv:
 	os.system( "ftpscrpt -f z:\scripts\snapshot.scp" )
 else:
 	print "--- THIS WAS JUST A TEST ---"
+"""	
 
 print "Done!"
