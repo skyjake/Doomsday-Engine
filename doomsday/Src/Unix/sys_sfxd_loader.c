2ba43566dca13d54bbf2fc33e39b0a77c1d87800
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2004-05-31 15:17:03 +0000

    Added the year 2004 to the copyright notice

--- doomsday/Src/Unix/sys_sfxd_loader.c
+++ doomsday/Src/Unix/sys_sfxd_loader.c
@@ -1,14 +1,26 @@
+/* DE1: $Id$
+ * Copyright (C) 2003, 2004 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
 
-//**************************************************************************
-//**
-//** SYS_SFXD_LOADER.C
-//**
-//** Loader for ds*.so
-//**
-//** Probably won't be needed because the OpenAL sound code can be
-//** staticly linked.
-//**
-//**************************************************************************
+/*
+ * sys_sfxd_loader.c: Loader for ds*.so
+ *
+ * Probably won't be needed because the OpenAL sound code can be
+ * staticly linked.
+ */
 
 // HEADER FILES ------------------------------------------------------------
 
