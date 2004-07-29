a7dd1f4e8d8736cb7db9ee5105d4708314721d29
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2004-07-29 19:41:32 +0000

    Fixed the banner

--- doomsday/Src/dd_input.c
+++ doomsday/Src/dd_input.c
@@ -1,11 +1,25 @@
+/* DE1: $Id$
+ * Copyright (C) 2003, 2004 Jaakko Ker�en <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ *
+ * Originally based on Hexen by Raven Software.
+ */
 
-//**************************************************************************
-//**
-//** DD_INPUT.C
-//**
-//** Base-level, system independent input code.
-//**
-//**************************************************************************
+/*
+ * dd_input.c: System Independent Input
+ */
 
 // HEADER FILES ------------------------------------------------------------
 
