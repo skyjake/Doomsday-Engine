ea535dcb32657205ec96f08fe3de7146ff0d80e5
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-04-09 21:36:50 +0000

    Initial checkin

---
+++ doomsday/Include/Unix/sys_dylib.h
@@ -0,0 +1,43 @@
+/* DE1: $Id$
+ * Copyright (C) 2004 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * sys_dylib.h: Dynamic Libraries
+ *
+ * These functions provide roughly the same functionality as the ltdl 
+ * library.  Since the ltdl library appears to be broken on Mac OS X, 
+ * these will be used instead when loading plugin libraries.
+ */
+
+#ifndef __DOOMSDAY_SYSTEM_DYNAMIC_LIBRARY_H__
+#define __DOOMSDAY_SYSTEM_DYNAMIC_LIBRARY_H__
+
+typedef void* lt_dlhandle;
+typedef void* lt_ptr;
+
+void        lt_dlinit(void);
+void        lt_dlexit(void);
+const char *lt_dlerror(void);
+void        lt_dladdsearchdir(const char *searchPath);
+int         lt_dlforeachfile(const char *searchPath, 
+                             int (*func) (const char *fileName, lt_ptr data), 
+							 lt_ptr data);
+lt_dlhandle lt_dlopenext(const char *baseFileName);
+void       *lt_dlsym(lt_dlhandle module, const char *symbolName);
+void        lt_dlclose(lt_dlhandle module);
+
+#endif
