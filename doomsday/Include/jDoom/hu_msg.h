c3cbd92b1cee3be13bf8d62d57836235441bc4e3
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-05-29 18:31:24 +0000

    Removed as obsolete

--- doomsday/Include/jDoom/hu_msg.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HUD_MESSAGES_H__
-#define __HUD_MESSAGES_H__
-
-void            HUMsg_Message(char *msg);
-void            HUMsg_Drawer(void);
-void            HUMsg_Ticker(void);
-
-#endif
