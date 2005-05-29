d6cb6d197b2768feef68e3e43ddfd42e41f2e899
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-05-29 07:51:27 +0000

    Commonised HUD widget code. Status bar and inventory implemented as widgets.

---
+++ doomsday/Include/jHexen/st_stuff.h
@@ -0,0 +1,34 @@
+// Called by main loop.
+boolean         ST_Responder(event_t *ev);
+
+// Called by main loop.
+void            ST_Ticker(void);
+
+// Called by main loop.
+void            ST_Drawer(int fullscreenmode, boolean refresh);
+
+// Called when the console player is spawned on each level.
+void            ST_Start(void);
+
+void            ST_Stop(void);
+
+// Called by startup code.
+void            ST_Init(void);
+
+void            ST_updateGraphics(void);
+
+// Called in P_inter & P_enemy
+void    	ST_doPaletteStuff(boolean forceChange);
+
+// States for status bar code.
+typedef enum {
+	AutomapState,
+	FirstPersonState
+} st_stateenum_t;
+
+// States for the chat code.
+typedef enum {
+	StartChatState,
+	WaitDestState,
+	GetChatState
+} st_chatstateenum_t;
