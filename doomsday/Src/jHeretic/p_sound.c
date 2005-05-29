ed1ee02131560307baefc489f46d8fc20c8ea1b8
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-05-29 15:54:51 +0000

    Game Status Cvar for map music.

--- doomsday/Src/jHeretic/p_sound.c
+++ doomsday/Src/jHeretic/p_sound.c
@@ -21,33 +21,40 @@
 
 // EXTERNAL DATA DECLARATIONS ----------------------------------------------
 
+extern int gsvMapMusic;
+
 // PUBLIC DATA DEFINITIONS -------------------------------------------------
 
 // PRIVATE DATA DEFINITIONS ------------------------------------------------
 
 // CODE --------------------------------------------------------------------
 
-//===========================================================================
-// S_LevelMusic
-//  Start the song for the current map.
-//===========================================================================
+/*
+ * Start the song for the current map.
+ */
 void S_LevelMusic(void)
 {
-	ddmapinfo_t info;
-	char    id[10];
+    ddmapinfo_t info;
+    char    id[10];
+    int    songid = 0;
 
-	if(gamestate != GS_LEVEL)
-		return;
+    if(gamestate != GS_LEVEL)
+        return;
 
-	sprintf(id, "E%iM%i", gameepisode, gamemap);
-	if(Def_Get(DD_DEF_MAP_INFO, id, &info) && info.music >= 0)
-	{
-		S_StartMusicNum(info.music, true);
-	}
-	else
-	{
-		S_StartMusicNum((gameepisode - 1) * 9 + gamemap - 1, true);
-	}
+    sprintf(id, "E%iM%i", gameepisode, gamemap);
+    if(Def_Get(DD_DEF_MAP_INFO, id, &info) && info.music >= 0)
+    {
+		songid = info.music;
+        S_StartMusicNum(songid, true);
+    }
+    else
+    {
+		songid = (gameepisode - 1) * 9 + gamemap - 1;
+        S_StartMusicNum(songid, true);
+    }
+
+    // set the map music game status cvar
+    gsvMapMusic = songid;
 }
 
 //===========================================================================
