1cc63f24094a7e9db233a22747c488bfe24ac57b
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-05-29 07:43:00 +0000

    Commonised menu code.

--- doomsday/Include/jHexen/mn_def.h
+++ doomsday/Include/jHexen/mn_def.h
@@ -3,6 +3,15 @@
 #ifndef __MENU_DEFS_H_
 #define __MENU_DEFS_H_
 
+#include "jHexen/h2def.h"
+
+// A combination of patch data and its lump number.
+typedef struct {
+	int             width, height;
+	int             leftoffset, topoffset;
+	int             lump;
+} dpatch_t;
+
 // Macros
 
 #define LEFT_DIR 0
@@ -12,6 +21,19 @@
 #define SELECTOR_YOFFSET (-1)
 #define SLOTTEXTLEN	16
 #define ASCII_CURSOR '_'
+
+#define LINEHEIGHT 20
+#define LINEHEIGHT_A 10
+#define LINEHEIGHT_B 20
+
+#define SKULLXOFF		-32
+#define SKULLYOFF		6
+#define SKULLW			22
+#define SKULLH			15
+#define CURSORPREF	"M_SLCTR%d"
+#define SKULLBASELMP	"FBULA0"
+
+#define NUMSAVESLOTS	6
 
 // Types
 
@@ -30,34 +52,27 @@ typedef enum {
 	MENU_OPTIONS,
 	MENU_OPTIONS2,
 	MENU_GAMEPLAY,
-	MENU_GRAPHICS,
-	MENU_EFFECTS,
-	MENU_RESOLUTION,
+	MENU_HUD,
+	MENU_MAP,
 	MENU_CONTROLS,
-	MENU_MOUSEOPTS,
-	MENU_JOYCONFIG,
+	MENU_MOUSE,
+	MENU_JOYSTICK,
 	MENU_FILES,
 	MENU_LOAD,
 	MENU_SAVE,
 	MENU_MULTIPLAYER,
-	MENU_PROTOCOL,
-	MENU_HOSTGAME,
-	MENU_JOINGAME,
 	MENU_GAMESETUP,
 	MENU_PLAYERSETUP,
-	MENU_NETGAME,
-	MENU_TCPIP,
-	MENU_SERIAL,
-	MENU_MODEM,
 	MENU_NONE
 } MenuType_t;
 
 typedef struct {
 	ItemType_t      type;
-	char           *text;
-	void            (*func) (int option);
+	char            *text;
+	void            (*func) (int option, void *data);
 	int             option;
-	MenuType_t      menu;
+	char            *lumpname;
+	void		*data;
 } MenuItem_t;
 
 typedef struct {
@@ -66,29 +81,28 @@ typedef struct {
 	void            (*drawFunc) (void);
 	int             itemCount;
 	MenuItem_t     *items;
-	int             oldItPos;
+	int             lastOn;
 	MenuType_t      prevMenu;
-	// Enhancements. -jk
-	void            (*textDrawer) (char *, int, int);
+	dpatch_t	*font;		// Font for menu items.
+	float		*color;		// their color.
 	int             itemHeight;
-	// For scrolling menus.
+	// For multipage menus.
 	int             firstItem, numVisItems;
-	float           offset;		   // To y.
 } Menu_t;
 
 extern int      MenuTime;
 extern boolean  shiftdown;
-extern Menu_t  *CurrentMenu;
-extern int      CurrentItPos;
+extern Menu_t  *currentMenu;
+extern short      itemOn;
 
 void            SetMenu(MenuType_t menu);
 
+extern Menu_t   MapDef;
+
+extern Menu_t   ControlsDef;
+
 // Multiplayer menus.
 extern Menu_t   MultiplayerMenu;
-
-//extern Menu_t ProtocolMenu;
-//extern Menu_t HostMenu;
-//extern Menu_t JoinMenu;
 extern Menu_t   GameSetupMenu;
 extern Menu_t   PlayerSetupMenu;
 
@@ -97,12 +111,45 @@ extern Menu_t   PlayerSetupMenu;
 //extern Menu_t SerialMenu;
 //extern Menu_t ModemMenu;
 
-void            MN_DrawMenuText(Menu_t * menu, int index, char *text);
+void		M_DrawSaveLoadBorder(int x, int y);
+void            M_WriteMenuText(Menu_t * menu, int index, char *text);
+int             M_StringWidth(char *string,  dpatch_t  *font);
+int     	M_StringHeight(char *string, dpatch_t *font);
 
-boolean         SCEnterMultiplayerMenu(int option);
+void            M_StartMessage(char *string, void *routine, boolean input);
+
+void		M_WriteText(int x, int y, char *string);
+void            M_WriteText2(int x, int y, char * string, dpatch_t *font,
+							 float red, float green, float blue, float alpha);
+void            M_WriteText3(int x, int y, const char *string, dpatch_t *font,
+							 float red, float green, float blue, float alpha,
+							 boolean doTypeIn, int initialCount);
+
+// Color widget.
+void    DrawColorWidget();
+void	SCColorWidget(int index, void *data);
+void    M_WGCurrentColor(int option, void *data);
+
+void    M_SetupNextMenu(Menu_t * menudef);
+void    M_DrawTitle(char *text, int y);
+void    M_DrawSlider(Menu_t * menu, int index, int width, int dot);
+void 	M_DrawColorBox(Menu_t * menu, int index, float r, float g, float b, float a);
+void    M_ClearMenus(void);
+void 	M_FloatMod10(float *variable, int option);
+
+void         SCEnterMultiplayerMenu(int option, void *data);
 void            MN_TickerEx(void); // The extended ticker.
+
+// Called by Init
+// registers all the CCmds and CVars for the menu
+void 		MN_Register(void);
+
+// Widget routines.
+boolean       	Cl_Responder(event_t *event); 	// Handles control in a menu widget
 
 // Edit field routines.
 boolean         Ed_Responder(event_t *event);
+
+static char *yesno[2] = { "NO", "YES" };
 
 #endif							// __MENU_DEFS_H_
