97c54964566260fb586b5693348ff1e12b5858fa
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-05-29 07:41:29 +0000

    Added textdefs for automap messages and XG class names. Updated style (uses spaces rather than tabs).

--- doomsday/Include/jHexen/dstrings.h
+++ doomsday/Include/jHexen/dstrings.h
@@ -145,8 +145,16 @@
 
 // AM_map.c ----------------------------------------------------------------
 
-#define AMSTR_FOLLOWON		"FOLLOW MODE ON"
-#define AMSTR_FOLLOWOFF		"FOLLOW MODE OFF"
+#define AMSTR_FOLLOWON "FOLLOW MODE ON"
+#define AMSTR_FOLLOWOFF "FOLLOW MODE OFF"
+#define AMSTR_ROTATEON "Rotate Mode ON"
+#define AMSTR_ROTATEOFF "Rotate Mode OFF"
+
+#define AMSTR_GRIDON "Grid ON"
+#define AMSTR_GRIDOFF "Grid OFF"
+
+#define AMSTR_MARKEDSPOT "Marked Spot"
+#define AMSTR_MARKSCLEARED "All Marks Cleared"
 
 // F_finale.c --------------------------------------------------------------
 
