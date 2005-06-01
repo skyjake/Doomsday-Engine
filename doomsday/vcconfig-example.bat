bf4bdeb1ae189b5ccf6736d3f5b142262ab3d428
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-06-01 17:03:10 +0000

    Added separate batch file for build configuration, so that vcbuild.bat contains only generic stuff

---
+++ doomsday/vcconfig-example.bat
@@ -0,0 +1,46 @@
+REM Build configuration (example).
+REM Modify and save as "vcconfig.bat".
+
+SET BIN_DIR=Bin\Release
+SET OBJ_DIR=Obj\Release
+
+REM -=- Requirements for Doomsday.exe -=-
+REM
+REM ---- Platform SDK:
+SET PLATFORM_INC=D:\VS.NET\Vc7\PlatformSDK\Include
+SET PLATFORM_LIB=D:\VS.NET\VC7\PlatformSDK\Lib
+SET LIBCI_LIB=D:\VS.NET\Vc7\Lib
+
+REM ---- DirectX:
+SET DX_INC=D:\sdk\dx8\include
+SET DX_LIB=D:\sdk\dx8\lib
+
+REM ---- FMOD:
+SET FMOD_INC=D:\sdk\fmod\api\inc
+SET FMOD_LIB=D:\sdk\fmod\api\lib
+
+REM ---- Creative Labs EAX:
+SET EAX_INC=D:\sdk\Creative Labs\EAX 2.0 Extensions SDK\Include
+SET EAX_LIB=D:\sdk\Creative Labs\EAX 2.0 Extensions SDK\Libs
+
+REM ---- SDL:
+SET SDL_INC=D:\sdk\SDL-1.2.6\include
+SET SDL_LIB=D:\sdk\SDL-1.2.6\lib
+
+REM ---- SDL_net:
+SET SDLNET_INC=D:\sdk\SDL_net-1.2.5\include
+SET SDLNET_LIB=D:\sdk\SDL_net-1.2.5\lib
+
+
+REM -=- Requirements for drOpenGL.dll -=-
+REM
+REM ---- OpenGL (GL/gl.h, GL/glext.h, GL/glu.h):
+SET GL_INC=D:\VS.NET\Vc7\Include
+
+
+REM -=- Requirements for dsA3D.dll -=-
+REM
+REM ---- Aureal A3D:
+SET A3D_INC=D:\sdk\Aureal\A3D 3.0 SDK\sdk\inc
+SET A3D_LIB=D:\sdk\Aureal\A3D 3.0 SDK\sdk\lib
+
