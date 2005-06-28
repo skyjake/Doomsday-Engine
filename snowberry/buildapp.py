4c654885f04f22ee1f4900d2d66c6c0890bf07c9
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-06-28 17:13:41 +0000

    Better build script

--- snowberry/buildapp.py
+++ snowberry/buildapp.py
@@ -9,11 +9,18 @@ ver = file('VERSION').read().strip()
 
 name = 'Doomsday Engine'
 iconfile = 'graphics/snowberry.icns'
-verstr = 'deng ' + ver
+verstr = ver + '-macosx'
 
 includes = []
 
-resources = []
+resources = [
+    'build/addons',
+    'build/conf',
+    'build/graphics',
+    'build/lang',
+    'build/plugins',
+    'build/profiles'
+]
 
 plist = dict(
     CFBundleIconFile           = iconfile,
@@ -33,38 +40,3 @@ opts = dict(py2app = dict(
     ))
     
 setup( app = ['snowberry.py'], data_files = resources, options = opts )
-
-
-
-
-#import bundlebuilder, os
-#
-#root = "/Users/jaakko/Projects/Snowberry"
-
-# Create the AppBuilder
-#app = bundlebuilder.AppBuilder(verbosity=1)
-
-#app.mainprogram = os.path.join(root, "snowberry.py")
-
-#app.standalone = 1
-#app.name = "Doomsday Engine"
-#app.iconfile = 'graphics/snowberry.icns'
-
-# includePackages forces inclusion of packages
-#app.includePackages.append("something")
-
-# Include resources into the package.
-#app.resources.append(os.path.join(root, "build/addons"))
-#app.resources.append(os.path.join(root, "build/graphics"))
-#app.resources.append(os.path.join(root, "build/conf"))
-#app.resources.append(os.path.join(root, "build/lang"))
-#app.resources.append(os.path.join(root, "build/profiles"))
-#app.resources.append(os.path.join(root, "build/plugins"))
-
-# Include the wxWidgets shared libraries.
-#app.libs.append("/usr/local/lib/wxPython-unicode-2.6.1.0/lib/libwx_macud-2.6.0.dylib")
-#app.libs.append("/usr/local/lib/wxPython-unicode-2.6.1.0/lib/libwx_macud-2.6.0.rsrc")
-
-# Build the app.
-#app.setup()
-#app.build()
