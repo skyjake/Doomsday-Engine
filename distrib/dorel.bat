ca95f3ca21cb13864161d8b5970809fe06f7e00a
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2005-06-29 20:36:39 +0000

    Updates for 1.9.0

--- distrib/dorel.bat
+++ distrib/dorel.bat
@@ -1,5 +1,18 @@
 REM -- Does a complete Release distribution.
 
+REM -- Rebuild Docs.
+cd ..\doomsday\Doc\Ame
+call make h t
+cd ..\..\..\distrib
+
+REM -- Recompile.
+cd ..\doomsday
+call d:\vs.net\vc7\bin\vcvars32.bat
+call vcbuild res
+call d:\vctk\vcvars32.bat
+call vcbuild all
+cd ..\distrib
+
 REM -- Run the Inno Setup Compiler.
 "C:\Program Files\Inno Setup 5\Compil32.exe" /cc setup.iss
 
