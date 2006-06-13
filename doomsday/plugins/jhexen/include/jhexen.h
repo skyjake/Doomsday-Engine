b408b36aad56df2ceb262cb0ae196a8f8935cbbf
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-06-13 18:34:29 +0000

    Added a master header file for jHexen. (Similar will soon be committed for jHeretic.)

---
+++ doomsday/plugins/jhexen/include/jhexen.h
@@ -0,0 +1,43 @@
+/* DE1: $Id: template.h 3311 2006-06-11 17:36:34Z skyjake $
+ * Copyright (C) 2006 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * jhexen.h: All jHexen Headers
+ *
+ * A convenience header for including all jHexen headers.
+ */
+
+#ifndef __JHEXEN_CONVENIENCE_H__
+#define __JHEXEN_CONVENIENCE_H__
+
+#include "../../jhexen/include/h2def.h"
+#include "../../jhexen/include/acfnlink.h"
+#include "../../jhexen/include/h2_actn.h"
+#include "../../jhexen/include/m_ctrl.h"
+#include "../../jhexen/include/mn_def.h"
+#include "../../jhexen/include/p_local.h"
+#include "../../jhexen/include/p_spec.h"
+#include "../../jhexen/include/r_local.h"
+#include "../../jhexen/include/sounds.h"
+#include "../../jhexen/include/soundst.h"
+#include "../../jhexen/include/st_stuff.h"
+#include "../../jhexen/include/textdefs.h"
+#include "../../jhexen/include/version.h"
+#include "../../jhexen/include/x_config.h"
+#include "../../jhexen/include/xddefs.h"
+
+#endif // __JHEXEN_CONVENIENCE_H__
