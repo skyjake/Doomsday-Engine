b24f85022b7ffe3f39bbfcf411a7ccb7dcf55f42
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-06-14 16:34:11 +0000

    Incremented to beta5.

--- doomsday/engine/portable/include/dd_version.h
+++ doomsday/engine/portable/include/dd_version.h
@@ -41,7 +41,7 @@
  * DOOMSDAY_VERSION unless you wish to break compatibility.
  */
 #define DOOMSDAY_VERSION		10801 // Don't touch; see above.
-#define DOOMSDAY_RELEASE_NAME	"0-beta4"
+#define DOOMSDAY_RELEASE_NAME	"0-beta5"
 #define DOOMSDAY_VERSION_TEXT	"1.9."DOOMSDAY_RELEASE_NAME
 
 #endif
