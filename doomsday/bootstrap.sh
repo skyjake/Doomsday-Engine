bdd5b8ed5d3b0b3f4e4bb2fcbfdd009723bdd821
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-06-14 20:01:42 +0000

    Begin autoconfiscation of deng

---
+++ doomsday/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Copyright © 2006:	Jamie Jones (Yagisan) <jamie_jones_au@yahoo.com.au>
+# Licensed under the GNU GPLv2 or later versions.
+
+test -w config.cache && rm config.cache
+test -w config.cross.cache && rm config.cross.cache
+libtoolize -c -f --ltdl --automake
+cp ./libltdl/install-sh .
+autoreconf -i
+echo " You are now ready to run \"cd ./build/unix; ../../configure && make \" "
