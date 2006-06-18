50b20492564e13656b00a7a11e672182a4e52696
skyjake <skyjake@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-06-18 07:22:18 +0000

    Moved obsolete frame packet routines to a separate source file.

--- doomsday/engine/portable/src/cl_frame.c
+++ doomsday/engine/portable/src/cl_frame.c
@@ -55,18 +55,15 @@ int     predicted_tics;
 
 // PRIVATE DATA DEFINITIONS ------------------------------------------------
 
-static int num_acks;
-byte    acks[MAX_ACKS];
-
 // The set history keeps track of received sets and is used to detect
 // duplicate frames.
-short   setHistory[SET_HISTORY_SIZE];
-int     historyIdx;
+static short    setHistory[SET_HISTORY_SIZE];
+static int      historyIdx;
 
 // The resend ID history keeps track of received resend deltas. Used
 // to detect duplicate resends.
-byte    resendHistory[RESEND_HISTORY_SIZE];
-int     resendHistoryIdx;
+static byte     resendHistory[RESEND_HISTORY_SIZE];
+static int      resendHistoryIdx;
 
 // CODE --------------------------------------------------------------------
 
@@ -307,67 +304,4 @@ void Cl_Frame2Received(int packetType)
 		}
 	}
 	Net_SendBuffer(0, 0);
-}
-
-/*
- * Read and ack a psv_frame delta set.
- *
- * THIS FUNCTION IS OBSOLETE (psv_frame is no longer used)
- */
-void Cl_ReadDeltaSet(void)
-{
-	byte    present = Msg_ReadByte();
-	byte    set = Msg_ReadByte();
-
-	// Add the set number to the list of acks.
-	if(num_acks < MAX_ACKS)
-		acks[num_acks++] = set;
-
-	if(present & BIT(DT_MOBJ))
-		while(Cl_ReadMobjDelta());
-	if(present & BIT(DT_PLAYER))
-		while(Cl_ReadPlayerDelta());
-	if(present & BIT(DT_LUMP))
-		while(Cl_ReadLumpDelta());
-	if(present & BIT(DT_SECTOR_SHORT_FLAGS))
-		while(Cl_ReadSectorDelta());
-	if(present & BIT(DT_SIDE))
-		while(Cl_ReadSideDelta());
-	if(present & BIT(DT_POLY))
-		while(Cl_ReadPolyDelta());
-}
-
-/*
- * Reads a psv_frame packet from the message buffer and applies the deltas
- * in it. Also acks the sets in the packet immediately.
- *
- * THIS FUNCTION IS OBSOLETE (psv_frame is no longer used)
- */
-void Cl_FrameReceived(void)
-{
-	int     i, frametime;
-
-	gotframe = true;
-	gotFirstFrame = true;
-
-#if _DEBUG
-	if(!gameReady)
-		Con_Message("Got frame but GAME NOT READY!\n");
-#endif
-
-	// Frame time, lowest byte of gametic.
-	frametime = Msg_ReadByte();
-
-	num_acks = 0;
-	while(!Msg_End())
-		Cl_ReadDeltaSet();
-
-	// Acknowledge all sets.
-	Msg_Begin(pcl_ack_sets);
-	for(i = 0; i < num_acks; i++)
-		Msg_WriteByte(acks[i]);
-	Net_SendBuffer(0, 0);
-
-	// Reset the predict counter.
-	predicted_tics = 0;
 }
---
+++ doomsday/engine/portable/src/cl_oldframe.c
@@ -0,0 +1,123 @@
+/* DE1: $Id$
+ * Copyright (C) 2003, 2004 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * cl_frame.c: Obsolete Frame Reception
+ *
+ * This file contains obsolete delta routines. They are preserved so that
+ * backwards compatibility is retained with older versions of the network
+ * protocol. 
+ *
+ * These routines should be considered FROZEN. Do not change them.
+ */
+
+// HEADER FILES ------------------------------------------------------------
+
+#include "de_base.h"
+#include "de_console.h"
+#include "de_network.h"
+
+// MACROS ------------------------------------------------------------------
+
+#define BIT(x)				(1 << (x))
+#define MAX_ACKS			40
+
+// TYPES -------------------------------------------------------------------
+
+// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
+
+// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+
+// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
+
+// EXTERNAL DATA DECLARATIONS ----------------------------------------------
+
+extern int      gotframe;
+extern int      predicted_tics;
+extern boolean  gotFirstFrame;
+
+// PUBLIC DATA DEFINITIONS -------------------------------------------------
+
+// PRIVATE DATA DEFINITIONS ------------------------------------------------
+
+static int num_acks;
+static byte acks[MAX_ACKS];
+
+// CODE --------------------------------------------------------------------
+
+/*
+ * Read and ack a psv_frame delta set.
+ *
+ * THIS FUNCTION IS OBSOLETE (psv_frame is no longer used)
+ */
+void Cl_ReadDeltaSet(void)
+{
+	byte    present = Msg_ReadByte();
+	byte    set = Msg_ReadByte();
+
+	// Add the set number to the list of acks.
+	if(num_acks < MAX_ACKS)
+		acks[num_acks++] = set;
+
+	if(present & BIT(DT_MOBJ))
+		while(Cl_ReadMobjDelta());
+	if(present & BIT(DT_PLAYER))
+		while(Cl_ReadPlayerDelta());
+	if(present & BIT(DT_LUMP))
+		while(Cl_ReadLumpDelta());
+	if(present & BIT(DT_SECTOR_SHORT_FLAGS))
+		while(Cl_ReadSectorDelta());
+	if(present & BIT(DT_SIDE))
+		while(Cl_ReadSideDelta());
+	if(present & BIT(DT_POLY))
+		while(Cl_ReadPolyDelta());
+}
+
+/*
+ * Reads a psv_frame packet from the message buffer and applies the deltas
+ * in it. Also acks the sets in the packet immediately.
+ *
+ * THIS FUNCTION IS OBSOLETE (psv_frame is no longer used)
+ */
+void Cl_FrameReceived(void)
+{
+	int     i, frametime;
+
+	gotframe = true;
+	gotFirstFrame = true;
+
+#if _DEBUG
+	if(!gameReady)
+		Con_Message("Got frame but GAME NOT READY!\n");
+#endif
+
+	// Frame time, lowest byte of gametic.
+	frametime = Msg_ReadByte();
+
+	num_acks = 0;
+	while(!Msg_End())
+		Cl_ReadDeltaSet();
+
+	// Acknowledge all sets.
+	Msg_Begin(pcl_ack_sets);
+	for(i = 0; i < num_acks; i++)
+		Msg_WriteByte(acks[i]);
+	Net_SendBuffer(0, 0);
+
+	// Reset the predict counter.
+	predicted_tics = 0;
+}
