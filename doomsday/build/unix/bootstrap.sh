a14e4f498469c448e590331b8bfb385b63a46ff9
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-07-02 07:28:45 +0000

    Toss out autotools. Possible better cmake based replacement being test implemented

--- doomsday/build/unix/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copyright © 2006:	Jamie Jones (Yagisan) <jamie_jones_au@yahoo.com.au>
-# Licensed under the GNU GPLv2 or later versions.
-
-test -w config.cache && rm config.cache
-test -w config.cross.cache && rm config.cross.cache
-libtoolize -c --ltdl --automake
-autoreconf -i -f
-echo " You are now ready to run \"./configure && make \" "
