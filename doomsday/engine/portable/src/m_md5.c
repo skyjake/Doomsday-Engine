a89f51709ce76a774f08a730022212c1b1755591
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-08-08 21:54:35 +0000

    temporary tweak to appease vc.

--- doomsday/engine/portable/src/m_md5.c
+++ doomsday/engine/portable/src/m_md5.c
@@ -61,12 +61,12 @@ typedef byte u8;
 static void md5_transform(u32 *hash, u32 const *in)
 {
     u32 a, b, c, d;
-    
+
     a = hash[0];
     b = hash[1];
     c = hash[2];
     d = hash[3];
-    
+
     MD5STEP(F1, a, b, c, d, in[0] + 0xd76aa478, 7);
     MD5STEP(F1, d, a, b, c, in[1] + 0xe8c7b756, 12);
     MD5STEP(F1, c, d, a, b, in[2] + 0x242070db, 17);
@@ -83,7 +83,7 @@ static void md5_transform(u32 *hash, u32 const *in)
     MD5STEP(F1, d, a, b, c, in[13] + 0xfd987193, 12);
     MD5STEP(F1, c, d, a, b, in[14] + 0xa679438e, 17);
     MD5STEP(F1, b, c, d, a, in[15] + 0x49b40821, 22);
-    
+
     MD5STEP(F2, a, b, c, d, in[1] + 0xf61e2562, 5);
     MD5STEP(F2, d, a, b, c, in[6] + 0xc040b340, 9);
     MD5STEP(F2, c, d, a, b, in[11] + 0x265e5a51, 14);
@@ -100,7 +100,7 @@ static void md5_transform(u32 *hash, u32 const *in)
     MD5STEP(F2, d, a, b, c, in[2] + 0xfcefa3f8, 9);
     MD5STEP(F2, c, d, a, b, in[7] + 0x676f02d9, 14);
     MD5STEP(F2, b, c, d, a, in[12] + 0x8d2a4c8a, 20);
-    
+
     MD5STEP(F3, a, b, c, d, in[5] + 0xfffa3942, 4);
     MD5STEP(F3, d, a, b, c, in[8] + 0x8771f681, 11);
     MD5STEP(F3, c, d, a, b, in[11] + 0x6d9d6122, 16);
@@ -117,7 +117,7 @@ static void md5_transform(u32 *hash, u32 const *in)
     MD5STEP(F3, d, a, b, c, in[12] + 0xe6db99e5, 11);
     MD5STEP(F3, c, d, a, b, in[15] + 0x1fa27cf8, 16);
     MD5STEP(F3, b, c, d, a, in[2] + 0xc4ac5665, 23);
-    
+
     MD5STEP(F4, a, b, c, d, in[0] + 0xf4292244, 6);
     MD5STEP(F4, d, a, b, c, in[7] + 0x432aff97, 10);
     MD5STEP(F4, c, d, a, b, in[14] + 0xab9423a7, 15);
@@ -134,14 +134,20 @@ static void md5_transform(u32 *hash, u32 const *in)
     MD5STEP(F4, d, a, b, c, in[11] + 0xbd3af235, 10);
     MD5STEP(F4, c, d, a, b, in[2] + 0x2ad7d2bb, 15);
     MD5STEP(F4, b, c, d, a, in[9] + 0xeb86d391, 21);
-    
+
     hash[0] += a;
     hash[1] += b;
     hash[2] += c;
     hash[3] += d;
 }
 
-static inline void le32_to_cpu_array(u32 *buf, unsigned int words)
+#if WIN32
+#define INLINE
+#else
+#define INLINE inline
+#endif
+
+static INLINE void le32_to_cpu_array(u32 *buf, unsigned int words)
 {
     while (words--) {
         *buf = ULONG(*buf);
@@ -149,7 +155,7 @@ static inline void le32_to_cpu_array(u32 *buf, unsigned int words)
     }
 }
 
-static inline void cpu_to_le32_array(u32 *buf, unsigned int words)
+static INLINE void cpu_to_le32_array(u32 *buf, unsigned int words)
 {
     while (words--) {
         *buf = ULONG(*buf);
@@ -157,7 +163,7 @@ static inline void cpu_to_le32_array(u32 *buf, unsigned int words)
     }
 }
 
-static inline void md5_transform_helper(struct md5_ctx *ctx)
+static INLINE void md5_transform_helper(struct md5_ctx *ctx)
 {
     le32_to_cpu_array(ctx->block, sizeof(ctx->block) / sizeof(u32));
     md5_transform(ctx->hash, ctx->block);
@@ -166,7 +172,7 @@ static inline void md5_transform_helper(struct md5_ctx *ctx)
 void md5_init(void *ctx)
 {
     struct md5_ctx *mctx = ctx;
-    
+
     mctx->hash[0] = 0x67452301;
     mctx->hash[1] = 0xefcdab89;
     mctx->hash[2] = 0x98badcfe;
@@ -178,29 +184,29 @@ void md5_update(void *ctx, const u8 *data, unsigned int len)
 {
     struct md5_ctx *mctx = ctx;
     const u32 avail = sizeof(mctx->block) - (mctx->byte_count & 0x3f);
-    
+
     mctx->byte_count += len;
-    
+
     if (avail > len) {
         memcpy((char *)mctx->block + (sizeof(mctx->block) - avail),
                data, len);
         return;
     }
-    
+
     memcpy((char *)mctx->block + (sizeof(mctx->block) - avail),
            data, avail);
-    
+
     md5_transform_helper(mctx);
     data += avail;
     len -= avail;
-    
+
     while (len >= sizeof(mctx->block)) {
         memcpy(mctx->block, data, sizeof(mctx->block));
         md5_transform_helper(mctx);
         data += sizeof(mctx->block);
         len -= sizeof(mctx->block);
     }
-    
+
     memcpy(mctx->block, data, len);
 }
 
@@ -210,7 +216,7 @@ void md5_final(void *ctx, u8 *out)
     const unsigned int offset = mctx->byte_count & 0x3f;
     char *p = (char *)mctx->block + offset;
     int padding = 56 - (offset + 1);
-    
+
     *p++ = 0x80;
     if (padding < 0) {
         memset(p, 0x00, padding + 8);
@@ -218,7 +224,7 @@ void md5_final(void *ctx, u8 *out)
         p = (char *)mctx->block;
         padding = 56;
     }
-    
+
     memset(p, 0, padding);
     mctx->block[14] = mctx->byte_count << 3;
     mctx->block[15] = mctx->byte_count >> 29;
