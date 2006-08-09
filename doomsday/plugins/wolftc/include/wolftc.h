20e1e491e78e64f427636a1f6e5af4fa957de718
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-08-09 15:16:17 +0000

    Added missing headers.

---
+++ doomsday/plugins/wolftc/include/wolftc.h
@@ -0,0 +1,52 @@
+/* DE1: $Id: template.h 3311 2006-06-11 17:36:34Z skyjake $
+ * Copyright (C) 2006 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not: http://www.opensource.org/
+ */
+
+/*
+ * wolftc.h: All WolfTC Headers
+ *
+ * A convenience header for including all WolfTC headers.
+ */
+
+#ifndef __WOLFTC_CONVENIENCE_H__
+#define __WOLFTC_CONVENIENCE_H__
+
+#include "../../wolftc/include/acfnlink.h"
+#include "../../wolftc/include/d_action.h"
+#include "../../wolftc/include/doomdef.h"
+#include "../../wolftc/include/dstrings.h"
+#include "../../wolftc/include/g_game.h"
+#include "../../wolftc/include/d_config.h"
+#include "../../wolftc/include/d_event.h"
+#include "../../wolftc/include/doomstat.h"
+#include "../../wolftc/include/doomtype.h"
+#include "../../wolftc/include/info.h"
+#include "../../wolftc/include/m_cheat.h"
+#include "../../wolftc/include/m_menu.h"
+#include "../../wolftc/include/mn_def.h"
+#include "../../wolftc/include/m_ctrl.h"
+#include "../../wolftc/include/p_inter.h"
+#include "../../wolftc/include/p_local.h"
+#include "../../wolftc/include/p_oldsvg.h"
+#include "../../wolftc/include/p_pspr.h"
+#include "../../wolftc/include/p_setup.h"
+#include "../../wolftc/include/r_defs.h"
+#include "../../wolftc/include/s_sound.h"
+#include "../../wolftc/include/st_stuff.h"
+#include "../../wolftc/include/tables.h"
+#include "../../wolftc/include/wi_stuff.h"
+
+#endif // __WOLFTC_CONVENIENCE_H__
