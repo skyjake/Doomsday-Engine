105b6ee349d04e0d2e4267b1f401cf33523a3b22
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-04 12:02:26 +0000

    Ensure license text is acurate and updated

--- doomsday/plugins/jdoom/src/g_ctrl.c
+++ doomsday/plugins/jdoom/src/g_ctrl.c
@@ -1,11 +1,11 @@
-/* DE1: $Id: template.c 2645 2006-01-21 12:58:39Z skyjake $
- * Copyright (C) 2006 Jaakko Keränen <jaakko.keranen@iki.fi>
- *                    Daniel Swanson <danij@users.sourceforge.net>
+/**\file
+ * Copyright © 2006 Jaakko Keränen <skyjake@dengine.net>
+ * Copyright © 2006 Daniel Swanson <danij@dengine.net>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,19 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA  02110-1301, USA.
- *
- * In addition, as a special exception, we, the authors of deng
- * give permission to link the code of our release of deng with
- * the libjhexen and/or the libjheretic libraries (or with modified
- * versions of it that use the same license as the libjhexen or
- * libjheretic libraries), and distribute the linked executables.
- * You must obey the GNU General Public License in all respects for
- * all of the code used other than “libjhexen or libjheretic”. If
- * you modify this file, you may extend this exception to your
- * version of the file, but you are not obligated to do so. If you
- * do not wish to do so, delete this exception statement from your version.
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
  */
 
 /*
