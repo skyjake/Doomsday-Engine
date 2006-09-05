25b07623cad381bff74f3afac25c5d7e1105c0e3
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-05 12:45:49 +0000

    Ensure license text is acurate and updated. Remove obsolete license text

--- doomsday/plugins/wolftc/include/tables.h
+++ doomsday/plugins/wolftc/include/tables.h
@@ -1,15 +1,21 @@
-/* $Id: tables.h 3305 2006-06-11 17:00:36Z skyjake $
+/**\file
+ * Copyright © 2003-2006 Jaakko Keränen <skyjake@dengine.net>
+ * Copyright © 1993-1996 by id Software, Inc.
  *
- * Copyright (C) 1993-1996 by id Software, Inc.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This source is available for distribution and/or modification
- * only under the terms of the DOOM Source Code License as
- * published by id Software. All rights reserved.
- *
- * The source is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
- * for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
  */
 
 /*
--- doomsday/plugins/wolftc/include/wolftc.h
+++ doomsday/plugins/wolftc/include/wolftc.h
@@ -1,5 +1,5 @@
-/* DE1: $Id: template.h 3311 2006-06-11 17:36:34Z skyjake $
- * Copyright (C) 2006 Jaakko Keränen <jaakko.keranen@iki.fi>
+/**\file
+ * Copyright © 2006 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,7 +12,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not: http://www.opensource.org/
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
  */
 
 /*
