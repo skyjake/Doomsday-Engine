9ca5cca18dc1a8b2f965e6418e56edbaf9573c03
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-06 15:55:17 +0000

    Ensure license text is acurate and updated

--- doomsday/plugins/jdoom/include/p_mobj.h
+++ doomsday/plugins/jdoom/include/p_mobj.h
@@ -19,20 +19,6 @@
  * Boston, MA  02110-1301  USA
  */
 
-/* $Id$
- *
- * Copyright (C) 1993-1996 by id Software, Inc.
- *
- * This source is available for distribution and/or modification
- * only under the terms of the DOOM Source Code License as
- * published by id Software. All rights reserved.
- *
- * The source is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
- * for more details.
- */
-
 /*
  * Map Objects, MObj, definition and handling.
  */
