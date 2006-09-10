f4c0b7f6e4de47ffb28efb95db7b3d1d59a5391c
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-10 07:22:44 +0000

    Copy/Paste error

--- doomsday/plugins/jheretic/include/h_type.h
+++ doomsday/plugins/jheretic/include/h_type.h
@@ -6,7 +6,6 @@
  *\author Copyright © 2003-2006 Jaakko Keränen <skyjake@dengine.net>
  *\author Copyright © 2005-2006 Daniel Swanson <danij@dengine.net>
  *\author Copyright © 2006 Jamie Jones <yagisan@dengine.net>
- *\author Copyright © 1993-1996 by id Software, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
