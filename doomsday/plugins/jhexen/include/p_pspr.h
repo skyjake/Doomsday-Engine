c0f2ff8ad1bb8efedb24c128af91689e0b58359b
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-15 15:22:47 +0000

    Ensure license text is correct and accurate

--- doomsday/plugins/jhexen/include/p_pspr.h
+++ doomsday/plugins/jhexen/include/p_pspr.h
@@ -1,15 +1,35 @@
-/* $Id: p_pspr.h 3305 2006-06-11 17:00:36Z skyjake $
+/**\file
+ *\section Copyright and License Summary
+ * License: GPL + jHeretic/jHexen Exception
+ * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
- * Copyright (C) 1993-1996 by id Software, Inc.
+ *\author Copyright © 2006 Daniel Swanson <danij@dengine.net>
  *
- * This source is available for distribution and/or modification
- * only under the terms of the DOOM Source Code License as
- * published by id Software. All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * The source is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
- * for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ *
+ * In addition, as a special exception, we, the authors of deng
+ * give permission to link the code of our release of deng with
+ * the libjhexen and/or the libjheretic libraries (or with modified
+ * versions of it that use the same license as the libjhexen or
+ * libjheretic libraries), and distribute the linked executables.
+ * You must obey the GNU General Public License in all respects for
+ * all of the code used other than “libjhexen or libjheretic”. If
+ * you modify this file, you may extend this exception to your
+ * version of the file, but you are not obligated to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
  */
 
 /*
