b5b3e9c1c6bf77a16dc87d9f6c89b815374ef6db
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-16 08:40:25 +0000

    Fixed bug which led to the last selected weapon's ammo icon being displayed when the current weapon requires no ammo at all.

--- doomsday/plugins/jheretic/src/st_stuff.c
+++ doomsday/plugins/jheretic/src/st_stuff.c
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  *
  * In addition, as a special exception, we, the authors of deng
@@ -528,6 +528,7 @@ void ST_updateWidgets(void)
     if(!found) // Weapon takes no ammo at all.
     {
         w_ready.num = &largeammo;
+        st_ammoicon = -1;
     }
 
     w_ready.data = plr->readyweapon;
