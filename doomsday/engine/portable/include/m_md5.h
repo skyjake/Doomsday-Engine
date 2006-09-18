ac94e4538bc26e12a53ca9da39f84c9f358ef658
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-18 19:37:52 +0000

    Ensure license text is correct and accurate

--- doomsday/engine/portable/include/m_md5.h
+++ doomsday/engine/portable/include/m_md5.h
@@ -1,19 +1,25 @@
-/* DE1: $Id: template.h 3311 2006-06-11 17:36:34Z skyjake $
-* Copyright (C) 2006 Jaakko Keränen <jaakko.keranen@iki.fi>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not: http://www.opensource.org/
-*/
+/**\file
+ *\section Copyright and License Summary
+ * License: GPL
+ * Online License Link: http://www.gnu.org/licenses/gpl.html
+ *
+ *\author Copyright © 2006 Jaakko Keränen <skyjake@dengine.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
 
 /*
  * m_md5.h: MD5 Hash Generator
