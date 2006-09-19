ce75db169f4d6f0143bf5534ad0685a3e7c91aed
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-09-19 16:54:46 +0000

    Ensure license text is correct and accurate

--- doomsday/engine/portable/src/tab_video.c
+++ doomsday/engine/portable/src/tab_video.c
@@ -1,20 +1,43 @@
-/* DE1: $Id$
- * Copyright (C) 2003, 2004 Jaakko Ker�nen <jaakko.keranen@iki.fi>
+/**\file
+ *\section Copyright and License Summary
+ * License: Raven
+ * Online License Link: http://www.dengine.net/raven_license/End_User_License_Hexen_Source_Code.html
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Copyright © 2003-2006 Jaakko Keränen <skyjake@dengine.net>
+ * Copyright © 1999 Activision
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is covered by the HERETIC / HEXEN (LIMITED USE) source
+ * code license; you can redistribute it and/or modify it under the terms
+ * of the HERETIC / HEXEN source code license as published by Activision.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not: http://www.opensource.org/
+ * THIS MATERIAL IS NOT MADE OR SUPPORTED BY ACTIVISION.
  *
- * From Hexen by Raven Software.
+ * WARRANTY INFORMATION.
+ * This program is provided as is. Activision and it's affiliates make no
+ * warranties of any kind, whether oral or written , express or implied,
+ * including any warranty of merchantability, fitness for a particular
+ * purpose or non-infringement, and no other representations or claims of
+ * any kind shall be binding on or obligate Activision or it's affiliates.
+ *
+ * LICENSE CONDITIONS.
+ * You shall not:
+ *
+ * 1) Exploit this Program or any of its parts commercially.
+ * 2) Use this Program, or permit use of this Program, on more than one
+ *    computer, computer terminal, or workstation at the same time.
+ * 3) Make copies of this Program or any part thereof, or make copies of
+ *    the materials accompanying this Program.
+ * 4) Use the program, or permit use of this Program, in a network,
+ *    multi-user arrangement or remote access arrangement, including any
+ *    online use, except as otherwise explicitly provided by this Program.
+ * 5) Sell, rent, lease or license any copies of this Program, without
+ *    the express prior written consent of Activision.
+ * 6) Remove, disable or circumvent any proprietary notices or labels
+ *    contained on or within the Program.
+ *
+ * You should have received a copy of the HERETIC / HEXEN source code
+ * license along with this program (Ravenlic.txt); if not:
+ * http://www.ravensoft.com/
  */
 
 /*
