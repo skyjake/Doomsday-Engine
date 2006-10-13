aa568ddd9515bd94c81f65eb9712949a1e754545
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2006-10-13 15:58:58 +0000

    Fixed typo.

--- doomsday/engine/portable/src/def_data.c
+++ doomsday/engine/portable/src/def_data.c
@@ -6,7 +6,7 @@
  *\author Copyright © 2003-2006 Jaakko Keränen <skyjake@dengine.net>
  *\author Copyright © 2006 Daniel Swanson <danij@dengine.net>
  *
- * This program is M_Free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
