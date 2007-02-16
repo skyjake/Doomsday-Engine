b4ddaa8ef1928926c39453c6978d094d7b2a4fbe
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-02-16 20:44:00 +0000

    Renamed p_player.c to avoid confusion in Xcode.

---
+++ doomsday/engine/portable/src/p_players.c
@@ -0,0 +1,74 @@
+/**\file
+ *\section Copyright and License Summary
+ * License: GPL
+ * Online License Link: http://www.gnu.org/licenses/gpl.html
+ *
+ *\author Copyright © 2003-2006 Jaakko Keränen <skyjake@dengine.net>
+ *\author Copyright © 2006 Daniel Swanson <danij@dengine.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+/*
+ * p_player.c: Players and Player Classes
+ */
+
+// HEADER FILES ------------------------------------------------------------
+
+#include "de_base.h"
+#include "de_network.h"
+
+// MACROS ------------------------------------------------------------------
+
+// TYPES -------------------------------------------------------------------
+
+// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
+
+// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+
+// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
+
+// EXTERNAL DATA DECLARATIONS ----------------------------------------------
+
+// PUBLIC DATA DEFINITIONS -------------------------------------------------
+
+// PRIVATE DATA DEFINITIONS ------------------------------------------------
+
+// CODE --------------------------------------------------------------------
+
+/**
+ * Determine which console is used by the given local player.  Local
+ * players are numbered starting from zero.
+ */
+int P_LocalToConsole(int localPlayer)
+{
+    int         i, count, n;
+
+    for(i = 0, count = 0; i < DDMAXPLAYERS; ++i)
+    {
+        // The numbering begins from the consoleplayer.
+        n = (i + consoleplayer) % DDMAXPLAYERS;
+
+        if(players[n].flags & DDPF_LOCAL)
+        {
+            if(count++ == localPlayer)
+                return n;
+        }
+    }
+
+    // No match!
+    return -1;
+}
