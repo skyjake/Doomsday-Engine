0db9b6d4ecd226b622754a8fc886541228369810
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-03-02 08:30:00 +0000

    Updated style.

--- doomsday/engine/portable/src/sys_sfxd_dummy.c
+++ doomsday/engine/portable/src/sys_sfxd_dummy.c
@@ -4,6 +4,7 @@
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
  *\author Copyright © 2003-2006 Jaakko Keränen <skyjake@dengine.net>
+ *\author Copyright © 2007 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,7 +24,7 @@
 
 /*
  * sys_sfxd_dummy.c: Dummy Sfx Driver
-
+ *
  * Used in dedicated server mode, when it's necessary to simulate 
  * sound playing but not actually play anything.
  */
@@ -47,15 +48,15 @@
 int     DS_DummyInit(void);
 void    DS_DummyShutdown(void);
 sfxbuffer_t *DS_DummyCreateBuffer(int flags, int bits, int rate);
-void    DS_DummyDestroyBuffer(sfxbuffer_t * buf);
-void    DS_DummyLoad(sfxbuffer_t * buf, struct sfxsample_s *sample);
-void    DS_DummyReset(sfxbuffer_t * buf);
-void    DS_DummyPlay(sfxbuffer_t * buf);
-void    DS_DummyStop(sfxbuffer_t * buf);
-void    DS_DummyRefresh(sfxbuffer_t * buf);
+void    DS_DummyDestroyBuffer(sfxbuffer_t *buf);
+void    DS_DummyLoad(sfxbuffer_t *buf, struct sfxsample_s *sample);
+void    DS_DummyReset(sfxbuffer_t *buf);
+void    DS_DummyPlay(sfxbuffer_t *buf);
+void    DS_DummyStop(sfxbuffer_t *buf);
+void    DS_DummyRefresh(sfxbuffer_t *buf);
 void    DS_DummyEvent(int type);
-void    DS_DummySet(sfxbuffer_t * buf, int property, float value);
-void    DS_DummySetv(sfxbuffer_t * buf, int property, float *values);
+void    DS_DummySet(sfxbuffer_t *buf, int property, float value);
+void    DS_DummySetv(sfxbuffer_t *buf, int property, float *values);
 void    DS_DummyListener(int property, float value);
 void    DS_DummyListenerv(int property, float *values);
 
@@ -88,44 +89,36 @@ static boolean inited;
 
 // CODE --------------------------------------------------------------------
 
-//===========================================================================
-// DS_DummyInit
-//  Init DirectSound, start playing the primary buffer. Returns true
-//  if successful.
-//===========================================================================
+/**
+ * Init DirectSound, start playing the primary buffer. Returns true
+ * if successful.
+ */
 int DS_DummyInit(void)
 {
 	if(inited)
-	{
-		// Already initialized.
-		return true;
-	}
+		return true; // Already initialized.
+
 	inited = true;
 	return true;
 }
 
-//===========================================================================
-// DS_DummyShutdown
-//  Shut everything down.
-//===========================================================================
+/**
+ * Shut everything down.
+ */
 void DS_DummyShutdown(void)
 {
 	inited = false;
 }
 
-//===========================================================================
-// DS_DummyEvent
-//  The Event function is called to tell the driver about certain critical
-//  events like the beginning and end of an update cycle.
-//===========================================================================
+/**
+ * The Event function is called to tell the driver about certain critical
+ * events like the beginning and end of an update cycle.
+ */
 void DS_DummyEvent(int type)
 {
 	// Do nothing...
 }
 
-//===========================================================================
-// DS_DummyCreateBuffer
-//===========================================================================
 sfxbuffer_t *DS_DummyCreateBuffer(int flags, int bits, int rate)
 {
 	sfxbuffer_t *buf;
@@ -140,22 +133,18 @@ sfxbuffer_t *DS_DummyCreateBuffer(int flags, int bits, int rate)
 	return buf;
 }
 
-//===========================================================================
-// DS_DummyDestroyBuffer
-//===========================================================================
-void DS_DummyDestroyBuffer(sfxbuffer_t * buf)
+void DS_DummyDestroyBuffer(sfxbuffer_t *buf)
 {
 	// Free the memory allocated for the buffer.
 	Z_Free(buf);
 }
 
-//===========================================================================
-// DS_DummyLoad
-//  Prepare the buffer for playing a sample by filling the buffer with 
-//  as much sample data as fits. The pointer to sample is saved, so
-//  the caller mustn't free it while the sample is loaded.
-//===========================================================================
-void DS_DummyLoad(sfxbuffer_t * buf, struct sfxsample_s *sample)
+/**
+ * Prepare the buffer for playing a sample by filling the buffer with 
+ * as much sample data as fits. The pointer to sample is saved, so
+ * the caller mustn't free it while the sample is loaded.
+ */
+void DS_DummyLoad(sfxbuffer_t *buf, struct sfxsample_s *sample)
 {
 	// Now the buffer is ready for playing.
 	buf->sample = sample;
@@ -163,30 +152,25 @@ void DS_DummyLoad(sfxbuffer_t * buf, struct sfxsample_s *sample)
 	buf->flags &= ~SFXBF_RELOAD;
 }
 
-//===========================================================================
-// DS_DummyReset
-//  Stops the buffer and makes it forget about its sample.
-//===========================================================================
-void DS_DummyReset(sfxbuffer_t * buf)
+/**
+ * Stops the buffer and makes it forget about its sample.
+ */
+void DS_DummyReset(sfxbuffer_t *buf)
 {
 	DS_DummyStop(buf);
 	buf->sample = NULL;
 	buf->flags &= ~SFXBF_RELOAD;
 }
 
-//===========================================================================
-// DS_DummyBufferLength
-//  Returns the length of the buffer in milliseconds.
-//===========================================================================
-unsigned int DS_DummyBufferLength(sfxbuffer_t * buf)
+/**
+ * @return          The length of the buffer in milliseconds.
+ */
+unsigned int DS_DummyBufferLength(sfxbuffer_t *buf)
 {
 	return 1000 * buf->sample->numsamples / buf->freq;
 }
 
-//===========================================================================
-// DS_DummyPlay
-//===========================================================================
-void DS_DummyPlay(sfxbuffer_t * buf)
+void DS_DummyPlay(sfxbuffer_t *buf)
 {
 	// Playing is quite impossible without a sample.
 	if(!buf->sample)
@@ -207,10 +191,7 @@ void DS_DummyPlay(sfxbuffer_t * buf)
 	buf->flags |= SFXBF_PLAYING;
 }
 
-//===========================================================================
-// DS_DummyStop
-//===========================================================================
-void DS_DummyStop(sfxbuffer_t * buf)
+void DS_DummyStop(sfxbuffer_t *buf)
 {
 	// Clear the flag that tells the Sfx module about playing buffers.
 	buf->flags &= ~SFXBF_PLAYING;
@@ -219,11 +200,10 @@ void DS_DummyStop(sfxbuffer_t * buf)
 	buf->flags |= SFXBF_RELOAD;
 }
 
-//===========================================================================
-// DS_DummyRefresh
-//  Buffer streamer. Called by the Sfx refresh thread. 
-//===========================================================================
-void DS_DummyRefresh(sfxbuffer_t * buf)
+/**
+ * Buffer streamer. Called by the Sfx refresh thread. 
+ */
+void DS_DummyRefresh(sfxbuffer_t *buf)
 {
 	// Can only be done if there is a sample and the buffer is playing.
 	if(!buf->sample || !(buf->flags & SFXBF_PLAYING))
@@ -237,58 +217,57 @@ void DS_DummyRefresh(sfxbuffer_t * buf)
 	}
 }
 
-//===========================================================================
-// DS_DummySet
-//  SFXBP_VOLUME (if negative, interpreted as attenuation)
-//  SFXBP_FREQUENCY
-//  SFXBP_PAN (-1..1)
-//  SFXBP_MIN_DISTANCE
-//  SFXBP_MAX_DISTANCE
-//  SFXBP_RELATIVE_MODE
-//===========================================================================
-void DS_DummySet(sfxbuffer_t * buf, int property, float value)
+/**
+ * @param property      SFXBP_VOLUME (if negative, interpreted as attenuation)
+ *                      SFXBP_FREQUENCY
+ *                      SFXBP_PAN (-1..1)
+ *                      SFXBP_MIN_DISTANCE
+ *                      SFXBP_MAX_DISTANCE
+ *                      SFXBP_RELATIVE_MODE
+ */
+void DS_DummySet(sfxbuffer_t *buf, int property, float value)
 {
-	switch (property)
+	switch(property)
 	{
 	case SFXBP_FREQUENCY:
 		buf->freq = buf->rate * value;
 		break;
+
+    default:
+        break;
 	}
 }
 
-//===========================================================================
-// DS_DummySetv
-//  SFXBP_POSITION
-//  SFXBP_VELOCITY
-//  Coordinates specified in world coordinate system, converted to DSound's: 
-//  +X to the right, +Y up and +Z away (Y and Z swapped, i.e.).
-//===========================================================================
-void DS_DummySetv(sfxbuffer_t * buf, int property, float *values)
+/**
+ * Coordinates specified in world coordinate system, converted to DSound's: 
+ * +X to the right, +Y up and +Z away (Y and Z swapped, i.e.).
+ *
+ * @param property      SFXBP_POSITION
+ *                      SFXBP_VELOCITY
+ */
+void DS_DummySetv(sfxbuffer_t *buf, int property, float *values)
 {
 }
 
-//===========================================================================
-// DS_DummyListener
-//  SFXLP_UNITS_PER_METER
-//  SFXLP_DOPPLER
-//  SFXLP_UPDATE
-//===========================================================================
+/**
+ * @param property      SFXLP_UNITS_PER_METER
+ *                      SFXLP_DOPPLER
+ *                      SFXLP_UPDATE
+ */
 void DS_DummyListener(int property, float value)
 {
 }
 
-//===========================================================================
-// DS_DummyListenerEnvironment
-//  Values use SRD_* for indices.
-//===========================================================================
+/**
+ * Values use SRD_* for indices.
+ */
 void DS_DummyListenerEnvironment(float *rev)
 {
 }
 
-//===========================================================================
-// DS_DummyListenerv
-//  Call SFXLP_UPDATE at the end of every channel update.
-//===========================================================================
+/**
+ * Call SFXLP_UPDATE at the end of every channel update.
+ */
 void DS_DummyListenerv(int property, float *values)
 {
 }
