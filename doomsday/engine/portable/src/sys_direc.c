eab9ee31af006c210ded2d3b55e6588fb907415a
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-03-02 08:49:29 +0000

    Updated style.

--- doomsday/engine/portable/src/sys_direc.c
+++ doomsday/engine/portable/src/sys_direc.c
@@ -4,6 +4,7 @@
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
  *\author Copyright © 2003-2006 Jaakko Keränen <skyjake@dengine.net>
+ *\author Copyright © 2007 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -77,7 +78,7 @@
 
 // CODE --------------------------------------------------------------------
 
-void Dir_GetDir(directory_t * dir)
+void Dir_GetDir(directory_t *dir)
 {
     memset(dir, 0, sizeof(*dir));
 
@@ -90,9 +91,9 @@ void Dir_GetDir(directory_t * dir)
     /* VERBOSE2( printf("Dir_GetDir: %s\n", dir->path) ); */
 }
 
-int Dir_ChDir(directory_t * dir)
+int Dir_ChDir(directory_t *dir)
 {
-    int     success;
+    int         success;
 
     _chdrive(dir->drive);
     success = !_chdir(dir->path);   // Successful if == 0.
@@ -104,9 +105,9 @@ int Dir_ChDir(directory_t * dir)
     return success;
 }
 
-void Dir_MakeDir(const char *path, directory_t * dir)
+void Dir_MakeDir(const char *path, directory_t *dir)
 {
-    char    temp[256];
+    char        temp[256];
 
     Dir_FileDir(path, dir);
     Dir_FileName(path, temp);
@@ -114,12 +115,12 @@ void Dir_MakeDir(const char *path, directory_t * dir)
     Dir_ValidDir(dir->path);    // Make it a well formed path.
 }
 
-/*
+/**
  * Translates the given filename (>,} => basedir).
  */
-void Dir_FileDir(const char *str, directory_t * dir)
+void Dir_FileDir(const char *str, directory_t *dir)
 {
-    char    temp[256], pth[256];
+    char        temp[256], pth[256];
 
     M_TranslatePath(str, pth);
     _fullpath(temp, pth, 255);
@@ -132,20 +133,20 @@ void Dir_FileDir(const char *str, directory_t * dir)
 
 void Dir_FileName(const char *str, char *name)
 {
-    char    ext[100];
+    char        ext[100];
 
     _splitpath(str, 0, 0, name, ext);
     strcat(name, ext);
 }
 
-/*
+/**
  * Calculate an identifier for the file based on its full path name.
  * The identifier is the MD5 hash of the path.
  */
 void Dir_FileID(const char *str, byte identifier[16])
 {
-    char temp[256];
-    md5_ctx_t context;
+    char        temp[256];
+    md5_ctx_t   context;
 
     // First normalize the name.
     memset(temp, 0, sizeof(temp));
@@ -162,24 +163,26 @@ void Dir_FileID(const char *str, byte identifier[16])
     md5_final(&context, identifier);
 }
 
-/*
- * Returns true if the directories are equal.
+/**
+ * @return              <code>true</code> if the directories are equal.
  */
-boolean Dir_IsEqual(directory_t * a, directory_t * b)
+boolean Dir_IsEqual(directory_t *a, directory_t *b)
 {
     if(a->drive != b->drive)
         return false;
     return !stricmp(a->path, b->path);
 }
 
-/*
- * Returns true iff the given path is absolute (starts with \ or / or
- * the second character is a ':' (drive).
+/**
+ * @return              <code>true</code> if the given path is absolute
+ *                      (starts with \ or / or the second character is
+ *                      a ':' (drive).
  */
 int Dir_IsAbsolute(const char *str)
 {
     if(!str)
         return 0;
+
     if(str[0] == '\\' || str[0] == '/' || str[1] == ':')
         return true;
 #ifdef UNIX
@@ -189,14 +192,14 @@ int Dir_IsAbsolute(const char *str)
     return false;
 }
 
-/*
+/**
  * Converts directory slashes to the correct type of slash.
  */
 void Dir_FixSlashes(char *path)
 {
-    int     i, len = strlen(path);
+    uint        i, len = strlen(path);
 
-    for(i = 0; i < len; i++)
+    for(i = 0; i < len; ++i)
     {
         if(path[i] == DIR_WRONG_SEP_CHAR)
             path[i] = DIR_SEP_CHAR;
@@ -204,14 +207,14 @@ void Dir_FixSlashes(char *path)
 }
 
 #ifdef UNIX
-/*
+/**
  * If the path begins with a tilde, replace it with either the value
  * of the HOME environment variable or a user's home directory (from
  * passwd).
  */
 void Dir_ExpandHome(char *str)
 {
-    char    buf[PATH_MAX];
+    char        buf[PATH_MAX];
 
     if(str[0] != '~')
         return;
@@ -230,7 +233,7 @@ void Dir_ExpandHome(char *str)
     }
     else
     {
-        char    userName[PATH_MAX], *end = NULL;
+        char        userName[PATH_MAX], *end = NULL;
         struct passwd *pw;
 
         end = strchr(str + 1, '/');
@@ -252,14 +255,14 @@ void Dir_ExpandHome(char *str)
 }
 #endif
 
-/*
+/**
  * Appends a backslash, if necessary. Also converts forward slashes into
  * backward ones. Does not check if the directory actually exists, just
  * that it's a well-formed path name.
  */
 void Dir_ValidDir(char *str)
 {
-    int     i, len = strlen(str);
+    uint        idx, len = strlen(str);
 
     if(!len)
         return;                 // Nothing to do.
@@ -267,11 +270,12 @@ void Dir_ValidDir(char *str)
     Dir_FixSlashes(str);
 
     // Remove whitespace from the end.
-    for(i = len - 1; isspace(str[i]) && i >= 0; i--)
-        str[i] = 0;
+    idx = len - 1;
+    while(idx && isspace(str[idx]))
+        str[idx--] = 0;
 
     // Make sure it ends in a directory separator character.
-    if(str[len - 1] != DIR_SEP_CHAR)
+    if(str[idx] != DIR_SEP_CHAR)
         strcat(str, DIR_SEP_STR);
 
 #ifdef UNIX
@@ -279,7 +283,7 @@ void Dir_ValidDir(char *str)
 #endif
 }
 
-/*
+/**
  * Converts a possibly relative path to a full path.
  */
 void Dir_MakeAbsolute(char *path)
