419de1e337e8b9b59fdc6921a3e87c7e56a38433
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-07-26 21:06:53 +0000

    Updated style and cleaned up.

--- doomsday/plugins/openal/src/driver_openal.c
+++ doomsday/plugins/openal/src/driver_openal.c
@@ -4,7 +4,7 @@
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
  *\author Copyright © 2003-2007 Jaakko Keränen <jaakko.keranen@iki.fi>
- *\author Copyright © 2006 Daniel Swanson <danij@dengine.net>
+ *\author Copyright © 2006-2007 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -75,10 +75,10 @@ enum { VX, VY, VZ };
 // EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
 
 #ifdef WIN32
-ALenum(*EAXGet) (const struct _GUID * propertySetID, ALuint property,
-                 ALuint source, ALvoid * value, ALuint size);
-ALenum(*EAXSet) (const struct _GUID * propertySetID, ALuint property,
-                 ALuint source, ALvoid * value, ALuint size);
+ALenum(*EAXGet) (const struct _GUID *propertySetID, ALuint property,
+                 ALuint source, ALvoid *value, ALuint size);
+ALenum(*EAXSet) (const struct _GUID *propertySetID, ALuint property,
+                 ALuint source, ALvoid *value, ALuint size);
 #endif
 
 // PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
@@ -86,15 +86,15 @@ ALenum(*EAXSet) (const struct _GUID * propertySetID, ALuint property,
 int     DS_Init(void);
 void    DS_Shutdown(void);
 sfxbuffer_t *DS_CreateBuffer(int flags, int bits, int rate);
-void    DS_DestroyBuffer(sfxbuffer_t * buf);
-void    DS_Load(sfxbuffer_t * buf, struct sfxsample_s *sample);
-void    DS_Reset(sfxbuffer_t * buf);
-void    DS_Play(sfxbuffer_t * buf);
-void    DS_Stop(sfxbuffer_t * buf);
-void    DS_Refresh(sfxbuffer_t * buf);
+void    DS_DestroyBuffer(sfxbuffer_t *buf);
+void    DS_Load(sfxbuffer_t *buf, struct sfxsample_s *sample);
+void    DS_Reset(sfxbuffer_t *buf);
+void    DS_Play(sfxbuffer_t *buf);
+void    DS_Stop(sfxbuffer_t *buf);
+void    DS_Refresh(sfxbuffer_t *buf);
 void    DS_Event(int type);
-void    DS_Set(sfxbuffer_t * buf, int property, float value);
-void    DS_Setv(sfxbuffer_t * buf, int property, float *values);
+void    DS_Set(sfxbuffer_t *buf, int property, float value);
+void    DS_Setv(sfxbuffer_t *buf, int property, float *values);
 void    DS_Listener(int property, float value);
 void    DS_Listenerv(int property, float *values);
 
@@ -127,22 +127,17 @@ ALCcontext *context = 0;
 
 // CODE --------------------------------------------------------------------
 
-//===========================================================================
-// Error
-//===========================================================================
-int Error(const char *where, const char *msg)
+static int error(const char *what, const char *msg)
 {
     ALenum  code = alGetError();
 
     if(code == AL_NO_ERROR)
         return false;
-    Con_Message("DS_%s(OpenAL): %s [%s]\n", where, msg, alGetString(code));
+
+    Con_Message("DS_%s(OpenAL): %s [%s]\n", what, msg, alGetString(code));
     return true;
 }
 
-//===========================================================================
-// DS_Init
-//===========================================================================
 int DS_Init(void)
 {
     if(initOk)
@@ -189,9 +184,6 @@ int DS_Init(void)
     return true;
 }
 
-//===========================================================================
-// DS_Shutdown
-//===========================================================================
 void DS_Shutdown(void)
 {
     if(!initOk)
@@ -206,41 +198,18 @@ void DS_Shutdown(void)
     initOk = false;
 }
 
-//===========================================================================
-// DS_CreateBuffer
-//===========================================================================
 sfxbuffer_t *DS_CreateBuffer(int flags, int bits, int rate)
 {
     sfxbuffer_t *buf;
     ALuint  bufferName, sourceName;
 
-    /*  IA3dSource2 *src;
-       sfxbuffer_t *buf;
-       bool play3d = (flags & SFXBF_3D) != 0;
-
-       // Create a new source.
-       if(FAILED(hr = a3d->NewSource(play3d? A3DSOURCE_TYPEDEFAULT
-       : A3DSOURCE_INITIAL_RENDERMODE_NATIVE, &src))) return NULL;
-
-       // Set its format.
-       WAVEFORMATEX format, *f = &format;
-       memset(f, 0, sizeof(*f));
-       f->wFormatTag = WAVE_FORMAT_PCM;
-       f->nChannels = 1;
-       f->nSamplesPerSec = rate;
-       f->nBlockAlign = bits/8;
-       f->nAvgBytesPerSec = f->nSamplesPerSec * f->nBlockAlign;
-       f->wBitsPerSample = bits;
-       src->SetAudioFormat((void*)f);
-     */
-
     // Create a new buffer and a new source.
     alGenBuffers(1, &bufferName);
-    if(Error("CreateBuffer", "GenBuffers"))
+    if(error("CreateBuffer", "GenBuffers"))
         return 0;
 
     alGenSources(1, &sourceName);
-    if(Error("CreateBuffer", "GenSources"))
+    if(error("CreateBuffer", "GenSources"))
     {
         alDeleteBuffers(1, &bufferName);
         return 0;
@@ -248,11 +217,10 @@ sfxbuffer_t *DS_CreateBuffer(int flags, int bits, int rate)
 
     // Attach the buffer to the source.
     alSourcei(sourceName, AL_BUFFER, bufferName);
-    Error("CreateBuffer", "Source BUFFER");
+    error("CreateBuffer", "Source BUFFER");
 
     if(!(flags & SFXBF_3D))
-    {
-        // 2D sounds are around the listener.
+    {   // 2D sounds are around the listener.
         alSourcei(sourceName, AL_SOURCE_RELATIVE, AL_TRUE);
         alSourcef(sourceName, AL_ROLLOFF_FACTOR, 0);
     }
@@ -265,14 +233,11 @@ sfxbuffer_t *DS_CreateBuffer(int flags, int bits, int rate)
     buf->bytes = bits / 8;
     buf->rate = rate;
     buf->flags = flags;
-    buf->freq = rate;           // Modified by calls to Set(SFXBP_FREQUENCY).
+    buf->freq = rate; // Modified by calls to Set(SFXBP_FREQUENCY).
     return buf;
 }
 
-//===========================================================================
-// DS_DestroyBuffer
-//===========================================================================
-void DS_DestroyBuffer(sfxbuffer_t * buf)
+void DS_DestroyBuffer(sfxbuffer_t *buf)
 {
     ALuint  srcName = Src(buf);
     ALuint  bufName = Buf(buf);
@@ -283,66 +248,54 @@ void DS_DestroyBuffer(sfxbuffer_t * buf)
     Z_Free(buf);
 }
 
-//===========================================================================
-// DS_Load
-//===========================================================================
-void DS_Load(sfxbuffer_t * buf, struct sfxsample_s *sample)
+void DS_Load(sfxbuffer_t *buf, struct sfxsample_s *sample)
 {
     // Does the buffer already have a sample loaded?
     if(buf->sample)
     {
         // Is the same one?
         if(buf->sample->id == sample->id)
-            return;             // No need to reload.
+            return; // No need to reload.
     }
 
     alBufferData(Buf(buf),
-                 sample->bytesper == 1 ? AL_FORMAT_MONO8 : AL_FORMAT_MONO16,
+                 sample->bytesPer == 1 ? AL_FORMAT_MONO8 : AL_FORMAT_MONO16,
                  sample->data, sample->size, sample->rate);
 
-    /*  alGetBufferi(Buf(buf), AL_SIZE, &d);
-       Con_Message("Loaded = %i\n", d);
-       alGetBufferi(Buf(buf), AL_FREQUENCY, &d);
-       Con_Message("Freq = %i\n", d);
-       alGetBufferi(Buf(buf), AL_BITS, &d);
-       Con_Message("Bits = %i\n", d); */
-
-    Error("Load", "BufferData");
+    error("Load", "BufferData");
     buf->sample = sample;
 }
 
-//===========================================================================
-// DS_Reset
-//  Stops the buffer and makes it forget about its sample.
-//===========================================================================
-void DS_Reset(sfxbuffer_t * buf)
+/**
+ * Stops the buffer and makes it forget about its sample.
+ */
+void DS_Reset(sfxbuffer_t *buf)
 {
     DS_Stop(buf);
     buf->sample = NULL;
-    // Unallocate the resources of the source.
-    //Src(buf)->FreeAudioData();
 }
 
-//===========================================================================
-// DS_Play
-//===========================================================================
-void DS_Play(sfxbuffer_t * buf)
+void DS_Play(sfxbuffer_t *buf)
 {
-    ALuint  source = Src(buf), bn;
-    ALint   i;
-    float   f;
+    ALint       i;
+    float       f;
+    ALuint      source = Src(buf), bn;
 
     // Playing is quite impossible without a sample.
     if(!buf->sample)
         return;
 
-    /*  alGetSourcei(source, AL_BUFFER, &bn);
-       Con_Message("Buffer = %x\n", bn);
-       if(bn != Buf(buf)) Con_Message("Not the same!\n"); */
+#if _DEBUG
+alGetSourcei(source, AL_BUFFER, &bn);
+Con_Message("Buffer = %x\n", bn);
+if(bn != Buf(buf))
+    Con_Message("Not the same!\n");
+#endif
+
     alSourcei(source, AL_BUFFER, Buf(buf));
     alSourcei(source, AL_LOOPING, (buf->flags & SFXBF_REPEAT) != 0);
     alSourcePlay(source);
-    Error("Play", "SourcePlay");
+    error("Play", "SourcePlay");
 
     alGetSourcei(source, AL_BUFFER, &bn);
     Con_Message("Buffer = %x (real = %x), isBuf:%i\n", bn, Buf(buf),
@@ -361,7 +314,7 @@ void DS_Play(sfxbuffer_t * buf)
     Con_Message("Pitch = %g\n", f);
 
     alGetSourcei(source, AL_SOURCE_STATE, &i);
-    Error("Play", "Get state");
+    error("Play", "Get state");
     Con_Message("State = %x\n", i);
     if(i != AL_PLAYING)
         Con_Message("not playing...\n");
@@ -370,23 +323,18 @@ void DS_Play(sfxbuffer_t * buf)
     buf->flags |= SFXBF_PLAYING;
 }
 
-//===========================================================================
-// DS_Stop
-//===========================================================================
-void DS_Stop(sfxbuffer_t * buf)
+void DS_Stop(sfxbuffer_t *buf)
 {
     if(!buf->sample)
         return;
+
     alSourceRewind(Src(buf));
     buf->flags &= ~SFXBF_PLAYING;
 }
 
-//===========================================================================
-// DS_Refresh
-//===========================================================================
-void DS_Refresh(sfxbuffer_t * buf)
+void DS_Refresh(sfxbuffer_t *buf)
 {
-    ALint   state;
+    ALint       state;
 
     if(!buf->sample)
         return;
@@ -398,32 +346,28 @@ void DS_Refresh(sfxbuffer_t * buf)
     }
 }
 
-//===========================================================================
-// DS_Event
-//===========================================================================
 void DS_Event(int type)
 {
-    /*switch(type)
-       {
-       case SFXEV_BEGIN:
-       a3d->Clear();
-       break;
-
-       case SFXEV_END:
-       a3d->Flush();
-       break;
-       } */
+    // Not supported.
 }
 
-//===========================================================================
-// Vectors
-//  Specify yaw and pitch in radians. 'up' can be NULL.
-//===========================================================================
-void Vectors(float yaw, float pitch, float *front, float *up)
+/**
+ * @param yaw           Yaw in radians.
+ * @param pitch         Pitch in radians.
+ * @param front         Ptr to front vector, can be @c NULL.
+ * @param up            Ptr to up vector, can be @c NULL.
+ */
+static void vectors(float yaw, float pitch, float *front, float *up)
 {
-    front[VX] = (float) (cos(yaw) * cos(pitch));
-    front[VZ] = (float) (sin(yaw) * cos(pitch));
-    front[VY] = (float) sin(pitch);
+    if(!front && !up)
+        return; // Nothing to do.
+
+    if(front)
+    {
+        front[VX] = (float) (cos(yaw) * cos(pitch));
+        front[VZ] = (float) (sin(yaw) * cos(pitch));
+        front[VY] = (float) sin(pitch);
+    }
 
     if(up)
     {
@@ -433,54 +377,23 @@ void Vectors(float yaw, float pitch, float *front, float *up)
     }
 }
 
-//===========================================================================
-// SetPan
-//  Pan is linear, from -1 to 1. 0 is in the middle.
-//===========================================================================
-void SetPan(ALuint source, float pan)
+/**
+ * Pan is linear, from -1 to 1. 0 is in the middle.
+ */
+static void setPan(ALuint source, float pan)
 {
-    /*  float gains[2];
+    float       pos[3];
 
-       if(pan < -1) pan = -1;
-       if(pan > 1) pan = 1;
-
-       if(pan == 0)
-       {
-       gains[0] = gains[1] = 1; // In the center.
-       }
-       else if(pan < 0) // On the left?
-       {
-       gains[0] = 1;
-       //gains[1] = pow(.5, (100*(1 - 2*pan))/6);
-       gains[1] = 1 + pan;
-       }
-       else if(pan > 0) // On the right?
-       {
-       //gains[0] = pow(.5, (100*(2*(pan-0.5)))/6);
-       gains[0] = 1 - pan;
-       gains[1] = 1;
-       }
-       source->SetPanValues(2, gains); */
-
-    float   pos[3];
-
-    //alSourcei(source, AL_SOURCE_RELATIVE, AL_TRUE);
-    Vectors((float) (headYaw - pan * PI / 2), headPitch, pos, 0);
+    vectors((float) (headYaw - pan * PI / 2), headPitch, pos, 0);
     alSourcefv(source, AL_POSITION, pos);
 }
 
-//===========================================================================
-// DS_Set
-//===========================================================================
-void DS_Set(sfxbuffer_t * buf, int property, float value)
+void DS_Set(sfxbuffer_t *buf, int prop, float value)
 {
     unsigned int dw;
+    ALuint      source = Src(buf);
 
-    /*  IA3dSource2 *s = Src(buf);
-       float minDist, maxDist; */
-    ALuint  source = Src(buf);
-
-    switch (property)
+    switch(prop)
     {
     case SFXBP_VOLUME:
         alSourcef(source, AL_GAIN, value);
@@ -488,7 +401,7 @@ void DS_Set(sfxbuffer_t * buf, int property, float value)
 
     case SFXBP_FREQUENCY:
         dw = (int) (buf->rate * value);
-        if(dw != buf->freq)     // Don't set redundantly.
+        if(dw != buf->freq) // Don't set redundantly.
         {
             buf->freq = dw;
             alSourcef(source, AL_PITCH, value);
@@ -496,7 +409,7 @@ void DS_Set(sfxbuffer_t * buf, int property, float value)
         break;
 
     case SFXBP_PAN:
-        SetPan(source, value);
+        setPan(source, value);
         break;
 
     case SFXBP_MIN_DISTANCE:
@@ -510,17 +423,17 @@ void DS_Set(sfxbuffer_t * buf, int property, float value)
     case SFXBP_RELATIVE_MODE:
         alSourcei(source, AL_SOURCE_RELATIVE, value ? AL_TRUE : AL_FALSE);
         break;
+
+    default:
+        break;
     }
 }
 
-//===========================================================================
-// DS_Setv
-//===========================================================================
-void DS_Setv(sfxbuffer_t * buf, int property, float *values)
+void DS_Setv(sfxbuffer_t *buf, int prop, float *values)
 {
-    ALuint  source = Src(buf);
+    ALuint      source = Src(buf);
 
-    switch (property)
+    switch(prop)
     {
     case SFXBP_POSITION:
         alSource3f(source, AL_POSITION, values[VX] / unitsPerMeter,
@@ -531,15 +444,15 @@ void DS_Setv(sfxbuffer_t * buf, int property, float *values)
         alSource3f(source, AL_VELOCITY, values[VX] / unitsPerMeter,
                    values[VZ] / unitsPerMeter, values[VY] / unitsPerMeter);
         break;
+
+    default:
+        break;
     }
 }
 
-//===========================================================================
-// DS_Listener
-//===========================================================================
-void DS_Listener(int property, float value)
+void DS_Listener(int prop, float value)
 {
-    switch (property)
+    switch(prop)
     {
     case SFXLP_UNITS_PER_METER:
         unitsPerMeter = value;
@@ -548,58 +461,17 @@ void DS_Listener(int property, float value)
     case SFXLP_DOPPLER:
         alDopplerFactor(value);
         break;
+
+    default:
+        break;
     }
 }
 
-//===========================================================================
-// SetEnvironment
-//===========================================================================
-void SetEnvironment(float *rev)
+void DS_Listenerv(int prop, float *values)
 {
-    /*  A3DREVERB_PROPERTIES rp;
-       A3DREVERB_PRESET *pre = &rp.uval.preset;
-       float val;
+    float       ori[6];
 
-       // Do we already have a reverb object?
-       if(a3dReverb == NULL)
-       {
-       // We need to create it.
-       if(FAILED(hr = a3d->NewReverb(&a3dReverb)))
-       return; // Silently go away.
-       // Bind it as the current one.
-       a3d->BindReverb(a3dReverb);
-       }
-       memset(&rp, 0, sizeof(rp));
-       rp.dwSize = sizeof(rp);
-       rp.dwType = A3DREVERB_TYPE_PRESET;
-       pre->dwSize = sizeof(A3DREVERB_PRESET);
-
-       val = rev[SRD_SPACE];
-       if(rev[SRD_DECAY] > .5)
-       {
-       // This much decay needs at least the Generic environment.
-       if(val < .2) val = .2f;
-       }
-       pre->dwEnvPreset = val >= 1? A3DREVERB_PRESET_PLAIN
-       : val >= .8? A3DREVERB_PRESET_CONCERTHALL
-       : val >= .6? A3DREVERB_PRESET_AUDITORIUM
-       : val >= .4? A3DREVERB_PRESET_CAVE
-       : val >= .2? A3DREVERB_PRESET_GENERIC
-       : A3DREVERB_PRESET_ROOM;
-       pre->fVolume = rev[SRD_VOLUME];
-       pre->fDecayTime = (rev[SRD_DECAY] - .5f) + .55f;
-       pre->fDamping = rev[SRD_DAMPING];
-       a3dReverb->SetAllProperties(&rp); */
-}
-
-//===========================================================================
-// DS_Listenerv
-//===========================================================================
-void DS_Listenerv(int property, float *values)
-{
-    float   ori[6];
-
-    switch (property)
+    switch(prop)
     {
     case SFXLP_PRIMARY_FORMAT:
         // No need to concern ourselves with this kind of things...
@@ -616,17 +488,17 @@ void DS_Listenerv(int property, float *values)
         break;
 
     case SFXLP_ORIENTATION:
-        Vectors(headYaw = (float) (values[VX] / 180 * PI),
+        vectors(headYaw = (float) (values[VX] / 180 * PI),
                 headPitch = (float) (values[VY] / 180 * PI),
                 ori, ori + 3);
         alListenerfv(AL_ORIENTATION, ori);
         break;
 
-    case SFXLP_REVERB:
-        //SetEnvironment(values);
+    case SFXLP_REVERB: // Not supported.
         break;
 
     default:
-        DS_Listener(property, 0);
+        DS_Listener(prop, 0);
+        break;
     }
 }
