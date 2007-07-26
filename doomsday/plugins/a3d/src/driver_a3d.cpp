528e218a17b07a296ab2febe903c6e622a24c537
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-07-26 21:13:52 +0000

    Updated style and cleaned up.

--- doomsday/plugins/a3d/src/driver_a3d.cpp
+++ doomsday/plugins/a3d/src/driver_a3d.cpp
@@ -4,7 +4,7 @@
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
  *\author Copyright © 2003-2007 Jaakko Keränen <jaakko.keranen@iki.fi>
- *\author Copyright © 2006 Daniel Swanson <danij@dengine.net>
+ *\author Copyright © 2006-2007 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -90,9 +90,9 @@ void            DS_Listenerv(int property, float *values);
 
 bool                initOk = false;
 int                 verbose;
-IA3d5               *a3d = 0;
-IA3dReverb          *a3dReverb = 0;
-IA3dListener        *a3dListener = 0;
+IA3d5              *a3d = 0;
+IA3dReverb         *a3dReverb = 0;
+IA3dListener       *a3dListener = 0;
 A3DCAPS_HARDWARE    hwCaps;
 HRESULT             hr;
 
@@ -100,27 +100,30 @@ HRESULT             hr;
 
 // CODE --------------------------------------------------------------------
 
-void Error(const char *where, const char *msg)
+static void error(const char *what, const char *msg)
 {
-    Con_Message("%s(A3D): %s [Result = 0x%x]\n", where, msg, hr);
+    Con_Message("%s(A3D): %s [Result = 0x%x]\n", what, msg, hr);
 }
 
 int DS_Init(void)
 {
     HWND hWnd;
 
-    if(initOk) return true;
+    if(initOk)
+        return true;
 
     // Are we in verbose mode?
     if((verbose = ArgExists("-verbose")))
         Con_Message("DS_Init(A3D): Starting A3D 3.0...\n");
 
     // Init A3D.
-    if(FAILED(A3dRegister())) return false;
-    if(FAILED(A3dInitialize())) return false;
+    if(FAILED(A3dRegister()))
+        return false;
+    if(FAILED(A3dInitialize()))
+        return false;
     if(FAILED(hr = A3dCreate(NULL, (void**) &a3d, NULL, A3D_REVERB)))
     {
-        Error("DS_Init", "Couldn't create A3D.");
+        error("DS_Init", "Couldn't create A3D.");
         return false;
     }
 
@@ -130,15 +133,15 @@ int DS_Init(void)
     // Set the cooperative level.
     if(FAILED(hr = a3d->SetCooperativeLevel(hWnd, A3D_CL_NORMAL)))
     {
-        Error("DS_Init", "Couldn't set cooperative level to normal.");
+        error("DS_Init", "Couldn't set cooperative level to normal.");
         return false;
     }
 
     // Give me the listener!
     if(FAILED(hr = a3d->QueryInterface(IID_IA3dListener,
-        (void**) &a3dListener)))
+                                       (void**) &a3dListener)))
     {
-        Error("DS_Init", "Couldn't get a listener.");
+        error("DS_Init", "Couldn't get a listener.");
         return false;
     }
 
@@ -162,9 +165,11 @@ int DS_Init(void)
 
 void DS_Shutdown(void)
 {
-    if(!initOk) return;
+    if(!initOk)
+        return;
 
-    if(a3d) a3d->Shutdown(); // This'll get rid of everything.
+    if(a3d)
+        a3d->Shutdown(); // This'll get rid of everything.
     a3d = NULL;
     a3dListener = NULL;
     a3dReverb = NULL;
@@ -176,11 +181,13 @@ sfxbuffer_t* DS_CreateBuffer(int flags, int bits, int rate)
 {
     IA3dSource2 *src;
     sfxbuffer_t *buf;
-    bool play3d = (flags & SFXBF_3D) != 0;
+    bool        play3d = (flags & SFXBF_3D) != 0;
 
     // Create a new source.
-    if(FAILED(hr = a3d->NewSource(play3d? A3DSOURCE_TYPEDEFAULT
-        : A3DSOURCE_INITIAL_RENDERMODE_NATIVE, &src))) return NULL;
+    if(FAILED(hr =
+       a3d->NewSource(play3d? A3DSOURCE_TYPEDEFAULT
+                      : A3DSOURCE_INITIAL_RENDERMODE_NATIVE, &src)))
+        return NULL;
 
     // Set its format.
     WAVEFORMATEX format, *f = &format;
@@ -200,7 +207,8 @@ sfxbuffer_t* DS_CreateBuffer(int flags, int bits, int rate)
     buf->bytes = bits/8;
     buf->rate = rate;
     buf->flags = flags;
-    buf->freq = rate;       // Modified by calls to Set(SFXBP_FREQUENCY).
+    buf->freq = rate; // Modified by calls to Set(SFXBP_FREQUENCY).
+
     return buf;
 }
 
@@ -218,7 +226,8 @@ void DS_Load(sfxbuffer_t *buf, struct sfxsample_s *sample)
     if(buf->sample)
     {
         // Is the same one?
-        if(buf->sample->id == sample->id) return;
+        if(buf->sample->id == sample->id)
+            return;
 
         // Free the existing data.
         s->FreeAudioData();
@@ -228,7 +237,9 @@ void DS_Load(sfxbuffer_t *buf, struct sfxsample_s *sample)
     // Allocate memory for the sample.
     if(FAILED(hr = s->AllocateAudioData(sample->size)))
     {
-        if(verbose) Error("DS_Load", "Failed to allocate audio data.");
+        if(verbose)
+            error("DS_Load", "Failed to allocate audio data.");
+
         return;
     }
 
@@ -238,7 +249,9 @@ void DS_Load(sfxbuffer_t *buf, struct sfxsample_s *sample)
     if(FAILED(hr = s->Lock(0, 0, &ptr[0], &bytes[0], &ptr[1], &bytes[1],
         A3D_ENTIREBUFFER)))
     {
-        if(verbose) Error("DS_Load", "Failed to lock source.");
+        if(verbose)
+            error("DS_Load", "Failed to lock source.");
+
         return;
     }
     memcpy(ptr[0], sample->data, bytes[0]);
@@ -248,7 +261,7 @@ void DS_Load(sfxbuffer_t *buf, struct sfxsample_s *sample)
     buf->sample = sample;
 }
 
-/*
+/**
  * Stops the buffer and makes it forget about its sample.
  */
 void DS_Reset(sfxbuffer_t *buf)
@@ -262,7 +275,9 @@ void DS_Reset(sfxbuffer_t *buf)
 void DS_Play(sfxbuffer_t *buf)
 {
     // Playing is quite impossible without a sample.
-    if(!buf->sample) return;
+    if(!buf->sample)
+        return;
+
     Src(buf)->Play(buf->flags & SFXBF_REPEAT? A3D_LOOPED : A3D_SINGLE);
     // The buffer is now playing.
     buf->flags |= SFXBF_PLAYING;
@@ -270,7 +285,9 @@ void DS_Play(sfxbuffer_t *buf)
 
 void DS_Stop(sfxbuffer_t *buf)
 {
-    if(!buf->sample) return;
+    if(!buf->sample)
+        return;
+
     Src(buf)->Stop();
     Src(buf)->Rewind();
     buf->flags &= ~SFXBF_PLAYING;
@@ -284,8 +301,7 @@ void DS_Refresh(sfxbuffer_t *buf)
     // Has the buffer finished playing?
     s->GetStatus(&status);
     if(!status)
-    {
-        // It has stopped playing.
+    {   // It has stopped playing.
         buf->flags &= ~SFXBF_PLAYING;
     }
 }
@@ -301,45 +317,52 @@ void DS_Event(int type)
     case SFXEV_END:
         a3d->Flush();
         break;
+
+    default:
+        break;
     }
 }
 
-/*
+/**
  * Pan is linear, from -1 to 1. 0 is in the middle.
  */
-void SetPan(IA3dSource2 *source, float pan)
+static void setPan(IA3dSource2 *source, float pan)
 {
-    float gains[2];
+    float       gains[2];
 
-    if(pan < -1) pan = -1;
-    if(pan > 1) pan = 1;
+    // Clamp.
+    if(pan < -1)
+        pan = -1;
+    if(pan > 1)
+        pan = 1;
 
     if(pan == 0)
-    {
-        gains[0] = gains[1] = 1;    // In the center.
+    {   // In the center.
+        gains[0] = gains[1] = 1;
     }
-    else if(pan < 0) // On the left?
-    {
+    else if(pan < 0)
+    {   // On the left.
         gains[0] = 1;
         //gains[1] = pow(.5, (100*(1 - 2*pan))/6);
         gains[1] = 1 + pan;
     }
-    else if(pan > 0) // On the right?
-    {
+    else if(pan > 0)
+    {   // On the right.
         //gains[0] = pow(.5, (100*(2*(pan-0.5)))/6);
         gains[0] = 1 - pan;
         gains[1] = 1;
     }
+
     source->SetPanValues(2, gains);
 }
 
-void DS_Set(sfxbuffer_t *buf, int property, float value)
+void DS_Set(sfxbuffer_t *buf, int prop, float value)
 {
-    DWORD dw;
+    DWORD       dw;
     IA3dSource2 *s = Src(buf);
-    float minDist, maxDist;
+    float       minDist, maxDist;
 
-    switch(property)
+    switch(prop)
     {
     case SFXBP_VOLUME:
         s->SetGain(value);
@@ -355,7 +378,7 @@ void DS_Set(sfxbuffer_t *buf, int property, float value)
         break;
 
     case SFXBP_PAN:
-        SetPan(s, value);
+        setPan(s, value);
         break;
 
     case SFXBP_MIN_DISTANCE:
@@ -370,16 +393,19 @@ void DS_Set(sfxbuffer_t *buf, int property, float value)
 
     case SFXBP_RELATIVE_MODE:
         s->SetTransformMode(value? A3DSOURCE_TRANSFORMMODE_HEADRELATIVE
-            : A3DSOURCE_TRANSFORMMODE_NORMAL);
+                            : A3DSOURCE_TRANSFORMMODE_NORMAL);
+        break;
+        
+    default:
         break;
     }
 }
 
-void DS_Setv(sfxbuffer_t *buf, int property, float *values)
+void DS_Setv(sfxbuffer_t *buf, int prop, float *values)
 {
     IA3dSource2 *s = Src(buf);
 
-    switch(property)
+    switch(prop)
     {
     case SFXBP_POSITION:
         s->SetPosition3f(values[VX], values[VZ], values[VY]);
@@ -388,12 +414,15 @@ void DS_Setv(sfxbuffer_t *buf, int property, float *values)
     case SFXBP_VELOCITY:
         s->SetVelocity3f(values[VX], values[VZ], values[VY]);
         break;
+
+    default:
+        break;
     }
 }
 
-void DS_Listener(int property, float value)
+void DS_Listener(int prop, float value)
 {
-    switch(property)
+    switch(prop)
     {
     case SFXLP_UNITS_PER_METER:
         a3d->SetUnitsPerMeter(value);
@@ -402,6 +431,9 @@ void DS_Listener(int property, float value)
     case SFXLP_DOPPLER:
         a3d->SetDopplerScale(value);
         break;
+
+    default:
+        break;
     }
 }
 
@@ -409,17 +441,18 @@ void SetEnvironment(float *rev)
 {
     A3DREVERB_PROPERTIES rp;
     A3DREVERB_PRESET *pre = &rp.uval.preset;
-    float val;
+    float       val;
 
     // Do we already have a reverb object?
     if(a3dReverb == NULL)
-    {
-        // We need to create it.
+    {   // We need to create it.
         if(FAILED(hr = a3d->NewReverb(&a3dReverb)))
             return; // Silently go away.
+
         // Bind it as the current one.
         a3d->BindReverb(a3dReverb);
     }
+
     memset(&rp, 0, sizeof(rp));
     rp.dwSize = sizeof(rp);
     rp.dwType = A3DREVERB_TYPE_PRESET;
@@ -427,25 +460,26 @@ void SetEnvironment(float *rev)
 
     val = rev[SRD_SPACE];
     if(rev[SRD_DECAY] > .5)
-    {
-        // This much decay needs at least the Generic environment.
-        if(val < .2) val = .2f;
+    {   // This much decay needs at least the Generic environment.
+        if(val < .2)
+            val = .2f;
     }
     pre->dwEnvPreset = val >= 1? A3DREVERB_PRESET_PLAIN
-        : val >= .8? A3DREVERB_PRESET_CONCERTHALL
-        : val >= .6? A3DREVERB_PRESET_AUDITORIUM
-        : val >= .4? A3DREVERB_PRESET_CAVE
-        : val >= .2? A3DREVERB_PRESET_GENERIC
-        : A3DREVERB_PRESET_ROOM;
+                     : val >= .8? A3DREVERB_PRESET_CONCERTHALL
+                     : val >= .6? A3DREVERB_PRESET_AUDITORIUM
+                     : val >= .4? A3DREVERB_PRESET_CAVE
+                     : val >= .2? A3DREVERB_PRESET_GENERIC
+                     : A3DREVERB_PRESET_ROOM;
+
     pre->fVolume = rev[SRD_VOLUME];
     pre->fDecayTime = (rev[SRD_DECAY] - .5f) + .55f;
     pre->fDamping = rev[SRD_DAMPING];
     a3dReverb->SetAllProperties(&rp);
 }
 
-void DS_Listenerv(int property, float *values)
+void DS_Listenerv(int prop, float *values)
 {
-    switch(property)
+    switch(prop)
     {
     case SFXLP_PRIMARY_FORMAT:
         // No need to concern ourselves with this kind of things...
@@ -468,6 +502,7 @@ void DS_Listenerv(int property, float *values)
         break;
 
     default:
-        DS_Listener(property, 0);
+        DS_Listener(prop, 0);
+        break;
     }
 }
