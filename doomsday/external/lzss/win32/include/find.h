4bc5eb92a561b7b34009a2c3bde48679f37cbe80
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-07-28 12:06:53 +0000

    missing newline

--- doomsday/external/lzss/win32/include/find.h
+++ doomsday/external/lzss/win32/include/find.h
@@ -22,17 +22,17 @@ finddata;
 #define FILE_READ(handle, buf, size, sz)        sz = read(handle, buf, size)
 #define FILE_WRITE(handle, buf, size, sz)       sz = write(handle, buf, size) 
 #define FILE_SEARCH_STRUCT                      struct finddata
-#define FILE_FINDFIRST(filename, attrib, dta)   myfindfirst(filename, dta, attrib)
-#define FILE_FINDNEXT(dta)                      myfindnext(dta)
-#define FILE_FINDCLOSE(dta)						myfindend(dta)
+#define FILE_FINDFIRST(filename, attrib, dta)   lzssfindfirst(filename, dta, attrib)
+#define FILE_FINDNEXT(dta)                      lzssfindnext(dta)
+#define FILE_FINDCLOSE(dta)						lzssfindend(dta)
 #define FILE_ATTRIB                             attrib
 #define FILE_SIZE                               size
 #define FILE_NAME                               name
 #define FILE_DATE								date
 #define FILE_TIME								time
 
-int myfindfirst(char *filename, struct finddata *dta, long attrib);
-int myfindnext(struct finddata *dta);
-void myfindend(struct finddata *dta);
+int lzssfindfirst(char *filename, struct finddata *dta, long attrib);
+int lzssfindnext(struct finddata *dta);
+void lzssfindend(struct finddata *dta);
 
 #endif
