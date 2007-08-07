2740bf5d1e0209f785cc8204e8c39d654b841d3e
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-08-07 10:14:07 +0000

    Catch out of range texture/flat indices passed to Rend_GetGraphicResourceDecoration(). This should not happen though (must be a bug elsewhere).

--- doomsday/engine/portable/src/rend_decor.c
+++ doomsday/engine/portable/src/rend_decor.c
@@ -3,9 +3,9 @@
  * License: GPL
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
- *\author Copyright Â© 2003-2007 Jaakko KerÃ¤nen <jaakko.keranen@iki.fi>
- *\author Copyright Â© 2006-2007 Daniel Swanson <danij@dengine.net>
- *\author Copyright Â© 2006 Jamie Jones <yagisan@dengine.net>
+ *\author Copyright © 2003-2007 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *\author Copyright © 2006-2007 Daniel Swanson <danij@dengine.net>
+ *\author Copyright © 2006 Jamie Jones <yagisan@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -96,7 +96,8 @@ void Rend_DecorRegister(void)
  */
 static ded_decor_t *Rend_GetGraphicResourceDecoration(int id, boolean isFlat)
 {
-    if(!id)
+    if(id < 0 ||
+       (isFlat && id >= numflats) || (!isFlat && id >= numtextures))
         return NULL;
 
     if(isFlat)
