c19fddb165e547a75309e05a7de2cd5c9b27c195
yagisan <yagisan@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-08-25 11:13:27 +0000

    Add SDL_sound to the list of possible sound drivers

--- doomsday/engine/portable/include/s_sfx.h
+++ doomsday/engine/portable/include/s_sfx.h
@@ -4,6 +4,7 @@
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
  *\author Copyright © 2003-2007 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *\author Copyright © 2007 Jamie Jones <yagisan@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -43,6 +44,7 @@ typedef enum {
 	SFXD_OPENAL,
 	SFXD_COMPATIBLE,
 	SFXD_SDL_MIXER,
+	SFXD_SDL_SOUND,
 	SFXD_DUMMY
 } sfxdriver_e;
 
