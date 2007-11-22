373ec2d22489a7c6d7d76afe9611d3018c16035b
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-11-22 16:50:18 +0000

    Updated style, cleaned up.

--- doomsday/engine/api/sys_musd.h
+++ doomsday/engine/api/sys_musd.h
@@ -4,7 +4,7 @@
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
  *\author Copyright © 2003-2007 Jaakko Keränen <jaakko.keranen@iki.fi>
- *\author Copyright © 2006 Daniel Swanson <danij@dengine.net>
+ *\author Copyright © 2006-2007 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@
  * Boston, MA  02110-1301  USA
  */
 
-/*
+/**
  * sys_musd.h: Music Driver
  */
 
@@ -44,8 +44,8 @@ enum {
 typedef struct musinterface_generic_s {
     int             (*Init) (void);
     void            (*Update) (void);
-    void            (*Set) (int property, float value);
-    int             (*Get) (int property, void *value);
+    void            (*Set) (int prop, float value);
+    int             (*Get) (int prop, void *value);
     void            (*Pause) (int pause);
     void            (*Stop) (void);
 } musinterface_generic_t;
