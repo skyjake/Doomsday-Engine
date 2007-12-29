44d15748db2e131748f81c1999b2141b49c50690
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2007-12-29 22:07:12 +0000

    Updated the Win32 manifest and resource setup for Doomsday.

--- doomsday/engine/win32/include/resource.h
+++ doomsday/engine/win32/include/resource.h
@@ -1,38 +1,15 @@
-/**\file
- *\section License
- * License: GPL
- * Online License Link: http://www.gnu.org/licenses/gpl.html
- *
- *\author Copyright © 2003-2007 Jaakko Keränen <jaakko.keranen@iki.fi>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
- * Boston, MA  02110-1301  USA
- */
-
 //{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
 // Used by Doomsday.rc
 //
-#define IDI_ICON1                       101
-#define IDI_DOOMSDAY                    106
+#define IDI_MAIN                        101
+#define IDI_DOOMSDAY_ICON               106
 #define IDD_STARTUP_WINDOW              113
 #define IDC_MESSAGES                    1000
 #define IDC_PROGRESS                    1004
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        117
