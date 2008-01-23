f6b8af01e87388701d97b81104de8c7613ae9aad
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2008-01-23 20:40:31 +0000

    Use time_t instead of long when representing system date/time in finddata_t (more portable).

--- doomsday/engine/portable/include/sys_findfile.h
+++ doomsday/engine/portable/include/sys_findfile.h
@@ -4,7 +4,7 @@
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
  *\author Copyright © 2004-2007 Jaakko Keränen <jaakko.keranen@iki.fi>
- *\author Copyright © 2007 Daniel Swanson <danij@dengine.net>
+ *\author Copyright © 2007-2008 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -37,8 +37,8 @@
 
 typedef struct finddata_s {
 	void           *finddata;
-	long            date;
-	long            time;
+	time_t          date;
+	time_t          time;
 	size_t          size;
 	char           *name;
 	long            attrib;
