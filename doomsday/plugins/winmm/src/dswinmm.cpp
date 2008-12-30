d364d388108bf1d62b97edd08e7c0b4277fb0241
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2008-12-30 11:36:12 +0000

    Switched the dswinmm plugin to C++. Renamed source files accordingly.

---
+++ doomsday/plugins/winmm/src/dswinmm.cpp
@@ -0,0 +1,77 @@
+/**\file
+ *\section License
+ * License: GPL
+ * Online License Link: http://www.gnu.org/licenses/gpl.html
+ *
+ *\author Copyright © 2008 Daniel Swanson <danij@dengine.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+/**
+ * dswinmm.c: Music Driver for Win32 Multimedia (winmm).
+ */
+
+// HEADER FILES ------------------------------------------------------------
+
+#include "dswinmm.h"
+
+// MACROS ------------------------------------------------------------------
+
+// TYPES -------------------------------------------------------------------
+
+// EXTERNAL FUNCTION PROTOTYPES --------------------------------------------
+
+// PUBLIC FUNCTION PROTOTYPES ----------------------------------------------
+
+// PRIVATE FUNCTION PROTOTYPES ---------------------------------------------
+
+// EXTERNAL DATA DECLARATIONS ----------------------------------------------
+
+// PUBLIC DATA DEFINITIONS -------------------------------------------------
+
+// PRIVATE DATA DEFINITIONS ------------------------------------------------
+
+static boolean initedOk = false;
+
+// CODE --------------------------------------------------------------------
+
+int DS_Init(void)
+{
+    initedOk = true;
+    return true;
+}
+
+void DS_Shutdown(void)
+{
+    if(!initedOk)
+        return; // Wha?
+
+    // In case the engine hasn't already done so, close open interfaces.
+    DM_CDAudio_Shutdown();
+    DM_Music_Shutdown();
+
+    initedOk = false;
+}
+
+/**
+ * The Event function is called to tell the driver about certain critical
+ * events like the beginning and end of an update cycle.
+ */
+void DS_Event(int type)
+{
+    // Do nothing...
+}
