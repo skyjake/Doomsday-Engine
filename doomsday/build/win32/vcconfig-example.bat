0180bd3de794ec280d6f553d5b8ff74241a2ae89
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2009-02-19 15:28:23 +0000

    Updated vcconfig-example.bat use default install paths for most SDKs.

--- doomsday/build/win32/vcconfig-example.bat
+++ doomsday/build/win32/vcconfig-example.bat
@@ -1,7 +1,7 @@
 REM Build configuration (example).
 REM Modify and save as "vcconfig.bat".
 
-SET ROOT_DIR=H:\deng\trunk\doomsday
+SET ROOT_DIR=D:\deng\svn\trunk\doomsday
 
 SET BIN_DIR=Bin\Release
 SET OBJ_DIR=Obj\Release
@@ -9,28 +9,28 @@ SET OBJ_DIR=Obj\Release
 REM -=- Requirements for Doomsday.exe -=-
 REM
 REM ---- Platform SDK:
-SET PLATFORM_INC=D:\VS.NET\Vc7\PlatformSDK\Include
-SET PLATFORM_LIB=D:\VS.NET\VC7\PlatformSDK\Lib
-SET LIBCI_LIB=D:\VS.NET\Vc7\Lib
+SET PLATFORM_INC=C:\Program Files\Microsoft SDKs\Windows\v6.0A\Include
+SET PLATFORM_LIB=C:\Program Files\Microsoft SDKs\Windows\v6.0A\Lib
+SET LIBCI_LIB=C:\Program Files\Microsoft SDKs\Windows\v6.0A\Include
 
 REM ---- Python Interpreter
-SET PYTHON_DIR=D:\Python24
+SET PYTHON_DIR=C:\Python25
 
 REM ---- DirectX:
-SET DX_INC=D:\sdk\dx8\include
-SET DX_LIB=D:\sdk\dx8\lib
+SET DX_INC=C:\Program Files (x86)\Microsoft DirectX SDK (March 2008)\Include
+SET DX_LIB=C:\Program Files (x86)\Microsoft DirectX SDK (March 2008)\Lib
 
 REM ---- Creative Labs EAX:
 SET EAX_INC=D:\sdk\Creative Labs\EAX 2.0 Extensions SDK\Include
 SET EAX_LIB=D:\sdk\Creative Labs\EAX 2.0 Extensions SDK\Libs
 
 REM ---- SDL:
-SET SDL_INC=D:\sdk\SDL-1.2.6\include
-SET SDL_LIB=D:\sdk\SDL-1.2.6\lib
+SET SDL_INC=D:\sdk\SDL-1.2.13\include
+SET SDL_LIB=D:\sdk\SDL-1.2.13\lib
 
 REM ---- SDL_net:
-SET SDLNET_INC=D:\sdk\SDL_net-1.2.5\include
-SET SDLNET_LIB=D:\sdk\SDL_net-1.2.5\lib
+SET SDLNET_INC=D:\sdk\SDL_net-1.2.6\include
+SET SDLNET_LIB=D:\sdk\SDL_net-1.2.6\lib
 
 REM ---- SDL_mixer:
 SET SDLMIXER_INC=D:\sdk\SDL_mixer-1.2.7\include
@@ -39,7 +39,7 @@ SET SDLMIXER_LIB=D:\sdk\SDL_mixer-1.2.7\lib
 REM -=- Requirements for drOpenGL.dll -=-
 REM
 REM ---- OpenGL (GL/gl.h, GL/glext.h, GL/glu.h):
-SET GL_INC=D:\VS.NET\Vc7\Include
+SET GL_INC=D:\sdk\OpenGL
 
 REM -=- Requirements for dsOpenAL.dll -=-
 REM
