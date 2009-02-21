276e7de5611b9966124cb15bce812b9cf831c8d0
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2009-02-21 00:38:31 +0000

    Minor changes to snowberry's build.bat

--- snowberry/build.bat
+++ snowberry/build.bat
@@ -4,10 +4,14 @@ REM -- A beta distribution package is compiled by compressing the
 REM -- executable, the necessary resource files, and the Snowberry 
 REM -- scripts into a self-extracting RAR archive.
 
+REM ---- Python Interpreter
+SET PYTHON_DIR=C:\Python25
+
 rd/s/q dist
 
 REM -- Make the executable.
-python setup.py py2exe
+"%PYTHON_DIR%"\python setup.py py2exe
 
 REM -- Additional binary dependencies.
-copy %PYTHON_DIR%\lib\site-packages\wx-2.8-msw-ansi\wx\msvcp71.dll dist
+ECHO Copying dependencies to ./dist...
+copy "%PYTHON_DIR%"\lib\site-packages\wx-2.8-msw-ansi\wx\msvcp71.dll dist
