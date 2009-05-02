0389bb4ffaaa8a905699432687f7437ceba6d03d
danij <danij@3894b877-cc0d-0410-b400-92b7ed5dc389>
2009-05-02 18:12:39 +0000

    Reordered the console output from the PRINT_PROF macro to be more easily readable.

--- doomsday/engine/portable/include/m_profiler.h
+++ doomsday/engine/portable/include/m_profiler.h
@@ -49,10 +49,10 @@ typedef struct profiler_s {
 # define END_PROF_TIMERS()  ,NUM_PROFS }; static profiler_t profiler_[NUM_PROFS];
 # define BEGIN_PROF(x)      (profiler_[x].startCount++, profiler_[x].startTime = Sys_GetRealTime())
 # define END_PROF(x)        (profiler_[x].totalTime += Sys_GetRealTime() - profiler_[x].startTime)
-# define PRINT_PROF(x)      Con_Message(#x ": %i ms (%i starts) [%f ms]\n", \
-                                profiler_[x].totalTime, profiler_[x].startCount, \
+# define PRINT_PROF(x)      Con_Message("[%f ms] " #x ": %i ms (%i starts)\n", \
                                 profiler_[x].startCount? profiler_[x].totalTime / \
-                                (float) profiler_[x].startCount : 0)
+                                (float) profiler_[x].startCount : 0, \
+                                profiler_[x].totalTime, profiler_[x].startCount)
 #else                           // Profiling is disabled.
 # define END_PROF_TIMERS()  ,NUM_PROFS };
 # define BEGIN_PROF(x)
