dea0ecf5d151fd60ac6019706c311d98641678ca
skyjake <skyjake@users.sourceforge.net>
2009-08-24 19:27:13 +0300

    Version bump to beta6.6.

--- doomsday/engine/portable/include/dd_version.h
+++ doomsday/engine/portable/include/dd_version.h
@@ -43,7 +43,7 @@
  * Revision number increases with each small (maintenance) release.
  */
 
-#define DOOMSDAY_RELEASE_NAME   "beta6.5"
+#define DOOMSDAY_RELEASE_NAME   "beta6.6"
 
 /**
  * Version constants.  The Game module can use DOOMSDAY_VERSION to
