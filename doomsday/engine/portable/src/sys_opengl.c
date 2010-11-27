f7cf0552cf9bbeda5161d89e48fd450dc1b4e88f
danij <danij@dengine.net>
2010-11-27 02:11:53 +0000

    Fixed compilation on platforms using SDL window management(?)

--- doomsday/engine/portable/src/sys_opengl.c
+++ doomsday/engine/portable/src/sys_opengl.c
@@ -3,8 +3,8 @@
  * License: GPL
  * Online License Link: http://www.gnu.org/licenses/gpl.html
  *
- *\author Copyright © 2003-2009 Jaakko Keränen <jaakko.keranen@iki.fi>
- *\author Copyright © 2007-2009 Daniel Swanson <danij@dengine.net>
+ *\author Copyright © 2003-2010 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *\author Copyright © 2007-2010 Daniel Swanson <danij@dengine.net>
  *\author Copyright © 2006 Jamie Jones <yagisan@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -56,11 +56,11 @@
 gl_state_t GL_state;
 gl_state_ext_t GL_state_ext;
 
-#ifdef GL_EXT_framebuffer_object
-PFNGLGENERATEMIPMAPEXTPROC glGenerateMipmapEXT;
-#endif
-
 #ifdef WIN32
+# ifdef GL_EXT_framebuffer_object
+PFNGLGENERATEMIPMAPEXTPROC glGenerateMipmapEXT;
+# endif
+
 PFNWGLSWAPINTERVALEXTPROC wglSwapIntervalEXT;
 PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB;
 
