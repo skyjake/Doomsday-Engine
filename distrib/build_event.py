0fc1975bd65af7135f8b5aa1aedfc4124021a968
Jaakko Keränen <jaakko.keranen@iki.fi>
2011-02-28 19:27:56 +0200

    Don't leave uncompressed compiler logs in the master dir.

--- distrib/build_event.py
+++ distrib/build_event.py
@@ -100,9 +100,10 @@ def encoded_build_description(name):
     # Logs.
     msg += '<h2>Build Logs</h2><ul>'
     for f in glob.glob(os.path.join(buildDir, 'build*txt')):
-        os.system('gzip -9c %s > %s' % (f, f+'.gz'))
+        os.system('gzip -9c %s' % f)
+    for f in glob.glob(os.path.join(buildDir, 'build*txt.gz')):
         f = os.path.basename(f)
-        msg += '<li><a href="%s/%s/%s.gz">%s</a></li>' % (FILES_URI, name, f, f)
+        msg += '<li><a href="%s/%s/%s">%s</a></li>' % (FILES_URI, name, f, f)
     msg += '</ul>'
         
     # Changes.
