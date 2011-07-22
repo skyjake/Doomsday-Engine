8f11e8505ae150b2ecdd170e19ea4ee6023ba525
skyjake <skyjake@users.sourceforge.net>
2011-07-22 10:47:43 +0300

    Added missing 'break's to a switch compound

--- doomsday/plugins/jdoom/src/wi_stuff.c
+++ doomsday/plugins/jdoom/src/wi_stuff.c
@@ -1258,6 +1258,7 @@ void WI_Ticker(void)
 #if _DEBUG
         Con_Error("WI_Ticker: Invalid state %i.", (int) inState);
 #endif
+        break;
     }
 }
 
@@ -1458,5 +1459,6 @@ void WI_SetState(interludestate_t st)
 #if _DEBUG
         Con_Error("WI_SetState: Invalid state %i.", (int) st);
 #endif
+        break;
     }
 }
