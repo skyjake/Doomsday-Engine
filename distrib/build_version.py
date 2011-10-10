200530bb142fe420097da41c10090783333131d7
skyjake <skyjake@users.sourceforge.net>
2011-10-10 11:07:51 +0300

    versions.pri: Read version numbers from header files
    The build_version.py script was improved to allow reading
    version number information for all the components directly
    from header files.
    
    When build_version.py is invoked from the command line,
    it expects to be given a header file name as the only argument.
    When imported as a module, it finds the Doomsday version
    and exports it as global variables.
    
    build_number.py was changed so that it only prints the build
    number if given the --print argument. By default it can be
    imported as a module and the todays_build() function returns
    the build number as a string.

--- distrib/build_version.py
+++ distrib/build_version.py
@@ -11,34 +11,49 @@ DOOMSDAY_VERSION_MINOR = 0
 DOOMSDAY_VERSION_REVISION = 0
 DOOMSDAY_RELEASE_TYPE = "Unstable"
 
-def find_version():
-    print "Determining Doomsday version...",
-    
-    versionBase = None
-    versionName = None
+def parse_header_for_version(headerFile):
     versionMajor = 0
     versionMinor = 0
     versionRevision = 0
-    releaseType = "Unstable"
+    versionName = ""
+    releaseType = ""
     
-    f = file(os.path.join(DOOMSDAY_DIR, "engine", "portable", "include", "dd_version.h"), 'rt')
+    f = file(os.path.join(headerFile), 'rt')
     for line in f.readlines():
         line = line.strip()
         if line[:7] != "#define": continue
+        if 'TEXTLONG' in line: continue
         baseAt = line.find("DOOMSDAY_VERSION_BASE")
+        baseOff = 21
+        if baseAt < 0:
+            baseAt = line.find("GAME_VERSION_TEXT")
+            baseOff = 17
+        if baseAt < 0:
+            baseAt = line.find("PLUGIN_VERSION_TEXT")
+            baseOff = 20
         nameAt = line.find("DOOMSDAY_RELEASE_NAME")
         typeAt = line.find("DOOMSDAY_RELEASE_TYPE")
         if baseAt > 0:
-            versionBase = line[baseAt + 21:].replace('\"','').strip()
+            versionBase = line[baseAt + baseOff:].replace('\"','').strip()
             toks = versionBase.split('.')
             versionMajor = toks[0]
             versionMinor = toks[1]
             versionRevision = toks[2]
-
         if nameAt > 0:
             versionName = line[nameAt + 21:].replace('\"','').strip()
         if typeAt > 0:
             releaseType = line[typeAt + 21:].replace('\"','').strip()
+            
+    return (versionMajor, versionMinor, versionRevision, versionName, releaseType)
+
+def find_version(quiet = False):
+    if not quiet: print "Determining Doomsday version...",
+    
+    versionMajor, versionMinor, versionRevision, versionName, releaseType = \
+        parse_header_for_version(os.path.join(DOOMSDAY_DIR, 'engine', 'portable', 'include', 'dd_version.h'))
+    if not releaseType: releaseType = "Unstable"
+
+    versionBase = "%s.%s.%s" % (versionMajor, versionMinor, versionRevision)
 
     global DOOMSDAY_VERSION_FULL
     global DOOMSDAY_VERSION_FULL_PLAIN
@@ -56,4 +71,21 @@ def find_version():
     DOOMSDAY_VERSION_MINOR = versionMinor
     DOOMSDAY_VERSION_REVISION = versionRevision
 
-    print DOOMSDAY_VERSION_FULL + " (%s)" % releaseType
+    if not quiet: print DOOMSDAY_VERSION_FULL + " (%s)" % releaseType
+
+        
+# Invoked from qmake? Returns "version_base buildnum releasetype win32_version_with_buildnum"
+if __name__ == '__main__':
+    import sys
+    import build_number
+    headerName = sys.argv[1]
+    buildNum = build_number.todays_build()
+
+    # Get the Doomsday version. We can use some of this information.
+    find_version(True)
+    
+    major, minor, revision, name, reltype = parse_header_for_version(headerName)
+    if not reltype: reltype = DOOMSDAY_RELEASE_TYPE
+    
+    print "%s.%s.%s %s %s %s,%s,%s,%s" % (major, minor, revision, buildNum, reltype,
+                                          major, minor, revision, buildNum)
