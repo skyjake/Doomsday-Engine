fa8adc133e575d633aa2d985f8607c14ba4a4a3e
skyjake <skyjake@users.sourceforge.net>
2011-11-30 17:16:35 +0200

    Fixing player view angles
    View angles are updated as often as possible (fractional ticks)
    so the latest values should always be used for new frames --
    no smoothing is needed.

--- doomsday/plugins/jhexen/src/hrefresh.c
+++ doomsday/plugins/jhexen/src/hrefresh.c
@@ -147,6 +147,8 @@ static void rendPlayerView(int player)
     player_t* plr = &players[player];
     boolean special200 = false;
     float pspriteOffsetY;
+    int viewAngle;
+    float viewPitch;
 
     if(!plr->plr->mo)
     {
@@ -190,15 +192,16 @@ static void rendPlayerView(int player)
     viewPos[VX] = plr->plr->mo->pos[VX] + plr->viewOffset[VX];
     viewPos[VY] = plr->plr->mo->pos[VY] + plr->viewOffset[VY];
     viewPos[VZ] = plr->viewZ + plr->viewOffset[VZ];
-    viewAngle = plr->plr->mo->angle + (int) (ANGLE_MAX * -G_GetLookOffset(player));
-    viewPitch = plr->plr->lookDir;
 
     DD_SetVariable(DD_VIEW_X, &viewPos[VX]);
     DD_SetVariable(DD_VIEW_Y, &viewPos[VY]);
     DD_SetVariable(DD_VIEW_Z, &viewPos[VZ]);
+    */
+    // View angles are updated with fractional ticks, so we can just use the current values.
     DD_SetVariable(DD_VIEW_ANGLE, &viewAngle);
     DD_SetVariable(DD_VIEW_PITCH, &viewPitch);
-    */
+    viewAngle = plr->plr->mo->angle + (int) (ANGLE_MAX * -G_GetLookOffset(player));
+    viewPitch = plr->plr->lookDir;
 
     pspriteOffsetY = HU_PSpriteYOffset(plr);
     DD_SetVariable(DD_PSPRITE_OFFSET_Y, &pspriteOffsetY);
