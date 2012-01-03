e7c324adddcef4092505d9e2b9196c20d3078a88
skyjake <skyjake@users.sourceforge.net>
2012-01-03 08:11:31 +0200

    PNG: Prevent libpng default error output
    The libpng manual says that a custom error handler should never
    return, so we'll just longjmp past the loader.

--- doomsday/engine/portable/src/gl_png.c
+++ doomsday/engine/portable/src/gl_png.c
@@ -53,6 +53,15 @@ static void setLastError(const char* msg)
 void PNGAPI user_error_fn(png_structp png_ptr, png_const_charp error_msg)
 {
     setLastError(error_msg);
+
+    // From libpng manual:
+    // "Errors handled through png_error() are fatal, meaning that png_error()
+    // should never return to its caller."
+
+    // Longjump out of the png loader (avoids libpng's default error message to stderr).
+    jmp_buf jmpbuf;
+    png_memcpy(jmpbuf, png_ptr->jmpbuf, png_sizeof(jmp_buf));
+    longjmp(jmpbuf, 1);
 }
 
 void PNGAPI user_warning_fn(png_structp png_ptr, png_const_charp warning_msg)
