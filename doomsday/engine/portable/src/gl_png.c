e7cef0c373ae16863719b06a4a0ee932a0677a1d
skyjake <skyjake@users.sourceforge.net>
2012-01-03 08:36:29 +0200

    PNG: API version check for longjmp
    png_longjmp was added in libpng 1.5.

--- doomsday/engine/portable/src/gl_png.c
+++ doomsday/engine/portable/src/gl_png.c
@@ -59,7 +59,13 @@ void PNGAPI user_error_fn(png_structp png_ptr, png_const_charp error_msg)
     // should never return to its caller."
 
     // Longjump out of the png loader (avoids libpng's default error message to stderr).
+#if PNG_LIBPNG_VER_MAJOR > 1 || PNG_LIBPNG_VER_MINOR >= 5
     png_longjmp(png_ptr, 1);
+#else
+    jmp_buf jmpbuf;
+    png_memcpy(jmpbuf, png_ptr->jmpbuf, png_sizeof(jmp_buf));
+    longjmp(jmpbuf, 1);
+#endif
 }
 
 void PNGAPI user_warning_fn(png_structp png_ptr, png_const_charp warning_msg)
