f78362c76010b68f22575967e69dc2b38ff76b20
skyjake <skyjake@users.sourceforge.net>
2012-01-05 08:51:14 +0200

    PNG: Fixed deprecation warnings with libpng 1.4
    longjmp has to be done differently in different versions of libpng.

--- doomsday/engine/portable/src/gl_png.c
+++ doomsday/engine/portable/src/gl_png.c
@@ -59,12 +59,14 @@ void PNGAPI user_error_fn(png_structp png_ptr, png_const_charp error_msg)
     // should never return to its caller."
 
     // Longjump out of the png loader (avoids libpng's default error message to stderr).
-#if PNG_LIBPNG_VER_MAJOR > 1 || PNG_LIBPNG_VER_MINOR >= 5
+#if PNG_LIBPNG_VER >= 10500
     png_longjmp(png_ptr, 1);
 #else
-    jmp_buf jmpbuf;
-    memcpy(jmpbuf, png_ptr->jmpbuf, sizeof(jmp_buf));
-    longjmp(jmpbuf, 1);
+# ifdef png_jmpbuf
+    longjmp(png_jmpbuf(png_ptr), 1);
+# else
+    longjmp(png_ptr->jmpbuf, 1);
+# endif
 #endif
 }
 
