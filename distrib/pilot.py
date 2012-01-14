65e1aa4bd208c97cf7747f84778eddf7301d4f39
skyjake <skyjake@users.sourceforge.net>
2012-01-14 06:59:05 +0200

    Builder: Copyright update

--- distrib/pilot.py
+++ distrib/pilot.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 #
 # The Doomsday Build Pilot
-# (c) 2011 Jaakko Keränen <jaakko.keranen@iki.fi>
+# (c) 2011-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
