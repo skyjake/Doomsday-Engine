cf2f25d0fe1293151e9ffb0dcb0f014a62ea5f85
skyjake <skyjake@users.sourceforge.net>
2012-01-19 11:25:22 +0200

    Bump protocol version number
    However, the protocol version should be checked already in
    unjoined mode, when the client is querying info about a
    server. The joined mode protocol has been changed from
    the ground up, so the current way of checking the number
    after the handshake will not work.

--- doomsday/engine/portable/include/sv_def.h
+++ doomsday/engine/portable/include/sv_def.h
@@ -31,8 +31,8 @@
 
 #include "dd_def.h"
 
-#define SV_VERSION          18
-#define SV_WELCOME_STRING   "Doomsday "DOOMSDAY_VERSION_TEXT" Server (R18)"
+#define SV_VERSION          19
+#define SV_WELCOME_STRING   "Doomsday "DOOMSDAY_VERSION_TEXT" Server (R19)"
 
 // Anything closer than this is always taken into consideration when
 // deltas are being generated.
