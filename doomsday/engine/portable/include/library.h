98a7ffd95f1220fcf9789168d71f79b01d678740
skyjake <skyjake@users.sourceforge.net>
2012-02-04 17:50:07 +0200

    Documentation: Updated apidoc for the Library class

--- doomsday/engine/portable/include/library.h
+++ doomsday/engine/portable/include/library.h
@@ -1,33 +1,35 @@
-/**\file library.h
- *\section License
- * License: GPL
- * Online License Link: http://www.gnu.org/licenses/gpl.html
- *
- *\author Copyright © 2006-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
- *\author Copyright © 2009-2012 Daniel Swanson <danij@dengine.net>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA  02110-1301  USA
- */
-
 /**
- * Dynamic Libraries.
+ * @file library.h
+ * Dynamic libraries. @ingroup base
  *
- * These functions provide roughly the same functionality as the ltdl
- * library.  Since the ltdl library appears to be broken on Mac OS X,
- * these will be used instead when loading plugin libraries.
+ * These functions provide roughly the same functionality as the ltdl library.
+ * Since the ltdl library appears to be broken on Mac OS X, these will be used
+ * instead when loading plugin libraries.
+ *
+ * During startup Doomsday loads multiple game plugins. However, only one can
+ * exist in memory at a time because they contain many of the same globally
+ * visible symbols. When a game is started, all game plugins are first released
+ * from memory after which the chosen game plugin is reloaded (see
+ * Library_ReleaseGames()).
+ *
+ * @todo Implement and use this class for Windows.
+ *
+ * @authors Copyright © 2006-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * @authors Copyright © 2009-2012 Daniel Swanson <danij@dengine.net>
+ *
+ * @par License
+ * GPL: http://www.gnu.org/licenses/gpl.html
+ *
+ * <small>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details. You should have received a copy of the GNU
+ * General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA</small>
  */
 
 #ifndef LIBDENG_SYSTEM_UTILS_DYNAMIC_LIBRARY_H
