9afd16e1ffabf4cbd3348bb3c75c8e2daa9be8e8
danij <danij@dengine.net>
2012-03-07 16:30:12 +0000

    Cleaned up bias light editor implementation

--- doomsday/engine/portable/include/edit_bias.h
+++ doomsday/engine/portable/include/edit_bias.h
@@ -1,41 +1,44 @@
-/**\file
- *\section License
- * License: GPL
- * Online License Link: http://www.gnu.org/licenses/gpl.html
- *
- *\author Copyright © 2003-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
- *\author Copyright © 2006-2012 Daniel Swanson <danij@dengine.net>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA  02110-1301  USA
- */
-
 /**
- * edit_bias.h: BIAS lighting editor.
+ * @file edit_bias.h
+ * Bias light source editor. @ingroup base
+ *
+ * @authors Copyright &copy; 2006-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * @authors Copyright &copy; 2006-2012 Daniel Swanson <danij@dengine.net>
+ *
+ * @par License
+ * GPL: http://www.gnu.org/licenses/gpl.html
+ *
+ * <small>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details. You should have received a copy of the GNU
+ * General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA</small>
  */
 
-#ifndef __DOOMSDAY_BIAS_EDITOR__
-#define __DOOMSDAY_BIAS_EDITOR__
+#ifndef LIBDENG_BIAS_EDITOR
+#define LIBDENG_BIAS_EDITOR
 
 #include "edit_bias.h"
 
-void            SBE_Register(void);
-void            SBE_EndFrame(void);
+/**
+ * Register all console commands and variables for this module.
+ */
+void SBE_Register(void);
 
-void            SBE_DrawCursor(void);
-void            SBE_DrawHUD(void);
-boolean         SBE_UsingHueCircle(void);
+void SBE_EndFrame(void);
 
-#endif
+void SBE_DrawCursor(void);
+
+void SBE_DrawHUD(void);
+
+/**
+ * @return  @c true iff the console player is currently using the HueCircle.
+ */
+boolean SBE_UsingHueCircle(void);
+
+#endif /// LIBDENG_BIAS_EDITOR
