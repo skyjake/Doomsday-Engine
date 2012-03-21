9ce4f4f9f2cc999f87f3dfe90d6761fea6982a17
skyjake <skyjake@users.sourceforge.net>
2012-03-21 17:17:37 +0200

    Win32: Translate virtual keys to DDKEYs
    Using the translation table found in sys_console.c.

--- doomsday/engine/portable/src/keycode.cpp
+++ doomsday/engine/portable/src/keycode.cpp
@@ -21,10 +21,153 @@
  * 02110-1301 USA</small>
  */
 
-#include "keycode.h"
-#include "dd_share.h"
 #include <QKeyEvent>
 #include <QDebug>
+
+#ifdef WIN32
+#  include "de_platform.h"
+#endif
+
+#include "keycode.h"
+#include "dd_share.h"
+
+#ifdef WIN32
+static int win32Keymap[256];
+
+/**
+ * Initialize the Windows virtual key => DDKEY translation map.
+ */
+void checkWin32Keymap()
+{
+    if(win32Keymap[VK_BACK] == DDKEY_BACKSPACE)
+    {
+        // Already go it.
+        return;
+    }
+
+    int* keymap = win32Keymap;
+
+    keymap[VK_BACK] = DDKEY_BACKSPACE; // Backspace
+    keymap[VK_TAB ] = DDKEY_TAB;
+    //keymap[VK_CLEAR] = ;
+    keymap[VK_RETURN] = DDKEY_RETURN;
+    keymap[VK_SHIFT] = DDKEY_RSHIFT;
+    keymap[VK_CONTROL] = DDKEY_RCTRL;
+    keymap[VK_MENU] = DDKEY_RALT;
+    keymap[VK_PAUSE] = DDKEY_PAUSE;
+    keymap[VK_CAPITAL] = DDKEY_CAPSLOCK;
+    //keymap[VK_KANA] = ;
+    //keymap[VK_HANGEUL] = ;
+    //keymap[VK_HANGUL] = ;
+    //keymap[VK_JUNJA] = ;
+    //keymap[VK_FINAL] = ;
+    //keymap[VK_HANJA] = ;
+    //keymap[VK_KANJI] = ;
+    keymap[VK_ESCAPE] = DDKEY_ESCAPE;
+    //keymap[VK_CONVERT] = ;
+    //keymap[VK_NONCONVERT] = ;
+    //keymap[VK_ACCEPT] = ;
+    //keymap[VK_MODECHANGE] = ;
+    keymap[VK_SPACE] = ' ';
+    keymap[VK_OEM_PLUS] = '+';
+    keymap[VK_OEM_COMMA] = ',';
+    keymap[VK_OEM_MINUS] = '-';
+    keymap[VK_OEM_PERIOD] = '.';
+    keymap[VK_OEM_1] = ';';
+    keymap[VK_OEM_2] = '/';
+    keymap[VK_OEM_3] = '\'';
+    keymap[VK_OEM_4] = '[';
+    keymap[VK_OEM_5] = DDKEY_BACKSLASH;
+    keymap[VK_OEM_6] = ']';
+    keymap[VK_OEM_7] = '#';
+    keymap[VK_OEM_8] = '`';
+    keymap[VK_PRIOR] = DDKEY_PGUP;
+    keymap[VK_NEXT] = DDKEY_PGDN;
+    keymap[VK_END] = DDKEY_END;
+    keymap[VK_HOME] = DDKEY_HOME;
+    keymap[VK_LEFT] = DDKEY_LEFTARROW;
+    keymap[VK_UP] = DDKEY_UPARROW;
+    keymap[VK_RIGHT] = DDKEY_RIGHTARROW;
+    keymap[VK_DOWN] = DDKEY_DOWNARROW;
+    //keymap[VK_SELECT] = ;
+    //keymap[VK_PRINT] = ;
+    //keymap[VK_EXECUTE] = ;
+    //keymap[VK_SNAPSHOT] = ;
+    keymap[VK_INSERT] = DDKEY_INS;
+    keymap[VK_DELETE] = DDKEY_DEL;
+    //keymap[VK_HELP] = ;
+    //keymap[VK_LWIN] = ;
+    //keymap[VK_RWIN] = ;
+    //keymap[VK_APPS] = ;
+    //keymap[VK_SLEEP] = ;
+    keymap[VK_NUMPAD0] = DDKEY_NUMPAD0;
+    keymap[VK_NUMPAD1] = DDKEY_NUMPAD1;
+    keymap[VK_NUMPAD2] = DDKEY_NUMPAD2;
+    keymap[VK_NUMPAD3] = DDKEY_NUMPAD3;
+    keymap[VK_NUMPAD4] = DDKEY_NUMPAD4;
+    keymap[VK_NUMPAD5] = DDKEY_NUMPAD5;
+    keymap[VK_NUMPAD6] = DDKEY_NUMPAD6;
+    keymap[VK_NUMPAD7] = DDKEY_NUMPAD7;
+    keymap[VK_NUMPAD8] = DDKEY_NUMPAD8;
+    keymap[VK_NUMPAD9] = DDKEY_NUMPAD9;
+    keymap[VK_MULTIPLY] = '*';
+    keymap[VK_ADD] = DDKEY_ADD;
+    //keymap[VK_SEPARATOR] = ;
+    keymap[VK_SUBTRACT] = DDKEY_SUBTRACT;
+    keymap[VK_DECIMAL] = DDKEY_DECIMAL;
+    keymap[VK_DIVIDE] = '/';
+    keymap[VK_F1] = DDKEY_F1;
+    keymap[VK_F2] = DDKEY_F2;
+    keymap[VK_F3] = DDKEY_F3;
+    keymap[VK_F4] = DDKEY_F4;
+    keymap[VK_F5] = DDKEY_F5;
+    keymap[VK_F6] = DDKEY_F6;
+    keymap[VK_F7] = DDKEY_F7;
+    keymap[VK_F8] = DDKEY_F8;
+    keymap[VK_F9] = DDKEY_F9;
+    keymap[VK_F10] = DDKEY_F10;
+    keymap[VK_F11] = DDKEY_F11;
+    keymap[VK_F12] = DDKEY_F12;
+    keymap[VK_SNAPSHOT] = DDKEY_PRINT;
+
+    keymap[0x30] = '0';
+    keymap[0x31] = '1';
+    keymap[0x32] = '2';
+    keymap[0x33] = '3';
+    keymap[0x34] = '4';
+    keymap[0x35] = '5';
+    keymap[0x36] = '6';
+    keymap[0x37] = '7';
+    keymap[0x38] = '8';
+    keymap[0x39] = '9';
+    keymap[0x41] = 'a';
+    keymap[0x42] = 'b';
+    keymap[0x43] = 'c';
+    keymap[0x44] = 'd';
+    keymap[0x45] = 'e';
+    keymap[0x46] = 'f';
+    keymap[0x47] = 'g';
+    keymap[0x48] = 'h';
+    keymap[0x49] = 'i';
+    keymap[0x4A] = 'j';
+    keymap[0x4B] = 'k';
+    keymap[0x4C] = 'l';
+    keymap[0x4D] = 'm';
+    keymap[0x4E] = 'n';
+    keymap[0x4F] = 'o';
+    keymap[0x50] = 'p';
+    keymap[0x51] = 'q';
+    keymap[0x52] = 'r';
+    keymap[0x53] = 's';
+    keymap[0x54] = 't';
+    keymap[0x55] = 'u';
+    keymap[0x56] = 'v';
+    keymap[0x57] = 'w';
+    keymap[0x58] = 'x';
+    keymap[0x59] = 'y';
+    keymap[0x5A] = 'z';
+}
+#endif // WIN32
 
 int Keycode_TranslateFromQt(int qtKey, int nativeVirtualKey)
 {   
@@ -90,6 +233,15 @@ int Keycode_TranslateFromQt(int qtKey, int nativeVirtualKey)
     /// We'll have to use the native virtual keys to make a distinction, e.g.,
     /// between the number row and the keypad. These are the real physical keys
     /// -- the insertion text is provided outside this mapping.
+
+#ifdef WIN32
+    checkWin32Keymap();
+    if(win32Keymap[nativeVirtualKey] > 0)
+    {
+        // We know a mapping for this.
+        return win32Keymap[nativeVirtualKey];
+    }
+#endif
 
 #ifdef MACOSX
     switch(nativeVirtualKey)
