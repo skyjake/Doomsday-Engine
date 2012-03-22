5d395c7d01b1f9d6d82576bac5378dc2137b0a8b
skyjake <skyjake@users.sourceforge.net>
2012-03-22 19:08:51 +0200

    DisplayMode|Documentation: Updated apidoc for the DisplayMode class

--- doomsday/engine/portable/include/displaymode.h
+++ doomsday/engine/portable/include/displaymode.h
@@ -44,7 +44,9 @@ typedef struct displaymode_s {
 
 /**
  * Initializes the DisplayMode class. Enumerates all available display modes and
- * saves the current display mode.
+ * saves the current display mode. Must be called at engine startup.
+ *
+ * @return @c true, if display modes were initialized successfully.
  */
 int DisplayMode_Init(void);
 
@@ -54,18 +56,62 @@ int DisplayMode_Init(void);
  */
 void DisplayMode_Shutdown(void);
 
+/**
+ * Returns the display mode that was in use when DisplayMode_Init() was called.
+ */
 const DisplayMode* DisplayMode_OriginalMode(void);
 
+/**
+ * Returns the current display mode.
+ */
 const DisplayMode* DisplayMode_Current(void);
 
+/**
+ * Returns the number of available display modes.
+ */
 int DisplayMode_Count(void);
 
+/**
+ * Returns one of the available display modes. Use DisplayMode_Count() to
+ * determine how many modes are available.
+ *
+ * @param index  Index of the mode, must be between 0 and DisplayMode_Count() - 1.
+ */
 const DisplayMode* DisplayMode_ByIndex(int index);
 
+/**
+ * Finds the closest available mode to the given criteria.
+ *
+ * @param width  Width in pixels.
+ * @param height  Height in pixels.
+ * @param depth  Color depth (bits per color component).
+ * @param freq  Refresh rate. If zero, prefers rates closest to the mode at startup time.
+ *
+ * @return Mode that most closely matches the criteria. Always returns one of
+ * the available modes; returns @c NULL only if DisplayMode_Init() has not yet
+ * been called.
+ */
 const DisplayMode* DisplayMode_FindClosest(int width, int height, int depth, float freq);
 
+/**
+ * Determines if two display modes are equivalent.
+ *
+ * @param a  DisplayMode instance.
+ * @param b  DisplayMode instance.
+ *
+ * @return  @c true or @c false.
+ */
 boolean DisplayMode_IsEqual(const DisplayMode* a, const DisplayMode* b);
 
+/**
+ * Changes the display mode.
+ *
+ * @param mode  Mode to change to. Must be one of the modes returned by the functions in displaymode.h.
+ * @param shouldCapture  @c true, if the mode is intended to capture the entire display.
+ *
+ * @return @c true, if a mode change occurred. @c false, otherwise (bad mode or
+ * when attempting to change to the current mode).
+ */
 int DisplayMode_Change(const DisplayMode* mode, boolean shouldCapture);
 
 #ifdef __cplusplus
