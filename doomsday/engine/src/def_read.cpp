df1fa9071e72265457133e7270c26d64e63c1e75
skyjake <skyjake@users.sf.net>
2012-11-14 19:06:39 +0200

    Updated copyright information

--- doomsday/engine/src/def_read.cpp
+++ doomsday/engine/src/def_read.cpp
@@ -18,8 +18,8 @@
  * a bunch of tokens, and the same parsing rules would be applied for
  * everything.
  *
+ * @author Copyright &copy; 2003-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
  * @author Copyright &copy; 2010-2012 Daniel Swanson <danij@dengine.net>
- * @author Copyright &copy; 2010-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
  *
  * @par License
  * GPL: http://www.gnu.org/licenses/gpl.html
