d5748a98ba86465d3b50b041cfe799973d1344f6
skyjake <skyjake@users.sf.net>
2012-12-11 19:24:19 +0200

    libdeng2: Standardizing log message style

--- doomsday/libdeng2/src/core/library.cpp
+++ doomsday/libdeng2/src/core/library.cpp
@@ -66,7 +66,7 @@ Library::Library(NativePath const &nativePath) : d(0)
     d = new Instance;
 
     LOG_AS("Library");
-    LOG_TRACE("Loading ") << nativePath.pretty();
+    LOG_TRACE("Loading \"%s\"") << nativePath.pretty();
 
 #ifndef DENG2_USE_DLOPEN
     d->library = new QLibrary(nativePath);
@@ -109,7 +109,7 @@ Library::~Library()
     if(d->library)
     {
         LOG_AS("~Library");
-        LOG_TRACE("Unloading ") << d->nativePath().pretty();
+        LOG_TRACE("Unloading \"%s\"") << d->nativePath().pretty();
 
         // Automatically call the shutdown function, if one exists.
         if(d->type.beginsWith("deng-plugin/") && hasSymbol("deng_ShutdownPlugin"))
