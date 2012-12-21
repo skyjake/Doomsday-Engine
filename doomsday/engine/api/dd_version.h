c91aaaf59210451c950d2db4072ee5fd7ac33d79
skyjake <skyjake@users.sf.net>
2012-12-21 15:10:22 +0200

    Bumped minor version from 9 to 10 (=> 1.10.0)
    Old meaning of version components:
    - 1: major
    - 9: minor
    - 10: revision
    - 0: patch
    
    Updated new meaning, dropping down to three components:
    - 1: major (large architectural changes)
    - 10: minor (normal releases on the roadmap)
    - 0: patch (extra releases for fixes)

--- doomsday/engine/api/dd_version.h
+++ doomsday/engine/api/dd_version.h
@@ -49,17 +49,17 @@ extern "C" {
  * Revision number increases with each small (maintenance) release.
  */
 
-#define DOOMSDAY_VERSION_BASE   "1.9.10"
-#define DOOMSDAY_VERSION_NUMBER 1,9,10,0 // For WIN32 version info.
+#define DOOMSDAY_VERSION_BASE   "1.10.0"
+#define DOOMSDAY_VERSION_NUMBER 1,10,0,0 // For WIN32 version info.
 //#define DOOMSDAY_RELEASE_NAME   "Example"
 
 /**
  * DOOMSDAY_RELEASE_TYPE determines the classification of the release.
  * Possible values are "Unstable", "Candidate" and "Stable".
  */
-//#define DOOMSDAY_RELEASE_TYPE   "Unstable"
+#define DOOMSDAY_RELEASE_TYPE   "Unstable"
 //#define DOOMSDAY_RELEASE_TYPE   "Candidate"
-#define DOOMSDAY_RELEASE_TYPE   "Stable"
+//#define DOOMSDAY_RELEASE_TYPE   "Stable"
 
 /**
  * Copyright string is used in WIN32 binary metadata markup.
