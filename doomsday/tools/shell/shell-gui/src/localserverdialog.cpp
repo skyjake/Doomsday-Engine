7d8e0c4b73eea76da2889ec1219f286fbc766ce9
skyjake <skyjake@users.sf.net>
2013-02-13 20:02:09 +0200

    Shell|GUI: Inform user about an invalid port number

--- doomsday/tools/shell/shell-gui/src/localserverdialog.cpp
+++ doomsday/tools/shell/shell-gui/src/localserverdialog.cpp
@@ -80,8 +80,8 @@ DENG2_PIMPL(LocalServerDialog)
         port->setMinimumWidth(80);
         port->setMaximumWidth(80);
         port->setText(QString::number(st.value("LocalServer/port", 13209).toInt()));
-        port->setToolTip(tr("Port must be between 0 and 65535."));
-        portMsg = new QLabel(tr("Port already in use."));
+        port->setToolTip(tr("The default port is 13209."));
+        portMsg = new QLabel;
         QPalette pal = portMsg->palette();
         pal.setColor(portMsg->foregroundRole(), Qt::red);
         portMsg->setPalette(pal);
@@ -178,19 +178,25 @@ void LocalServerDialog::validate()
     if(txt.isEmpty() || port < 0 || port >= 0x10000)
     {
         isValid = false;
+        d->portMsg->setText(tr("Must be between 0 and 65535."));
+        d->portMsg->show();
     }
-
-    // Check known running servers.
-    bool inUse = false;
-    foreach(Address const &sv, GuiShellApp::app().serverFinder().foundServers())
+    else
     {
-        if(Socket::isHostLocal(sv.host()) && sv.port() == port)
+        // Check known running servers.
+        bool inUse = false;
+        foreach(Address const &sv, GuiShellApp::app().serverFinder().foundServers())
         {
-            isValid = false;
-            inUse = true;
+            if(Socket::isHostLocal(sv.host()) && sv.port() == port)
+            {
+                isValid = false;
+                inUse = true;
+                d->portMsg->setText(tr("Port already in use."));
+                break;
+            }
         }
+        d->portMsg->setVisible(inUse);
     }
-    d->portMsg->setVisible(inUse);
 
     if(d->runtime->path().isEmpty()) isValid = false;
 
