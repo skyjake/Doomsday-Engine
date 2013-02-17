ef809128997e5ba433585c28f13929db83656fbe
skyjake <skyjake@users.sourceforge.net>
2013-02-17 13:29:32 +0200

    Builder|Fixed: Mac OS X distribution file names

--- distrib/builder/event.py
+++ distrib/builder/event.py
@@ -35,12 +35,20 @@ class Event:
         self.packageName = {'doomsday': 'Doomsday',
                             'fmod':     'FMOD Ex Audio Plugin'}
         
-        # Platforms:  Name                            File ext     sys_id()
-        self.oses = [('Windows (x86)',                '.exe',      'win32-32bit'),
-                     ('Mac OS X 10.4+ (ppc/i386)',    '32bit.dmg', 'darwin-32bit'),
-                     ('Mac OS X 10.6+ (x86_64/i386)', '.dmg',      'darwin-64bit'),
-                     ('Ubuntu (x86)',                 'i386.deb',  'linux2-32bit'),
-                     ('Ubuntu (x86_64)',              'amd64.deb', 'linux2-64bit')]
+        if self.num >= 778: # Mac distribution naming was changed.
+            # Platforms:  Name                            File ext     sys_id()
+            self.oses = [('Windows (x86)',                '.exe',      'win32-32bit'),
+                         ('Mac OS X 10.6+ (x86_64/i386)', '.dmg',      'darwin-64bit'),
+                         ('Mac OS X 10.4+ (ppc/i386)',    '32bit.dmg', 'darwin-32bit'),
+                         ('Ubuntu (x86_64)',              'amd64.deb', 'linux2-64bit'),
+                         ('Ubuntu (x86)',                 'i386.deb',  'linux2-32bit')]
+        else:
+            # Platforms:  Name                            File ext     sys_id()
+            self.oses = [('Windows (x86)',                '.exe',      'win32-32bit'),
+                         ('Mac OS X 10.4+ (ppc/i386)',    '.dmg',      'darwin-32bit'),
+                         ('Mac OS X 10.6+ (x86_64/i386)', '64bit.dmg', 'darwin-64bit'),
+                         ('Ubuntu (x86)',                 'i386.deb',  'linux2-32bit'),
+                         ('Ubuntu (x86_64)',              'amd64.deb', 'linux2-64bit')]            
 
         self.platId = {'win32-32bit':  'win-x86',
                        'darwin-32bit': 'mac10_4-x86-ppc',
