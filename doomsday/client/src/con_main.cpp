skyjake/Doomsday-Engine

commit 43414ec10be77f1e9c6dc3c0cc5752caa924b318

Fixed: Main window destruction upon fatal error
The main window was destroyed behind the back of the window manager.

doomsday/client/src/con_main.cpp
@@ -2082,7 +2082,7 @@ void Con_AbnormalShutdown(char const *message)
     Window *mainWindow = Window::main();
     DENG_ASSERT(mainWindow != 0);
     mainWindow->trapMouse(false);
-    delete mainWindow; mainWindow = 0;
+
     DENG2_GUI_APP->loop().pause();
 #endif
 

doomsday/client/src/dd_main.cpp
@@ -1747,7 +1747,7 @@ int DD_EarlyInit()
 
 #ifdef __CLIENT__
     // Bring the window manager online.
-    Window::intialize();
+    Window::initialize();
 #endif
 
     // Instantiate the Games collection.

commit 8f1d2e77547da3d09b115e1af12c3a623bfb5c72

Fixed|Window Manager: Crash during abnormal shutdown message
The main window must be deleted (or its drawing prevented) or otherwise
a crash will occur while the shutdown message is being shown.

Fixes regression introduced by https://github.com/skyjake/Doomsday-Engine/commit/43414ec10be77f1e9c6dc3c0cc5752caa924b318

doomsday/client/src/con_main.cpp
@@ -2080,6 +2080,11 @@ void Con_AbnormalShutdown(char const *message)
     Window::main().trapMouse(false);
 
     DENG2_GUI_APP->loop().pause();
+
+    // This is an abnormal shutdown, we cannot continue drawing
+    // any of the windows. (Alternatively could hide/disable drawing
+    // of the windows.)
+    Window::shutdown();
 #endif
 
     if(message) // Only show if a message given.