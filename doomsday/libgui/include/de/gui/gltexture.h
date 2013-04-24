c9683c766392527db49b6da495914743080a294c
skyjake <skyjake@users.sf.net>
2013-04-24 21:34:29 +0300

    libgui|GLTexture: 2D textures, cube maps, automatic/manual mipmaps
    Implemented GLTexture with management of 2D textures and cube maps.
    Image content can be uploaded to any mipmap level. Mipmaps can be
    generated automatically, by request, or set up entirely manually.

--- doomsday/libgui/include/de/gui/gltexture.h
+++ doomsday/libgui/include/de/gui/gltexture.h
@@ -25,13 +25,39 @@
 
 #include "libgui.h"
 #include "opengl.h"
+#include "../Image"
 
 namespace de {
+
+namespace gl {
+    enum Filter {
+        Nearest,
+        Linear
+    };
+    enum MipFilter {
+        MipNone,
+        MipNearest,
+        MipLinear
+    };
+    enum Wrapping {
+        Repeat,
+        RepeatMirrored,
+        ClampToEdge
+    };
+    enum CubeFace {
+        PositiveX,
+        NegativeX,
+        PositiveY,
+        NegativeY,
+        PositiveZ,
+        NegativeZ
+    };
+}
 
 /**
  * GL texture.
  *
- * @todo Support for cube textures (6 faces instead of one).
+ * Supports cube maps: 6 faces instead of one.
  *
  * @ingroup gl
  */
@@ -39,18 +65,144 @@ class LIBGUI_PUBLIC GLTexture : public Asset
 {
 public:
     typedef Vector2ui Size;
+    typedef Vector2<gl::Wrapping> Wraps;
 
 public:
+    /**
+     * Constructs a texture without any content. GLTexture instances are not
+     * ready until they have some content defined.
+     */
     GLTexture();
+
+    /**
+     * Release all image content associated with the texture.
+     */
+    void clear();
+
+    void setMagFilter(gl::Filter magFilter);
+    void setMinFilter(gl::Filter minFilter, gl::MipFilter mipFilter);
+    void setFilter(gl::Filter magFilter, gl::Filter minFilter, gl::MipFilter mipFilter) {
+        setMagFilter(magFilter);
+        setMinFilter(minFilter, mipFilter);
+    }
+    void setWrapS(gl::Wrapping mode);
+    void setWrapT(gl::Wrapping mode);
+    inline void setWrap(gl::Wrapping s, gl::Wrapping t) {
+        setWrapS(s);
+        setWrapT(t);
+    }
+    inline void setWrap(Wraps const &st) {
+        setWrapS(st.x);
+        setWrapT(st.y);
+    }
+
+    gl::Filter minFilter() const;
+    gl::Filter magFilter() const;
+    gl::MipFilter mipFilter() const;
+    gl::Wrapping wrapS() const;
+    gl::Wrapping wrapT() const;
+    Wraps wrap() const;
+
+    bool isCubeMap() const;
+
+    /**
+     * Enables or disables automatic mipmap generation on the texture.
+     *
+     * @param genMips  @c true to generate mipmaps whenever level 0 changes.
+     */
+    void setAutoGenMips(bool genMips);
+
+    bool autoGenMips() const;
+
+    /**
+     * Reserves a specific size of undefined memory for a level.
+     *
+     * @param size    Size in texels.
+     * @param format  Pixel format that will be later used for uploading content.
+     *                Determines internal storage pixel format.
+     * @param level   Mipmap level.
+     */
+    void setUndefinedImage(Size const &size, Image::Format format, int level = 0);
+
+    /**
+     * Reserves a specific size of undefined memory for a cube map level.
+     *
+     * @param face   Face of a cube map.
+     * @param size   Size in texels.
+     * @param level  Mipmap level.
+     */
+    void setUndefinedImage(gl::CubeFace face, Size const &size, Image::Format format, int level = 0);
+
+    /**
+     * Sets the image content of the texture at a particular level. The format
+     * of the image determines which GL format is chosen for the texture.
+     *
+     * @param image  Image to upload to a GL texture.
+     * @param level  Level on which to store the image.
+     */
+    void setImage(Image const &image, int level = 0);
+
+    /**
+     * Sets the image content of the texture at a particular level. The format
+     * of the image determines which GL format is chosen for the texture.
+     *
+     * @param face   Face of a cube map.
+     * @param image  Image to upload to a GL texture.
+     * @param level  Level on which to store the image.
+     */
+    void setImage(gl::CubeFace face, Image const &image, int level = 0);
+
+    /**
+     * Replaces a portion of existing content. The image should be provided in
+     * the same format as the previous full content.
+     *
+     * @param image  Image to copy.
+     * @param pos    Position where the image is being copied.
+     * @param level  Mipmap level.
+     */
+    void setSubImage(Image const &image, Vector2i const &pos, int level = 0);
+
+    void setSubImage(gl::CubeFace face, Image const &image, Vector2i const &pos, int level = 0);
+
+    /**
+     * Generate a full set of mipmap levels based on the content on level 0.
+     * Automatic mipmap generation can be enabled with setAutoGenMips().
+     */
+    void generateMipmap();
 
     /**
      * Returns the size of the texture (mipmap level 0).
      */
     Size size() const;
 
+    /**
+     * Returns the number of mipmap levels.
+     */
+    int mipLevels() const;
+
+    /**
+     * Returns the size of a particular mipmap level.
+     * @param level  Mip level.
+     * @return Size in texels. (0,0) for an invalid level.
+     */
+    Size levelSize(int level) const;
+
     GLuint glName() const;
 
-    void glBindToUnit(int unit);
+    void glBindToUnit(int unit) const;
+
+public:
+    /**
+     * Calculates how many mipmap levels are produced for a specific size
+     * of mipmap level 0 content.
+     *
+     * @param size  Size in texels at level 0.
+     *
+     * @return Number of mipmap levels.
+     */
+    static int levelsForSize(Size const &size);
+
+    static Size levelSize(Size const &size0, int level);
 
 private:
     DENG2_PRIVATE(d)
--- doomsday/libgui/src/gltexture.cpp
+++ doomsday/libgui/src/gltexture.cpp
@@ -21,12 +21,39 @@
 
 namespace de {
 
+namespace internal
+{
+    enum TextureFlag {
+        AutoMips,
+        MipmapAvailable,
+        ParamsChanged
+    };
+    Q_DECLARE_FLAGS(TextureFlags, TextureFlag)
+}
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(internal::TextureFlags)
+
+using namespace internal;
+using namespace gl;
+
 DENG2_PIMPL(GLTexture)
 {
-    GLuint name;
     Size size;
+    GLuint name;
+    GLenum texTarget;
+    Filter minFilter;
+    Filter magFilter;
+    MipFilter mipFilter;
+    Wraps wrap;
+    TextureFlags flags;
 
-    Instance(Public *i) : Base(i), name(0)
+    Instance(Public *i)
+        : Base(i),
+          name(0),
+          texTarget(GL_TEXTURE_2D),
+          minFilter(Linear), magFilter(Linear), mipFilter(MipNone),
+          wrap(Wraps(Repeat, Repeat)),
+          flags(ParamsChanged)
     {}
 
     ~Instance()
@@ -50,14 +77,305 @@ DENG2_PIMPL(GLTexture)
             name = 0;
         }
     }
+
+    void clear()
+    {
+        release();
+        size = Size();
+        texTarget = GL_TEXTURE_2D;
+        self.setState(NotReady);
+    }
+
+    bool isCube() const
+    {
+        return texTarget == GL_TEXTURE_CUBE_MAP;
+    }
+
+    static GLenum glWrap(gl::Wrapping w)
+    {
+        switch(w)
+        {
+        case Repeat:         return GL_REPEAT;
+        case RepeatMirrored: return GL_MIRRORED_REPEAT;
+        case ClampToEdge:    return GL_CLAMP_TO_EDGE;
+        }
+        return GL_REPEAT;
+    }
+
+    static GLenum glMinFilter(gl::Filter min, gl::MipFilter mip)
+    {
+        if(mip == MipNone)
+        {
+            if(min == Nearest) return GL_NEAREST;
+            if(min == Linear)  return GL_LINEAR;
+        }
+        else if(mip == MipNearest)
+        {
+            if(min == Nearest) return GL_NEAREST_MIPMAP_NEAREST;
+            if(min == Linear)  return GL_LINEAR_MIPMAP_NEAREST;
+        }
+        else // MipLinear
+        {
+            if(min == Nearest) return GL_NEAREST_MIPMAP_LINEAR;
+            if(min == Linear)  return GL_LINEAR_MIPMAP_LINEAR;
+        }
+        return GL_NEAREST;
+    }
+
+    static GLenum glFace(gl::CubeFace face)
+    {
+        switch(face)
+        {
+        case PositiveX: return GL_TEXTURE_CUBE_MAP_POSITIVE_X;
+        case PositiveY: return GL_TEXTURE_CUBE_MAP_POSITIVE_Y;
+        case PositiveZ: return GL_TEXTURE_CUBE_MAP_POSITIVE_Z;
+        case NegativeX: return GL_TEXTURE_CUBE_MAP_NEGATIVE_X;
+        case NegativeY: return GL_TEXTURE_CUBE_MAP_NEGATIVE_Y;
+        case NegativeZ: return GL_TEXTURE_CUBE_MAP_NEGATIVE_Z;
+        }
+        return GL_TEXTURE_CUBE_MAP_POSITIVE_X;
+    }
+
+    void glBind() const
+    {
+        glBindTexture(texTarget, self.isReady()? name : 0);
+    }
+
+    void glUnbind() const
+    {
+        glBindTexture(texTarget, 0);
+    }
+
+    /**
+     * Update the OpenGL texture parameters. You must bind the texture before
+     * calling.
+     */
+    void glUpdateParamsOfBoundTexture()
+    {
+        glTexParameteri(texTarget, GL_TEXTURE_WRAP_S,     glWrap(wrap.x));
+        glTexParameteri(texTarget, GL_TEXTURE_WRAP_T,     glWrap(wrap.y));
+        glTexParameteri(texTarget, GL_TEXTURE_MAG_FILTER, minFilter == Nearest? GL_NEAREST : GL_LINEAR);
+        glTexParameteri(texTarget, GL_TEXTURE_MIN_FILTER, glMinFilter(minFilter, mipFilter));
+
+        flags &= ~ParamsChanged;
+    }
+
+    void glImage(int level, Size const &size, Image::GLFormat const &glFormat,
+                 void const *data, CubeFace face = PositiveX)
+    {
+        glPixelStorei(GL_UNPACK_ALIGNMENT, glFormat.rowAlignment);
+        glTexImage2D(isCube()? glFace(face) : texTarget,
+                     level, glFormat.format, size.x, size.y, 0,
+                     glFormat.format, glFormat.type, data);
+    }
+
+    void glSubImage(int level, Vector2i const &pos, Size const &size,
+                    Image::GLFormat const &glFormat, void const *data, CubeFace face = PositiveX)
+    {
+        glPixelStorei(GL_UNPACK_ALIGNMENT, glFormat.rowAlignment);
+        glTexSubImage2D(isCube()? glFace(face) : texTarget,
+                        level, pos.x, pos.y, size.x, size.y,
+                        glFormat.format, glFormat.type, data);
+    }
 };
 
 GLTexture::GLTexture() : d(new Instance(this))
 {}
 
+void GLTexture::clear()
+{
+    d->clear();
+}
+
+void GLTexture::setMagFilter(Filter magFilter)
+{
+    d->magFilter = magFilter;
+    d->flags |= ParamsChanged;
+}
+
+void GLTexture::setMinFilter(Filter minFilter, MipFilter mipFilter)
+{
+    d->minFilter = minFilter;
+    d->mipFilter = mipFilter;
+    d->flags |= ParamsChanged;
+}
+
+void GLTexture::setWrapS(Wrapping mode)
+{
+    d->wrap.x = mode;
+    d->flags |= ParamsChanged;
+}
+
+void GLTexture::setWrapT(Wrapping mode)
+{
+    d->wrap.y = mode;
+    d->flags |= ParamsChanged;
+}
+
+Filter GLTexture::minFilter() const
+{
+    return d->minFilter;
+}
+
+Filter GLTexture::magFilter() const
+{
+    return d->magFilter;
+}
+
+MipFilter GLTexture::mipFilter() const
+{
+    return d->mipFilter;
+}
+
+Wrapping GLTexture::wrapS() const
+{
+    return d->wrap.x;
+}
+
+Wrapping GLTexture::wrapT() const
+{
+    return d->wrap.y;
+}
+
+GLTexture::Wraps GLTexture::wrap() const
+{
+    return d->wrap;
+}
+
+bool GLTexture::isCubeMap() const
+{
+    return d->isCube();
+}
+
+void GLTexture::setAutoGenMips(bool genMips)
+{
+    if(genMips)
+        d->flags |= AutoMips;
+    else
+        d->flags &= ~AutoMips;
+}
+
+bool GLTexture::autoGenMips() const
+{
+    return d->flags.testFlag(AutoMips);
+}
+
+void GLTexture::setUndefinedImage(GLTexture::Size const &size, Image::Format format, int level)
+{
+    d->texTarget = GL_TEXTURE_2D;
+
+    d->alloc();
+    d->glBind();
+    d->glImage(level, size, Image::glFormat(format), NULL);
+    d->glUnbind();
+
+    setState(Ready);
+}
+
+void GLTexture::setUndefinedImage(CubeFace face, GLTexture::Size const &size,
+                                  Image::Format format, int level)
+{
+    d->texTarget = GL_TEXTURE_CUBE_MAP;
+
+    d->alloc();
+    d->glBind();
+    d->glImage(level, size, Image::glFormat(format), NULL, face);
+    d->glUnbind();
+
+    setState(Ready);
+}
+
+void GLTexture::setImage(Image const &image, int level)
+{
+    d->texTarget = GL_TEXTURE_2D;
+
+    d->alloc();
+    d->glBind();
+    d->glImage(level, image.size(), image.glFormat(), image.bits());
+    d->glUnbind();
+
+    if(!level && d->flags.testFlag(AutoMips))
+    {
+        generateMipmap();
+    }
+
+    setState(Ready);
+}
+
+void GLTexture::setImage(CubeFace face, Image const &image, int level)
+{
+    d->texTarget = GL_TEXTURE_CUBE_MAP;
+
+    d->alloc();
+    d->glBind();
+    d->glImage(level, image.size(), image.glFormat(), image.bits(), face);
+    d->glUnbind();
+
+    if(!level && d->flags.testFlag(AutoMips))
+    {
+        generateMipmap();
+    }
+
+    setState(Ready);
+}
+
+void GLTexture::setSubImage(Image const &image, Vector2i const &pos, int level)
+{
+    d->texTarget = GL_TEXTURE_2D;
+
+    d->alloc();
+    d->glBind();
+    d->glSubImage(level, pos, image.size(), image.glFormat(), image.bits());
+    d->glUnbind();
+
+    if(!level && d->flags.testFlag(AutoMips))
+    {
+        generateMipmap();
+    }
+}
+
+void GLTexture::setSubImage(CubeFace face, Image const &image, Vector2i const &pos, int level)
+{
+    d->texTarget = GL_TEXTURE_CUBE_MAP;
+
+    d->alloc();
+    d->glBind();
+    d->glSubImage(level, pos, image.size(), image.glFormat(), image.bits(), face);
+    d->glUnbind();
+
+    if(!level && d->flags.testFlag(AutoMips))
+    {
+        generateMipmap();
+    }
+}
+
+void GLTexture::generateMipmap()
+{
+    if(d->name)
+    {
+        d->glBind();
+        glGenerateMipmap(d->texTarget);
+        d->glUnbind();
+
+        d->flags |= MipmapAvailable;
+    }
+}
+
 GLTexture::Size GLTexture::size() const
 {
     return d->size;
+}
+
+int GLTexture::mipLevels() const
+{
+    if(!isReady()) return 0;
+    return d->flags.testFlag(MipmapAvailable)? levelsForSize(d->size) : 1;
+}
+
+GLTexture::Size GLTexture::levelSize(int level) const
+{
+    if(level < 0) return Size();
+    return levelSize(d->size, level);
 }
 
 GLuint GLTexture::glName() const
@@ -65,10 +383,40 @@ GLuint GLTexture::glName() const
     return d->name;
 }
 
-void GLTexture::glBindToUnit(int unit)
+void GLTexture::glBindToUnit(int unit) const
 {
     glActiveTexture(GL_TEXTURE0 + unit);
-    glBindTexture(GL_TEXTURE_2D, d->name);
+    d->glBind();
+
+    if(d->flags.testFlag(ParamsChanged))
+    {
+        d->glUpdateParamsOfBoundTexture();
+    }
+}
+
+int GLTexture::levelsForSize(GLTexture::Size const &size)
+{
+    int mipLevels = 0;
+    duint w = size.x;
+    duint h = size.y;
+    while(w > 1 || h > 1)
+    {
+        w = de::max(1u, w >> 1);
+        h = de::max(1u, h >> 1);
+        mipLevels++;
+    }
+    return mipLevels;
+}
+
+GLTexture::Size GLTexture::levelSize(GLTexture::Size const &size0, int level)
+{
+    Size s = size0;
+    for(int i = 0; i < level; ++i)
+    {
+        s.x = de::max(1u, s.x >> 1);
+        s.y = de::max(1u, s.y >> 1);
+    }
+    return s;
 }
 
 } // namespace de
