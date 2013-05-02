28d3a9a4a85b245a334fa2fc82c20301218a221f
skyjake <skyjake@users.sf.net>
2013-05-02 07:27:17 +0300

    Bumped version to 1.10.2

--- doomsday/api/dd_version.h
+++ doomsday/api/dd_version.h
@@ -51,8 +51,8 @@ extern "C" {
  * Revision number increases with each small (maintenance) release.
  */
 
-#define DOOMSDAY_VERSION_BASE   "1.10.1"
-#define DOOMSDAY_VERSION_NUMBER 1,10,1,0 // For WIN32 version info.
+#define DOOMSDAY_VERSION_BASE   "1.10.2"
+#define DOOMSDAY_VERSION_NUMBER 1,10,2,0 // For WIN32 version info.
 //#define DOOMSDAY_RELEASE_NAME   "Example"
 
 /**
