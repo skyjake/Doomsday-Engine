1013bbcaf0f65e18cdff761b4826e17f78901456
skyjake <skyjake@users.sf.net>
2013-05-02 15:08:54 +0300

    libdeng2: Added concurrent tasks (Task and TaskPool)

---
+++ doomsday/libdeng2/include/de/concurrency/task.h
@@ -0,0 +1,56 @@
+/** @file task.h  Concurrent task.
+ *
+ * @authors Copyright (c) 2013 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * @par License
+ * GPL: http://www.gnu.org/licenses/gpl.html
+ *
+ * <small>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details. You should have received a copy of the GNU
+ * General Public License along with this program; if not, see:
+ * http://www.gnu.org/licenses</small> 
+ */
+
+#ifndef LIBDENG2_TASK_H
+#define LIBDENG2_TASK_H
+
+#include <QRunnable>
+
+#include "../libdeng2.h"
+
+namespace de {
+
+class TaskPool;
+
+/**
+ * Concurrent task that will be executed by a TaskPool asynchronously. Override
+ * runTask() in a derived class.
+ */
+class DENG2_PUBLIC Task : public QRunnable
+{
+public:
+    Task();
+    virtual ~Task() {}
+
+    TaskPool &pool() const;
+    void run();
+
+    /**
+     * Task classes must override this.
+     */
+    virtual void runTask() = 0;
+
+private:
+    friend class TaskPool;
+
+    TaskPool *_pool;
+};
+
+} // namespace de
+
+#endif // LIBDENG2_TASK_H
---
+++ doomsday/libdeng2/include/de/concurrency/taskpool.h
@@ -0,0 +1,80 @@
+/** @file taskpool.h  Pool of tasks.
+ *
+ * @authors Copyright (c) 2013 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * @par License
+ * GPL: http://www.gnu.org/licenses/gpl.html
+ *
+ * <small>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details. You should have received a copy of the GNU
+ * General Public License along with this program; if not, see:
+ * http://www.gnu.org/licenses</small> 
+ */
+
+#ifndef LIBDENG2_TASKPOOL_H
+#define LIBDENG2_TASKPOOL_H
+
+#include <QObject>
+
+#include "../libdeng2.h"
+
+namespace de {
+
+class Task;
+
+/**
+ * Pool of concurrent tasks.
+ */
+class DENG2_PUBLIC TaskPool : public QObject
+{
+    Q_OBJECT
+
+public:
+    enum Priority
+    {
+        LowPriority    = 0,
+        MediumPriority = 1,
+        HighPriority   = 2
+    };
+
+public:
+    TaskPool();
+
+    virtual ~TaskPool();
+
+    /**
+     * Starts a new concurrent task. Ownership of the task is given to the
+     * pool.
+     *
+     * @param task  Task instance. Ownership given.
+     */
+    void start(Task *task, Priority priority = LowPriority);
+
+    /**
+     * Blocks execution until all running tasks have finished.
+     */
+    void waitForDone();
+
+    /**
+     * Determines if all started tasks have finished.
+     */
+    bool isDone() const;
+
+signals:
+    void allTasksDone();
+
+private:
+    friend class Task;
+    void taskFinished(Task &task);
+
+    DENG2_PRIVATE(d)
+};
+
+} // namespace de
+
+#endif // LIBDENG2_TASKPOOL_H
