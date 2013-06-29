25ae18e261155476ee16d14ce37009474e5896cd
danij <danij@dengine.net>
2013-06-29 20:27:20 +0100

    Fixed|Homepage|Master Server: Minor master server fixes following PHP upgrade
    Replaced usage of the now deprecated split(). Server announcements
    are now assumed to be UTF8 encoded.

--- web/master.php
+++ web/master.php
@@ -1,26 +1,22 @@
 <?php
-/**
- * @file master.php Doomsday Master Server interface
+/** @file master.php Doomsday Master Server interface
  *
- * @section License
+ * @authors Copyright Â© 2003-2013 Jaakko KerÃ¤nen <jaakko.keranen@iki.fi>
+ * @authors Copyright Â© 2009-2013 Daniel Swanson <danij@dengine.net>
+ *
+ * @par License
  * GPL: http://www.gnu.org/licenses/gpl.html
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * @author Copyright &copy; 2003-2012 Jaakko Keränen <jaakko.keranen@iki.fi>
- * @author Copyright &copy; 2009-2013 Daniel Swanson <danij@dengine.net>
+ * <small>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details. You should have received a copy of the GNU
+ * General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA</small>
  */
 
 require_once('classes/masterserver.class.php');
@@ -38,8 +34,12 @@ function update_server($announcement, $addr)
 {
     $info = array();
 
+    mb_regex_encoding('UTF-8');
+    mb_internal_encoding('UTF-8');
+
     // First we must determine if the announcement is valid.
-    $lines = split("\n", $announcement);
+    $lines = mb_split("\r\n|\n|\r", $announcement);
+
     while(list($line_number, $line) = each($lines))
     {
         // Separate the label from the value.
