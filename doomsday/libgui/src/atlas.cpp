711dae8b09f3298a36f4434a5e19df4ef6efd27a
skyjake <skyjake@users.sf.net>
2013-07-06 15:52:17 +0300

    Fixed|Client: Crash and mishandling of failed entry allocations
    When the entry atlas is so full that an allocation of an entry fragment
    fails, LogWidget did not handle the situation correctly. Now if this
    occurs, all out-of-view items are released to make room on the atlas
    and one extra attempt is made to allocate the visible entries.
    
    Also increased the maximum size of the entry atlas (ideally this would
    be display mode dependent).
    
    A crash was occurring because all entries were zapped from the atlas
    during the update of an entry, confusing GLTextComposer.

--- doomsday/libgui/src/atlas.cpp
+++ doomsday/libgui/src/atlas.cpp
@@ -250,7 +250,7 @@ Id Atlas::alloc(Image const &image)
     }
     else
     {
-        LOG_DEBUG("Atlas is full with %.1f%% usage") << d->usedPercentage()*100;
+        LOG_TRACE("Atlas is full with %.1f%% usage") << d->usedPercentage()*100;
 
         DENG2_FOR_AUDIENCE(OutOfSpace, i)
         {
