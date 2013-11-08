d43756b3dbbeb18be580314d1ed02df5ac5872e7
Christopher Bruns <cmbruns@rotatingpenguin.com>
2013-11-07 21:04:56 -0500

    Stereo 3D|Client: Expose rend-vr-hud-distance as a CVAR, and set its default to 20 (map units)

--- doomsday/client/src/render/vr.cpp
+++ doomsday/client/src/render/vr.cpp
@@ -71,7 +71,7 @@ byte  VR::swapEyes = 0;
 // Global variables
 bool  VR::applyFrustumShift = true;
 float  VR::eyeShift = 0;
-float  VR::hudDistance = 30.0f;
+float  VR::hudDistance = 20.0f;
 float  VR::weaponDistance = 10.0f;
 
 
@@ -105,14 +105,15 @@ static void vrLatencyChanged()
 
 void VR::consoleRegister()
 {
-    C_VAR_FLOAT ("rend-vr-ipd",              & VR::ipd,           0, 0.02f, 0.2f);
-    C_VAR_FLOAT ("rend-vr-player-height",    & VR::playerHeight,  0, 1.0f, 3.0f);
     C_VAR_FLOAT ("rend-vr-dominant-eye",     & VR::dominantEye,   0, -1.0f, 1.0f);
+    C_VAR_FLOAT ("rend-vr-hud-distance",     & VR::hudDistance,           0, 0.01f, 40.0f);
+    C_VAR_FLOAT ("rend-vr-ipd",              & VR::ipd,           0, 0.02f, 0.2f);
+    C_VAR_INT2  ("rend-vr-mode",             & vrMode,            0, 0, (int)(VR::MODE_MAX_3D_MODE_PLUS_ONE - 1), vrModeChanged);
+    C_VAR_FLOAT ("rend-vr-player-height",    & VR::playerHeight,  0, 1.0f, 3.0f);
     C_VAR_FLOAT ("rend-vr-rift-aspect",      & vrRiftAspect,      0, 0.10f, 10.0f);
     C_VAR_FLOAT ("rend-vr-rift-fovx",        & vrRiftFovX,        0, 5.0f, 270.0f);
     C_VAR_FLOAT2("rend-vr-rift-latency",     & vrLatency,         0, 0.0f, 0.250f, vrLatencyChanged);
     C_VAR_BYTE  ("rend-vr-swap-eyes",        & VR::swapEyes,      0, 0, 1);
-    C_VAR_INT2  ("rend-vr-mode",             & vrMode,            0, 0, (int)(VR::MODE_MAX_3D_MODE_PLUS_ONE - 1), vrModeChanged);
 }
 
 
