77e84d2e5fa2f04088f54ce227b7e6faeba77ebd
Jaakko Keränen <jaakko.keranen@iki.fi>
2014-01-06 21:49:20 +0200

    UI|Client: Put together the log settings dialog’s contents
    Todo: Better layout and full functionality.

--- doomsday/client/src/ui/dialogs/logsettingsdialog.cpp
+++ doomsday/client/src/ui/dialogs/logsettingsdialog.cpp
@@ -17,20 +17,94 @@
  */
 
 #include "ui/dialogs/logsettingsdialog.h"
+#include "ui/widgets/variablechoicewidget.h"
+#include "ui/widgets/variabletogglewidget.h"
 #include "SignalAction"
+
+#include <de/App>
 
 using namespace de;
 
 DENG2_PIMPL(LogSettingsDialog)
 {
+    ui::ListData levels;
+    VariableToggleWidget *separately;
+    struct Domain {
+        VariableChoiceWidget *level;
+        VariableToggleWidget *alert;
+    };
+    Domain generic;
+    Domain resource;
+    Domain map;
+    Domain script;
+    Domain gfx;
+    Domain audio;
+    Domain input;
+    Domain network;
+
     Instance(Public *i) : Base(i)
-    {}
+    {
+        self.area().add(separately =
+                new VariableToggleWidget(tr("Filter Separately"), App::config()["log.filterSeparately"]));
+
+        levels << new ChoiceItem(tr("X-Verbose"), LogEntry::XVerbose)
+               << new ChoiceItem(tr("Verbose"),   LogEntry::Verbose)
+               << new ChoiceItem(tr("Message"),   LogEntry::Message)
+               << new ChoiceItem(tr("Note"),      LogEntry::Note)
+               << new ChoiceItem(tr("Warning"),   LogEntry::Warning)
+               << new ChoiceItem(tr("Error"),     LogEntry::Error)
+               << new ChoiceItem(tr("Critical"),  LogEntry::Critical);
+
+        initDomain(generic,  "generic");
+        initDomain(resource, "resource");
+        initDomain(map,      "map");
+        initDomain(script,   "script");
+        initDomain(gfx,      "gl");
+        initDomain(audio,    "audio");
+        initDomain(input,    "input");
+        initDomain(network,  "network");
+    }
+
+    void initDomain(Domain &dom, String const &name)
+    {
+        self.area().add(dom.level =
+                new VariableChoiceWidget(App::config()["log.filter." + name + ".minLevel"]));
+        dom.level->popup().menu().setItems(levels);
+        dom.level->updateFromVariable();
+
+        self.area().add(dom.alert =
+                new VariableToggleWidget(tr("Alerts"), App::config()["alert." + name]));
+        dom.alert->setActiveValue(LogEntry::Warning);
+        dom.alert->setInactiveValue(LogEntry::MAX_LOG_LEVELS);
+    }
+
+    ~Instance()
+    {
+        // The common 'levels' will be deleted soon.
+        generic.level->popup().menu().useDefaultItems();
+        resource.level->popup().menu().useDefaultItems();
+        map.level->popup().menu().useDefaultItems();
+        script.level->popup().menu().useDefaultItems();
+        gfx.level->popup().menu().useDefaultItems();
+        audio.level->popup().menu().useDefaultItems();
+        input.level->popup().menu().useDefaultItems();
+        network.level->popup().menu().useDefaultItems();
+    }
 };
 
 LogSettingsDialog::LogSettingsDialog(String const &name)
     : DialogWidget(name, WithHeading), d(new Instance(this))
 {
     heading().setText(tr("Log Filter & Alerts"));
+
+    LabelWidget *levelLabel  = LabelWidget::newWithText(tr("Level:"), &area());
+    LabelWidget *resLabel    = LabelWidget::newWithText(tr("Resources:"), &area());
+    LabelWidget *mapLabel    = LabelWidget::newWithText(tr("Map:"), &area());
+    LabelWidget *scriptLabel = LabelWidget::newWithText(tr("Scripting:"), &area());
+    LabelWidget *glLabel     = LabelWidget::newWithText(tr("Graphics:"), &area());
+    LabelWidget *audioLabel  = LabelWidget::newWithText(tr("Audio:"), &area());
+    LabelWidget *inputLabel  = LabelWidget::newWithText(tr("Input:"), &area());
+    LabelWidget *netLabel    = LabelWidget::newWithText(tr("Network:"), &area());
 
     /*
     LabelWidget *modeLabel     = LabelWidget::newWithText(tr("Stereo Mode:"), &area());
@@ -41,8 +115,20 @@ LogSettingsDialog::LogSettingsDialog(String const &name)
 
     // Layout.
     GridLayout layout(area().contentRule().left(), area().contentRule().top());
-    layout.setGridSize(2, 0);
+    layout.setGridSize(3, 0);
     layout.setColumnAlignment(0, ui::AlignRight);
+
+    layout << *levelLabel << *d->generic.level << *d->generic.alert
+           << Const(0);
+    layout.append(*d->separately, 2);
+
+    layout << *resLabel    << *d->resource.level << *d->resource.alert
+           << *mapLabel    << *d->map.level      << *d->map.alert
+           << *scriptLabel << *d->script.level   << *d->script.alert
+           << *glLabel     << *d->gfx.level      << *d->gfx.alert
+           << *audioLabel  << *d->audio.level    << *d->audio.alert
+           << *inputLabel  << *d->input.level    << *d->input.alert
+           << *netLabel    << *d->network.level  << *d->network.alert;
 
     /*
     layout << *modeLabel     << *d->mode
