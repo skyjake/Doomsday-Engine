8f6a510f5db5431c8b06c312e9045fccb17d7e5f
Jaakko Keränen <jaakko.keranen@iki.fi>
2014-03-16 09:54:52 +0200

    Renderer|FX: Moved color filter later in the FX stack
    If color filter is applied before bloom, it can alter the resulting hue
    and brightness of the filter quite much.
    
    Also, as the color filter is used for pain/etc. effects (visualization
    for things that the player senses) they should be applied after the
    optical effects.

--- doomsday/client/src/render/cameralensfx.cpp
+++ doomsday/client/src/render/cameralensfx.cpp
@@ -78,7 +78,7 @@ struct ConsoleEffectStack
 
 static ConsoleEffectStack fxConsole[DDMAXPLAYERS];
 
-#define IDX_LENS_FLARES         3
+#define IDX_LENS_FLARES         2
 #define IDX_POST_PROCESSING     4
 
 D_CMD(PostFx)
@@ -123,10 +123,10 @@ void LensFx_Init()
     for(int i = 0; i < DDMAXPLAYERS; ++i)
     {
         ConsoleEffectStack &stack = fxConsole[i];
-        stack.effects.append(new fx::ColorFilter(i));
         stack.effects.append(new fx::Vignette(i));
         stack.effects.append(new fx::Bloom(i));
-        stack.effects.append(new fx::LensFlares(i));
+        stack.effects.append(new fx::LensFlares(i));     // IDX_LENS_FLARES
+        stack.effects.append(new fx::ColorFilter(i));
         stack.effects.append(new fx::PostProcessing(i)); // IDX_POST_PROCESSING
     }
 }
