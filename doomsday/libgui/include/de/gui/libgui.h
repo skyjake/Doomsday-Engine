73268186d58db5569f5607c824864afa72166acb
Jaakko Keränen <jaakko.keranen@iki.fi>
2014-06-08 12:22:37 +0300

    Debug|libgui: Don't assert GL errors (just log)

--- doomsday/libgui/include/de/gui/libgui.h
+++ doomsday/libgui/include/de/gui/libgui.h
@@ -43,9 +43,8 @@
 #endif
 
 // Assertion specific to GL errors.
-#if defined(Q_WS_X11) || defined(WIN32)
-// Under X11 we're having more OpenGL errors; should investigate why.
-#  define LIBGUI_ASSERT_GL(cond) // ignored
+#if 1 || defined(Q_WS_X11) || defined(WIN32)
+#  define LIBGUI_ASSERT_GL(cond) // just logged, no abort
 #else
 #  define LIBGUI_ASSERT_GL(cond) DENG2_ASSERT(cond)
 #endif
