34cff9eac8f9e3a1c0fda2b30e663334c34a6cfd
Jaakko Keränen <jaakko.keranen@iki.fi>
2015-02-18 15:49:47 +0200

    libdoomsday: Moved net.dengine.base package
    This package belongs under the ownership of libdoomsday.

--- doomsday/apps/libdoomsday/DengDoomsdayConfig.cmake
+++ doomsday/apps/libdoomsday/DengDoomsdayConfig.cmake
@@ -1,5 +1,7 @@
-include ("${CMAKE_CURRENT_LIST_DIR}/DengDoomsday.cmake")
-
 find_package (DengCore REQUIRED)
 find_package (DengLegacy REQUIRED)
 find_package (DengShell REQUIRED)
+
+include ("${CMAKE_CURRENT_LIST_DIR}/DengDoomsday.cmake")
+
+list (APPEND DENG_REQUIRED_PACKAGES net.dengine.base)
