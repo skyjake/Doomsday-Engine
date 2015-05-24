2ada8e795a3e42f5a79a954790903f183b48456f
Jaakko Keränen <jaakko.keranen@iki.fi>
2015-05-24 23:09:39 +0300

    Fixed|Builder: Updated paths for running Doxygen

--- distrib/autobuild.py
+++ distrib/autobuild.py
@@ -389,43 +389,43 @@ def generate_apidoc():
     system_command('wc -l doxyissues-qch.txt')
 
     print >> sys.stderr, "\nPublic API 2.0 docs..."
-    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/libcore'))    
-    system_command('doxygen api2.doxy >/dev/null 2>../doxyissues-api2.txt')
-    system_command('wc -l ../doxyissues-api2.txt')
+    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/sdk/libcore'))    
+    system_command('doxygen api2.doxy >/dev/null 2>../../doxyissues-api2.txt')
+    system_command('wc -l ../../doxyissues-api2.txt')
     
     print >> sys.stderr, "\nPublic API docs..."
-    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/client'))    
-    system_command('doxygen api.doxy >/dev/null 2>../doxyissues-api.txt')
-    system_command('wc -l ../doxyissues-api.txt')
+    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/apps/client'))    
+    system_command('doxygen api.doxy >/dev/null 2>../../doxyissues-api.txt')
+    system_command('wc -l ../../doxyissues-api.txt')
 
     print >> sys.stderr, "\nlibshell docs..."
-    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/libshell'))
-    system_command('doxygen shell.doxy >/dev/null 2>../doxyissues-shell.txt')
-    system_command('wc -l ../doxyissues-shell.txt')
+    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/sdk/libshell'))
+    system_command('doxygen shell.doxy >/dev/null 2>../../doxyissues-shell.txt')
+    system_command('wc -l ../../doxyissues-shell.txt')
 
     print >> sys.stderr, "\nInternal Win32 docs..."
-    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/client'))
-    system_command('doxygen client-win32.doxy >/dev/null 2>../doxyissues-win32.txt')
-    system_command('wc -l ../doxyissues-win32.txt')
+    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/apps/client'))
+    system_command('doxygen client-win32.doxy >/dev/null 2>../../doxyissues-win32.txt')
+    system_command('wc -l ../../doxyissues-win32.txt')
 
     print >> sys.stderr, "\nInternal Mac/Unix docs..."
-    system_command('doxygen client-mac.doxy >/dev/null 2>../doxyissues-mac.txt')        
-    system_command('wc -l ../doxyissues-mac.txt')
+    system_command('doxygen client-mac.doxy >/dev/null 2>../../doxyissues-mac.txt')        
+    system_command('wc -l ../../doxyissues-mac.txt')
 
     print >> sys.stderr, "\nDoom plugin docs..."
-    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/plugins/doom'))
-    system_command('doxygen doom.doxy >/dev/null 2>../../doxyissues-doom.txt')
-    system_command('wc -l ../../doxyissues-doom.txt')
+    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/apps/plugins/doom'))
+    system_command('doxygen doom.doxy >/dev/null 2>../../../doxyissues-doom.txt')
+    system_command('wc -l ../../../doxyissues-doom.txt')
 
     print >> sys.stderr, "\nHeretic plugin docs..."
-    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/plugins/heretic'))
-    system_command('doxygen heretic.doxy >/dev/null 2>../../doxyissues-heretic.txt')
-    system_command('wc -l ../../doxyissues-heretic.txt')
+    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/apps/plugins/heretic'))
+    system_command('doxygen heretic.doxy >/dev/null 2>../../../doxyissues-heretic.txt')
+    system_command('wc -l ../../../doxyissues-heretic.txt')
 
     print >> sys.stderr, "\nHexen plugin docs..."
-    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/plugins/hexen'))
-    system_command('doxygen hexen.doxy >/dev/null 2>../../doxyissues-hexen.txt')
-    system_command('wc -l ../../doxyissues-hexen.txt')
+    os.chdir(os.path.join(builder.config.DISTRIB_DIR, '../doomsday/apps/plugins/hexen'))
+    system_command('doxygen hexen.doxy >/dev/null 2>../../../doxyissues-hexen.txt')
+    system_command('wc -l ../../../doxyissues-hexen.txt')
 
 
 def generate_readme():
