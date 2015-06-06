9853e9a19f1273c4f0a73798065853022441863e
Jaakko Keränen <jaakko.keranen@iki.fi>
2015-06-06 12:47:35 +0300

    Documentation: Updated console variable help, fixed generator script
    The script was still using the old location of net.dengine.base.pack.

--- doomsday/build/scripts/makehelp.py
+++ doomsday/build/scripts/makehelp.py
@@ -11,7 +11,7 @@ conHelp = os.path.join(dengRoot, 'build', 'scripts', 'conhelp.py')
 os.chdir(os.path.join(dengRoot, 'doc'))
 
 print "Engine help"
-cpHelpTxt = os.path.join(dengRoot, 'net.dengine.base.pack', 'helpstrings.txt')
+cpHelpTxt = os.path.join(dengRoot, 'apps', 'libdoomsday', 'net.dengine.base.pack', 'helpstrings.txt')
 conhelp.makeHelp(cpHelpTxt, ['engine'])
 
 panel = conhelp.amethyst(file(os.path.join('engine', 'controlpanel.ame')).read())
@@ -20,15 +20,15 @@ file(cpHelpTxt, 'a').write(panel)
 
 print "libdoom help"
 conhelp.makeHelp(
-    os.path.join(dengRoot, 'plugins', 'doom', 'data', 'conhelp.txt'),
+    os.path.join(dengRoot, 'apps', 'plugins', 'doom', 'data', 'conhelp.txt'),
     ['libcommon', 'libdoom'])
     
 print "libheretic help"
 conhelp.makeHelp(
-    os.path.join(dengRoot, 'plugins', 'heretic', 'data', 'conhelp.txt'),
+    os.path.join(dengRoot, 'apps', 'plugins', 'heretic', 'data', 'conhelp.txt'),
     ['libcommon', 'libheretic'])
     
 print "libhexen help"
 conhelp.makeHelp(
-    os.path.join(dengRoot, 'plugins', 'hexen', 'data', 'conhelp.txt'),
+    os.path.join(dengRoot, 'apps', 'plugins', 'hexen', 'data', 'conhelp.txt'),
     ['libcommon', 'libhexen'])
