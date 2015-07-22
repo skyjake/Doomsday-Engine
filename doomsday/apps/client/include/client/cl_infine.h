25a297ccf6277d45223abfa4b58dcc849390943d
danij <danij@dengine.net>
2015-07-22 04:06:31 +0100

    Cleanup: Removed include/de_infine.h

--- doomsday/apps/client/include/client/cl_infine.h
+++ doomsday/apps/client/include/client/cl_infine.h
@@ -1,6 +1,6 @@
 /** @file cl_infine.h  Clientside InFine.
  *
- * @authors Copyright © 2010 Daniel Swanson <danij@dengine.net>
+ * @authors Copyright © 2010-2015 Daniel Swanson <danij@dengine.net>
  *
  * @par License
  * GPL: http://www.gnu.org/licenses/gpl.html
@@ -16,8 +16,8 @@
  * http://www.gnu.org/licenses</small>
  */
 
-#ifndef DENG_CLIENT_INFINE
-#define DENG_CLIENT_INFINE
+#ifndef CLIENT_INFINE
+#define CLIENT_INFINE
 
 #include "api_infine.h"
 #include <de/reader.h>
@@ -34,4 +34,4 @@ void Cl_Finale(Reader *msg);
  */
 void Cl_RequestFinaleSkip();
 
-#endif // DENG_CLIENT_INFINE
+#endif  // CLIENT_INFINE
