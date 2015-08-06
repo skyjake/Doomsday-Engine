544b48edd3e4bad6529a899725981cd6ef31f9cd
danij <danij@dengine.net>
2015-08-06 21:39:39 +0100

    Audio|libdoomsday: Cleanup

--- doomsday/apps/libdoomsday/include/doomsday/audio/logical.h
+++ doomsday/apps/libdoomsday/include/doomsday/audio/logical.h
@@ -1,6 +1,16 @@
 /** @file logical.h  Logical Sound Manager.
  *
+ * The Logical Sound Manager. Tracks all currently playing sounds in the world,
+ * regardless of whether Sfx is available or if the sounds are actually audible
+ * to anyone.
+ *
+ * Uses PU_MAP, so this has to be inited for every map. (Done via S_MapChange()).
+ *
+ * @todo This should be part of an audio system base class that can be used
+ * both by the client and the server. -jk
+
  * @authors Copyright © 2003-2013 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * @authors Copyright © 2015 Daniel Swanson <danij@dengine.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,17 +39,46 @@
 extern "C" {
 #endif
 
+/**
+ * Initialize the Logical Sound Manager for a new map.
+ */
 LIBDOOMSDAY_PUBLIC void Sfx_InitLogical(void);
+
+/**
+ * Remove stopped logical sounds from the hash.
+ */
 LIBDOOMSDAY_PUBLIC void Sfx_PurgeLogical(void);
+
+/**
+ * The sound is entered into the list of playing sounds. Called when a 'world class'
+ * sound is started, regardless of whether it's actually started on the local system.
+ */
 LIBDOOMSDAY_PUBLIC void Sfx_StartLogical(int id, mobj_t *origin, dd_bool isRepeating);
+
+/**
+ * The sound is removed from the list of playing sounds. Called whenever a sound is
+ * stopped, regardless of whether it was actually playing on the local system.
+ *
+ * @note If @a id == 0 and  @a origin == 0 then stop everything.
+ *
+ * @return  Number of sounds stopped.
+ */
 LIBDOOMSDAY_PUBLIC int Sfx_StopLogical(int id, mobj_t *origin);
+
+/**
+ * Returns true if the sound is currently playing somewhere in the world.
+ * It doesn't matter if it's audible or not.
+ *
+ * @param id  @c 0= true if any sounds are playing using the specified @a origin.
+ */
 LIBDOOMSDAY_PUBLIC dd_bool Sfx_IsPlaying(int id, mobj_t *origin);
 
 LIBDOOMSDAY_PUBLIC void Sfx_Logical_SetOneSoundPerEmitter(dd_bool enabled);
+
 LIBDOOMSDAY_PUBLIC void Sfx_Logical_SetSampleLengthCallback(uint (*callback)(int));
 
 #ifdef __cplusplus
-} // extern "C"
+}  // extern "C"
 #endif
 
-#endif // LIBDOOMSDAY_AUDIO_LOGICAL_H
+#endif  // LIBDOOMSDAY_AUDIO_LOGICAL_H
--- doomsday/apps/libdoomsday/src/audio/logical.cpp
+++ doomsday/apps/libdoomsday/src/audio/logical.cpp
@@ -1,6 +1,7 @@
-/**\file logical.cpp
+/** @file logical.cpp  Logical Sound Manager.
  *
  * @authors Copyright © 2003-2013 Jaakko Keränen <jaakko.keranen@iki.fi>
+ * @authors Copyright © 2015 Daniel Swanson <danij@dengine.net>
  *
  * @par License
  * GPL: http://www.gnu.org/licenses/gpl.html
@@ -16,184 +17,161 @@
  * http://www.gnu.org/licenses</small>
  */
 
-/**
- * @par The Logical Sound Manager
- *
- * The Logical Sound Manager. Tracks all currently playing sounds
- * in the world, regardless of whether Sfx is available or if the
- * sounds are actually audible to anyone.
- *
- * Uses PU_MAP, so this has to be inited for every map.
- * (Done via S_MapChange()).
- *
- * @todo This should be part of an audio system base class that can be used
- * both by the client and the server. -jk
- */
-
 #include "doomsday/audio/logical.h"
 #include <de/timer.h>
 #include <de/memoryzone.h>
 
+using namespace de;
+
 // The logical sounds hash table uses sound IDs as keys.
-#define LOGIC_HASH_SIZE     64
+#define LOGIC_HASH_SIZE     ( 64 )
 
-#define PURGE_INTERVAL      2000    // 2 seconds
+#define PURGE_INTERVAL      ( 2000 )  ///< 2 seconds
 
-typedef struct logicsound_s {
-    struct logicsound_s *next, *prev;
-    int     id;
+struct logicsound_t
+{
+    logicsound_t *next, *prev;
+
+    dint id;
     mobj_t *origin;
-    uint    endTime;
-    byte    isRepeating;
-} logicsound_t;
+    duint endTime;
+    bool isRepeating;
+};
 
-typedef struct logichash_s {
+struct logichash_t
+{
     logicsound_t *first, *last;
-} logichash_t;
-
+};
 static logichash_t logicHash[LOGIC_HASH_SIZE];
+
 static dd_bool logicalOneSoundPerEmitter;
-static uint (*logicalSoundLengthCallback)(int);
 
-void Sfx_Logical_SetOneSoundPerEmitter(dd_bool enabled)
+static duint (*logicalSoundLengthCallback)(dint);
+
+static logichash_t &getLogicHash(dint id)
 {
-    logicalOneSoundPerEmitter = enabled;
+    return ::logicHash[(duint) id % LOGIC_HASH_SIZE];
 }
 
-void Sfx_Logical_SetSampleLengthCallback(uint (*callback)(int))
-{
-    logicalSoundLengthCallback = callback;
-}
-
-/*
- * Initialize the Logical Sound Manager for a new map.
- */
-void Sfx_InitLogical(void)
-{
-    // Memory in the hash table is PU_MAP, so this is acceptable.
-    memset(logicHash, 0, sizeof(logicHash));
-}
-
-/*
- * Return a pointer the logical sounds hash.
- */
-logichash_t *Sfx_LogicHash(int id)
-{
-    return &logicHash[(uint) id % LOGIC_HASH_SIZE];
-}
-
-/*
+/**
  * Create and link a new logical sound hash table node.
  */
-logicsound_t *Sfx_CreateLogical(int id)
+static logicsound_t *newLogicSound(dint id)
 {
-    logichash_t *hash = Sfx_LogicHash(id);
-    logicsound_t *node = reinterpret_cast<logicsound_t *>(Z_Calloc(sizeof(logicsound_t), PU_MAP, 0));
+    auto *node = reinterpret_cast<logicsound_t *>(Z_Calloc(sizeof(logicsound_t), PU_MAP, nullptr));
 
-    if(hash->last)
+    logichash_t &hash = getLogicHash(id);
+    if(hash.last)
     {
-        hash->last->next = node;
-        node->prev = hash->last;
+        hash.last->next = node;
+        node->prev = hash.last;
     }
-    hash->last = node;
-    if(!hash->first)
-        hash->first = node;
+    hash.last = node;
+    if(!hash.first)
+        hash.first = node;
 
     node->id = id;
     return node;
 }
 
-/*
+/**
  * Unlink and destroy a logical sound hash table node.
  */
-void Sfx_DestroyLogical(logicsound_t * node)
+static void destroyLogicSound(logicsound_t *node)
 {
-    logichash_t *hash = Sfx_LogicHash(node->id);
+    if(!node) return;
 
-    if(hash->first == node)
-        hash->first = node->next;
-    if(hash->last == node)
-        hash->last = node->prev;
+    logichash_t &hash = getLogicHash(node->id);
+    if(hash.first == node)
+        hash.first = node->next;
+    if(hash.last == node)
+        hash.last = node->prev;
+
     if(node->next)
         node->next->prev = node->prev;
     if(node->prev)
         node->prev->next = node->next;
 
-#ifdef _DEBUG
-    memset(node, 0xDD, sizeof(*node));
+#ifdef DENG2_DEBUG
+    std::memset(node, 0xDD, sizeof(*node));
 #endif
     Z_Free(node);
 }
 
-/*
- * The sound is entered into the list of playing sounds. Called when a
- * 'world class' sound is started, regardless of whether it's actually
- * started on the local system.
- */
-void Sfx_StartLogical(int id, mobj_t *origin, dd_bool isRepeating)
+void Sfx_Logical_SetOneSoundPerEmitter(dd_bool enabled)
 {
-    DENG_ASSERT(logicalSoundLengthCallback != 0);
+    ::logicalOneSoundPerEmitter = enabled;
+}
 
-    logicsound_t *node;
-    uint length = (isRepeating ? 1 : logicalSoundLengthCallback(id));
+void Sfx_Logical_SetSampleLengthCallback(duint (*callback)(dint))
+{
+    ::logicalSoundLengthCallback = callback;
+}
 
+void Sfx_InitLogical()
+{
+    // Memory in the hash table is PU_MAP, so this is acceptable.
+    std::memset(::logicHash, 0, sizeof(::logicHash));
+}
+
+void Sfx_StartLogical(dint id, mobj_t *origin, dd_bool isRepeating)
+{
+    DENG2_ASSERT(::logicalSoundLengthCallback != nullptr);
+
+    duint const length = (isRepeating ? 1 : ::logicalSoundLengthCallback(id));
     if(!length)
     {
         // This is not a valid sound.
         return;
     }
 
-    if(origin && logicalOneSoundPerEmitter)
+    if(origin && ::logicalOneSoundPerEmitter)
     {
         // Stop all previous sounds from this origin (only one per origin).
         Sfx_StopLogical(0, origin);
     }
 
     id &= ~DDSF_FLAG_MASK;
-    node = Sfx_CreateLogical(id);
-    node->origin = origin;
-    node->isRepeating = isRepeating;
-    node->endTime = Timer_RealMilliseconds() + length;
+
+    logicsound_t *node = newLogicSound(id);
+    node->origin      = origin;
+    node->isRepeating = CPP_BOOL(isRepeating);
+    node->endTime     = Timer_RealMilliseconds() + length;
 }
 
-/*
- * The sound is removed from the list of playing sounds. Called whenever
- * a sound is stopped, regardless of whether it was actually playing on
- * the local system. Returns the number of sounds stopped.
- *
- * id=0, origin=0: stop everything
- */
-int Sfx_StopLogical(int id, mobj_t *origin)
+dint Sfx_StopLogical(dint id, mobj_t *origin)
 {
-    logicsound_t *it, *next;
-    int     stopCount = 0;
-    int     i;
+    dint stopCount = 0;
 
     if(id)
     {
-        for(it = Sfx_LogicHash(id)->first; it; it = next)
+        logicsound_t *it = getLogicHash(id).first;
+        while(it)
         {
-            next = it->next;
+            logicsound_t *next = it->next;
             if(it->id == id && it->origin == origin)
             {
-                Sfx_DestroyLogical(it);
+                destroyLogicSound(it);
                 stopCount++;
             }
+            it = next;
         }
     }
     else
     {
         // Browse through the entire hash.
-        for(i = 0; i < LOGIC_HASH_SIZE; i++)
+        for(dint i = 0; i < LOGIC_HASH_SIZE; ++i)
         {
-            for(it = logicHash[i].first; it; it = next)
+            logicsound_t *it = logicHash[i].first;
+            while(it)
             {
-                next = it->next;
+                logicsound_t *next = it->next;
                 if(!origin || it->origin == origin)
                 {
-                    Sfx_DestroyLogical(it);
+                    destroyLogicSound(it);
                     stopCount++;
                 }
+                it = next;
             }
         }
     }
@@ -201,52 +179,41 @@ int Sfx_StopLogical(int id, mobj_t *origin)
     return stopCount;
 }
 
-/*
- * Remove stopped logical sounds from the hash.
- */
-void Sfx_PurgeLogical(void)
+void Sfx_PurgeLogical()
 {
-    static uint lastTime = 0;
-    uint    i, nowTime = Timer_RealMilliseconds();
-    logicsound_t *it, *next;
+    static duint lastTime = 0;
 
-    if(nowTime - lastTime < PURGE_INTERVAL)
-    {
-        // It's too early.
-        return;
-    }
+    // Too soon for a purge?
+    duint const nowTime = Timer_RealMilliseconds();
+    if(nowTime - lastTime < PURGE_INTERVAL) return;
+
+    // Peform the purge now.
     lastTime = nowTime;
 
     // Check all sounds in the hash.
-    for(i = 0; i < LOGIC_HASH_SIZE; i++)
+    for(dint i = 0; i < LOGIC_HASH_SIZE; ++i)
     {
-        for(it = logicHash[i].first; it; it = next)
+        logicsound_t *it = logicHash[i].first;
+        while(it)
         {
-            next = it->next;
+            logicsound_t *next = it->next;
             if(!it->isRepeating && it->endTime < nowTime)
             {
                 // This has stopped.
-                Sfx_DestroyLogical(it);
+                destroyLogicSound(it);
             }
+            it = next;
         }
     }
 }
 
-/*
- * Returns true if the sound is currently playing somewhere in the world.
- * It doesn't matter if it's audible or not.
- *
- * id=0: true if any sounds are playing using the specified origin
- */
-dd_bool Sfx_IsPlaying(int id, mobj_t *origin)
+dd_bool Sfx_IsPlaying(dint id, mobj_t *origin)
 {
-    uint    nowTime = Timer_RealMilliseconds();
-    logicsound_t *it;
-    int     i;
+    duint const nowTime = Timer_RealMilliseconds();
 
     if(id)
     {
-        for(it = Sfx_LogicHash(id)->first; it; it = it->next)
+        for(logicsound_t *it = getLogicHash(id).first; it; it = it->next)
         {
             if(it->id == id && it->origin == origin &&
                (it->endTime > nowTime || it->isRepeating))
@@ -259,16 +226,13 @@ dd_bool Sfx_IsPlaying(int id, mobj_t *origin)
     else if(origin)
     {
         // Check if the origin is playing any sound.
-        for(i = 0; i < LOGIC_HASH_SIZE; i++)
+        for(dint i = 0; i < LOGIC_HASH_SIZE; ++i)
+        for(logicsound_t *it = logicHash[i].first; it; it = it->next)
         {
-            for(it = logicHash[i].first; it; it = it->next)
+            if(it->origin == origin && (it->endTime > nowTime || it->isRepeating))
             {
-                if(it->origin == origin &&
-                   (it->endTime > nowTime || it->isRepeating))
-                {
-                    // This one is playing.
-                    return true;
-                }
+                // This one is playing.
+                return true;
             }
         }
     }
