37a2c3ddc168d7f5ccc121917d49a800a23b470b
Jaakko Keränen <jaakko.keranen@iki.fi>
2016-02-02 07:47:42 +0200

    Windows|Fixed: Missing export

--- doomsday/sdk/libappfw/include/de/framework/callbackaction.h
+++ doomsday/sdk/libappfw/include/de/framework/callbackaction.h
@@ -19,6 +19,7 @@
 #ifndef LIBAPPFW_CALLBACKACTION_H
 #define LIBAPPFW_CALLBACKACTION_H
 
+#include "../libappfw.h"
 #include <de/Action>
 #include <functional>
 
@@ -27,7 +28,7 @@ namespace de {
 /**
  * Action that calls a callback function when triggered.
  */
-class CallbackAction : public Action
+class LIBAPPFW_PUBLIC CallbackAction : public Action
 {
 public:
     typedef std::function<void ()> Callback;
