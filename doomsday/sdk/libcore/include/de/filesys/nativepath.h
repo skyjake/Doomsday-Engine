a4ca99dbdada17af1755cfd1efe803478a791319
Jaakko Keränen <jaakko.keranen@iki.fi>
2016-03-20 11:57:55 +0200

    Cleanup

--- doomsday/sdk/libcore/include/de/filesys/nativepath.h
+++ doomsday/sdk/libcore/include/de/filesys/nativepath.h
@@ -14,7 +14,7 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
  * General Public License for more details. You should have received a copy of
  * the GNU Lesser General Public License along with this program; if not, see:
- * http://www.gnu.org/licenses</small> 
+ * http://www.gnu.org/licenses</small>
  */
 
 #ifndef LIBDENG2_NATIVEPATH_H
