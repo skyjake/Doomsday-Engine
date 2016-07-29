7e765e43065629dfb69606c83ca80df8538ba728
Jaakko Keränen <jaakko.keranen@iki.fi>
2016-07-29 21:57:45 +0300

    Cleanup|libcore: Undefined more conflicting X11 defines

--- doomsday/sdk/libcore/include/de/core/event.h
+++ doomsday/sdk/libcore/include/de/core/event.h
@@ -13,13 +13,20 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
  * General Public License for more details. You should have received a copy of
  * the GNU Lesser General Public License along with this program; if not, see:
- * http://www.gnu.org/licenses</small> 
+ * http://www.gnu.org/licenses</small>
  */
 
 #ifndef LIBDENG2_EVENT_H
 #define LIBDENG2_EVENT_H
 
 #include "../libcore.h"
+
+#ifdef KeyPress
+#  undef KeyPress
+#endif
+#ifdef KeyRelease
+#  undef KeyRelease
+#endif
 
 namespace de {
 
