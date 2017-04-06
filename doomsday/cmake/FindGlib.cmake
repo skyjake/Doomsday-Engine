71718a5d6b48c64eaddb384147fa31b9982f042a
Jaakko Keränen <jaakko.keranen@iki.fi>
2017-04-06 17:12:27 +0300

    macOS|CMake|FluidSynth: Option to use a static version of GLib et al.
    Previously, the build was configured to use GLib from Homebrew, but
    that is built for the currently installed version of macOS. Doomsday
    is targeting macOS 10.10, so a manually compiled GLib is required
    instead.
    
    IssueID #2242

--- doomsday/cmake/FindGlib.cmake
+++ doomsday/cmake/FindGlib.cmake
@@ -1,7 +1,26 @@
-find_package (PkgConfig)
+if (APPLE AND GLIB_STATIC_DIR AND GETTEXT_STATIC_DIR)
+    # Use the static versions of GLib and its dependencies.
+    add_library (glib INTERFACE)
+    target_link_libraries (glib INTERFACE
+        ${GLIB_STATIC_DIR}/lib/libglib-2.0.a
+        ${GLIB_STATIC_DIR}/lib/libgthread-2.0.a
+        ${GETTEXT_STATIC_DIR}/lib/libintl.a
+        iconv
+    )
+    target_include_directories (glib INTERFACE
+        ${GLIB_STATIC_DIR}/include/glib-2.0
+        ${GLIB_STATIC_DIR}/lib/glib-2.0/include
+        ${GETTEXT_STATIC_DIR}/include
+    )
+    link_framework (glib INTERFACE CoreFoundation)
+    link_framework (glib INTERFACE CoreServices)
+else ()
+    # Find GLib via pkg-config.
+    find_package (PkgConfig)
 
-add_pkgconfig_interface_library (glib glib-2.0 gthread-2.0)
+    add_pkgconfig_interface_library (glib glib-2.0 gthread-2.0)
 
-get_property (GLIB_LIBRARIES TARGET glib 
-    PROPERTY INTERFACE_LINK_LIBRARIES
-)
+    get_property (GLIB_LIBRARIES TARGET glib
+        PROPERTY INTERFACE_LINK_LIBRARIES
+    )
+endif ()
