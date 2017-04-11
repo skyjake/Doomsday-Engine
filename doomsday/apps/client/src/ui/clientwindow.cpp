cc651ccb3a1836accc6ca99581b464434ba34770
Jaakko Keränen <jaakko.keranen@iki.fi>
2017-04-11 19:12:55 +0300

    UI|Client: Show the “X Quit” button after startup
    IssueID #2217

--- doomsday/apps/client/src/ui/clientwindow.cpp
+++ doomsday/apps/client/src/ui/clientwindow.cpp
@@ -370,6 +370,8 @@ DENG2_PIMPL(ClientWindow)
         {
             taskBar->close();
         }
+
+        Loop::get().timer(1.0, [this] () { showOrHideQuitButton(); });
     }
 
     void currentGameChanged(Game const &newGame)
