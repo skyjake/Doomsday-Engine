bbfa693e4527de614a7be73970f7cb828ffb88c8
Jaakko Keränen <jaakko.keranen@iki.fi>
2017-04-15 09:33:54 +0300

    Unix|CMake: Added CMake variables for base and plugin directories
    IssueID #2244

--- doomsday/cmake/PlatformUnix.cmake
+++ doomsday/cmake/PlatformUnix.cmake
@@ -5,13 +5,24 @@ set (DENG_X11 ON)
 set (DENG_PLATFORM_SUFFIX x11)
 set (DENG_AMETHYST_PLATFORM UNIX)
 
+set (DENG_BASE_DIR    "" CACHE STRING "Base directory path (defaults to {prefix}/${DENG_INSTALL_DATA_DIR})")
+set (DENG_LIBRARY_DIR "" CACHE STRINGS "Plugin directory path (defaults to {prefix}/${DENG_INSTALL_PLUGIN_DIR})")
+
 add_definitions (
     -DDENG_X11
     -D__USE_BSD 
     -D_GNU_SOURCE=1
-    -DDENG_BASE_DIR="${CMAKE_INSTALL_PREFIX}/${DENG_INSTALL_DATA_DIR}"
-    -DDENG_LIBRARY_DIR="${CMAKE_INSTALL_PREFIX}/${DENG_INSTALL_PLUGIN_DIR}"
 )
+if (DENG_BASE_DIR)
+    add_definitions (-DDENG_BASE_DIR="${DENG_BASE_DIR}")
+else ()
+    add_definitions (-DDENG_BASE_DIR="${CMAKE_INSTALL_PREFIX}/${DENG_INSTALL_DATA_DIR}")
+endif ()
+if (DENG_LIBRARY_DIR)
+    add_definitions (-DDENG_LIBRARY_DIR="${DENG_LIBRARY_DIR}")
+else ()
+    add_definitions (-DDENG_LIBRARY_DIR="${CMAKE_INSTALL_PREFIX}/${DENG_INSTALL_PLUGIN_DIR}")
+endif ()
 
 if (CPACK_GENERATOR STREQUAL DEB)
     add_definitions (-DDENG_PLATFORM_ID="ubuntu16-${DENG_ARCH}")
