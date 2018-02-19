8dcf5eaff70a3941501cf6344b4f5d2e5b67557e
Jaakko Keränen <jaakko.keranen@iki.fi>
2018-02-19 10:17:05 +0200

    libgui|ModelDrawable: Print loaded animation tick rates to log

--- doomsday/sdk/libgui/src/graphics/modeldrawable.cpp
+++ doomsday/sdk/libgui/src/graphics/modeldrawable.cpp
@@ -718,7 +718,9 @@ DENG2_PIMPL(ModelDrawable)
         animNameToIndex.clear();
         for (duint i = 0; i < scene->mNumAnimations; ++i)
         {
-            LOG_GL_VERBOSE("Animation #%i name:%s") << i << scene->mAnimations[i]->mName.C_Str();
+            LOG_GL_VERBOSE("Animation #%i name:%s tps:%f")
+                    << i << scene->mAnimations[i]->mName.C_Str()
+                    << scene->mAnimations[i]->mTicksPerSecond;
 
             String const name = scene->mAnimations[i]->mName.C_Str();
             if (!name.isEmpty())
