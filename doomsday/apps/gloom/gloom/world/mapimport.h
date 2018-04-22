7336150a315361d6f08dd1b0c64d0e4880b6c730
Jaakko Keränen <jaakko.keranen@iki.fi>
2018-04-22 12:23:20 +0300

    Gloom: Promoted Gloom from a test app to a regular app
    Now that there is need to use libdoomsday, it should be in the apps
    directory.

---
+++ doomsday/apps/gloom/gloom/world/mapimport.h
@@ -0,0 +1,42 @@
+/** @file mapimport.h  Importer for id-formatted map data.
+ *
+ * @authors Copyright (c) 2018 Jaakko Keränen <jaakko.keranen@iki.fi>
+ *
+ * @par License
+ * LGPL: http://www.gnu.org/licenses/lgpl.html
+ *
+ * <small>This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details. You should have received a copy of
+ * the GNU Lesser General Public License along with this program; if not, see:
+ * http://www.gnu.org/licenses</small>
+ */
+
+#ifndef GLOOM_MAPIMPORT_H
+#define GLOOM_MAPIMPORT_H
+
+#include <de/LumpCatalog>
+
+namespace gloom {
+
+using namespace de;
+
+/**
+ * Imports an id-formatted map, converting it to the Gloom format.
+ */
+class MapImport
+{
+public:
+    MapImport(const LumpCatalog &lumps, const String &mapId);
+
+private:
+    DENG2_PRIVATE(d)
+};
+
+} // namespace gloom
+
+#endif // GLOOM_MAPIMPORT_H
