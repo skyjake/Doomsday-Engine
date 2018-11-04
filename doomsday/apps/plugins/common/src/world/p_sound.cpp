e98b33bd808b1934d8b2163d2162e44d34c77042
Jaakko Keränen <jaakko.keranen@iki.fi>
2018-11-04 12:47:14 +0200

    Music|Definitions: Fall back to default music
    If a custom Map Info doesn’t specify any music, it should use the map’s default song.
    
    Removed the default songs from the games’ patchmapinfo.ded, as that method of applying the default songs does not work with custom Map Infos that don’t set a music ID. Instead, there is now a Music definition matching each map ID.
    
    IssueID #2291

--- doomsday/apps/plugins/common/src/world/p_sound.cpp
+++ doomsday/apps/plugins/common/src/world/p_sound.cpp
@@ -35,11 +35,21 @@ using namespace de;
 
 void S_MapMusic(de::Uri const &mapUri)
 {
-    Block const musicIdUtf8 = G_MapInfoForMapUri(mapUri).gets("music").toUtf8();
-    if(S_StartMusic(musicIdUtf8.constData(), true))
+    String musicId = G_MapInfoForMapUri(mapUri).gets("music");
+    if (musicId.isEmpty())
+    {
+        musicId = mapUri.path();
+    }
+    if (S_StartMusic(musicId.toUtf8(), true))
     {
         // Set the game status cvar for the map music.
-        Con_SetInteger2("map-music", Defs().getMusicNum(musicIdUtf8.constData()), SVF_WRITE_OVERRIDE);
+        Con_SetInteger2("map-music", Defs().getMusicNum(musicId.toUtf8()), SVF_WRITE_OVERRIDE);
+    }
+    else
+    {
+        // We don't have anything to play.
+        S_StopMusic();
+        Con_SetInteger2("map-music", -1, SVF_WRITE_OVERRIDE);
     }
 }
 
