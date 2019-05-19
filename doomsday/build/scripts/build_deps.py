063e8a0b9c684fa19e4be32085ead03bf3db85c4
Jaakko Keränen <jaakko.keranen@iki.fi>
2019-05-19 09:41:08 +0300

    Builder: Check out a remote branch for deps

--- doomsday/build/scripts/build_deps.py
+++ doomsday/build/scripts/build_deps.py
@@ -16,21 +16,21 @@ for fmt in FORMATS:
 # The libraries are installed under the specified directory.
 dependencies = [
     (
-        'the_Foundation', 
-        'ssh://git@github.com/skyjake/the_Foundation.git', 'master',
+        'the_Foundation',
+        'ssh://git@github.com/skyjake/the_Foundation.git', 'origin/master',
         ['-DUNISTRING_DIR=/usr/local']
     ),
     (
         'Open Asset Import Library',
-        'https://github.com/assimp/assimp.git', 'v4.1.0', 
+        'https://github.com/assimp/assimp.git', 'v4.1.0',
         ['-Wno-dev',
          '-DASSIMP_BUILD_ASSIMP_TOOLS=OFF',
          '-DASSIMP_BUILD_TESTS=OFF'] +
         ['-DASSIMP_BUILD_%s_IMPORTER=%s' % (fmt, fmt_flag[fmt]) for fmt in FORMATS]
     ),
     (
-        'cginternals/glbinding', 
-        'https://github.com/cginternals/glbinding.git', 'v3.0.2', 
+        'cginternals/glbinding',
+        'https://github.com/cginternals/glbinding.git', 'v3.0.2',
         ['-Wno-dev',
          '-DOPTION_BUILD_EXAMPLES=NO',
          '-DOPTION_BUILD_TOOLS=NO',
@@ -55,7 +55,7 @@ for long_name, git_url, git_tag, cmake_opts in dependencies:
     if not os.path.exists(src_dir):
         os.makedirs(src_dir, exist_ok=True)
         os.chdir(src_dir)
-        subprocess.check_call(['git', 'clone', git_url, '.']) 
+        subprocess.check_call(['git', 'clone', git_url, '.'])
     else:
         os.chdir(src_dir)
         subprocess.check_call(['git', 'fetch', '--tags'])
@@ -67,13 +67,13 @@ for long_name, git_url, git_tag, cmake_opts in dependencies:
     os.makedirs(build_dir, exist_ok=True)
     os.chdir(build_dir)
     print(build_dir)
-    subprocess.check_call(['cmake', 
-        '-G', GENERATOR, 
+    subprocess.check_call(['cmake',
+        '-G', GENERATOR,
         '-DCMAKE_BUILD_TYPE=' + BUILD_TYPE,
-        '-DCMAKE_INSTALL_PREFIX=' + PRODUCTS_DIR] + 
-        cmake_opts +         
+        '-DCMAKE_INSTALL_PREFIX=' + PRODUCTS_DIR] +
+        cmake_opts +
         ['..'])
     subprocess.check_call(['cmake',
         '--build', '.',
-        '--target', 'install'        
+        '--target', 'install'
         ])
