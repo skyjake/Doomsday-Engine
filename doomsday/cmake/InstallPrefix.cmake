11d7a0d586c78d0904bdf2665ee60d48630571e0
Jaakko Keränen <jaakko.keranen@iki.fi>
2019-05-29 22:10:42 +0300

    CMake: Simplified install prefix configuration

--- doomsday/cmake/InstallPrefix.cmake
+++ doomsday/cmake/InstallPrefix.cmake
@@ -16,14 +16,28 @@ if (DEFINED DE_PREFIX OR NOT DEFINED DE_PREFIX_SET)
     else ()
         #message ("The default install prefix can be overridden with DE_PREFIX.")
         get_filename_component (installPrefix "${DE_DISTRIB_DIR}" REALPATH)
-        set (CMAKE_INSTALL_PREFIX "${installPrefix}" CACHE STRING "Install prefix" FORCE)
+        message (FATAL_ERROR "DE_PREFIX is obsolete. Use CMAKE_INSTALL_PREFIX instead.")
     endif ()
-    if (NOT _oldPrefix STREQUAL CMAKE_INSTALL_PREFIX)
-	message (STATUS "Install prefix: ${CMAKE_INSTALL_PREFIX}")
-    endif ()
-    set (DE_PREFIX_SET YES CACHE STRING "Install prefix applied from the DE_PREFIX variable")
-    mark_as_advanced (DE_PREFIX_SET)
-endif ()
+
+# Install destination. DE_PREFIX can be used to set the location manually.
+# By default we'll use distrib/products as the prefix.
+#if (DEFINED DE_PREFIX OR NOT DEFINED DE_PREFIX_SET)
+#    if (DEFINED DE_PREFIX)
+#        get_filename_component (PREFIX "${PREFIX}" ABSOLUTE)
+#        set (CMAKE_INSTALL_PREFIX "${PREFIX}" CACHE "Install prefix" STRING FORCE)
+#    else ()
+#        #message ("The default install prefix can be overridden with DE_PREFIX.")
+
+get_filename_component (defaultInstallPrefix "${DE_DISTRIB_DIR}" REALPATH)
+set (CMAKE_INSTALL_PREFIX "${defaultInstallPrefix}" CACHE STRING "Install prefix")
+
+#    endif ()
+#    if (NOT _oldPrefix STREQUAL CMAKE_INSTALL_PREFIX)
+#message (STATUS "Install prefix: ${CMAKE_INSTALL_PREFIX}")
+#    endif ()
+#    set (DE_PREFIX_SET YES CACHE STRING "Install prefix applied from the DE_PREFIX variable")
+#    mark_as_advanced (DE_PREFIX_SET)
+#endif ()
 
 # Some CMake targets may be exported to the install directory.
 list (APPEND CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX})
