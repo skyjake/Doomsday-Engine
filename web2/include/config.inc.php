c5c42c197cf9430105a3382303de9792f33b12ad
Jaakko Keränen <jaakko.keranen@iki.fi>
2019-07-29 21:54:37 +0300

    Homepage: Switch to HTTPS for certain links

--- web2/include/config.inc.php
+++ web2/include/config.inc.php
@@ -4,13 +4,13 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 
-define('SITE_ROOT',     'http://dengine.net');
+define('SITE_ROOT',     'https://dengine.net');
 define('SITE_DATA',     '/home/skyjake/files/sitedata.json');
 define('ROOT_PATH',     '/home/skyjake/public_html');
 
 define('DENG_API_URL',      'http://api.dengine.net/1');
 define('DENG_MANUAL_URL',   'https://manual.dengine.net');
-define('DENG_BLOG_URL',     'http://blog.dengine.net');
+define('DENG_BLOG_URL',     'https://blog.dengine.net');
 
 define('DENG_LIB_DIR',          '/home/skyjake/public_html/lib');
 define('DENG_CACHE_PATH',       '/home/skyjake/public_html/cache/api');
