27010777755be7231f6a1c9fb4f9196f72cbc8a6
Jaakko Keränen <jaakko.keranen@iki.fi>
2020-05-10 11:34:23 +0300

    Fixed|HUD: Incorrect default value for "view-cross-width"
    IssueID #2425

--- doomsday/apps/plugins/heretic/src/h_main.cpp
+++ doomsday/apps/plugins/heretic/src/h_main.cpp
@@ -182,6 +182,7 @@ void H_PreInit()
     cfg.common.hudIconAlpha = 1;
     cfg.common.xhairAngle = 0;
     cfg.common.xhairSize = .5f;
+    cfg.common.xhairLineWidth = 1;
     cfg.common.xhairVitality = false;
     cfg.common.xhairColor[0] = 1;
     cfg.common.xhairColor[1] = 1;
