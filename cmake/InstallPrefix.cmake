a283cbdf276453ac679bf6c255caf50748fa29f7
Jaakko Keränen <jaakko.keranen@iki.fi>
2026-04-18 18:22:38 +0300

    CMake: Installing to the default location

--- cmake/InstallPrefix.cmake
+++ cmake/InstallPrefix.cmake
@@ -9,11 +9,5 @@ if (DEFINED DE_PREFIX)
     message (FATAL_ERROR "DE_PREFIX is obsolete. Use CMAKE_INSTALL_PREFIX instead.")
 endif ()
 
-# Default install destination: <root>/products (unless the user specified something else).
-if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-    set (CMAKE_INSTALL_PREFIX "${DE_SOURCE_DIR}/products"
-         CACHE PATH "Install prefix" FORCE)
-endif ()
-
 # Some CMake targets may be exported to the install directory.
 list (APPEND CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX})
